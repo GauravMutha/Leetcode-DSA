c01a711684e2be8dc941aa1e0878806c5d64344f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-07-28 20:16:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.87%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int numberOfSteps(int num) {
-        //Bit Manipulation
+        //Bit Manipulation - O(logn)
         if(num==0){
             return 0;
         }
+        int res=0;
         
-        else if(num%2==0){
-            return  numberOfSteps(num>>1)+1;
+        while(num!=0){
+            res+=(num&1)?2:1;
+            num>>=1;
         }
-        else{
-            return numberOfSteps(num&=~1)+1;
-        }
+        return res-1;
+        
     }
 };
