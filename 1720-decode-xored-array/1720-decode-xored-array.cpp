d9a7753a15b2b086c8ffa8070748a8cad7aa07b6
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-07-28 18:03:27 +0530

    Time: 45 ms (56.81%), Space: 24.7 MB (99.34%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,39 +1,11 @@
 class Solution {
 public:
-   void calc(vector<int>& v,int first,int ind,vector<int>& ans){
-        if(ans.size()==(v.size()+1)){
-            return;
-        }
-        else if(first==v[ind]){
-            ans.push_back(0);
-        }
-        else if(first==0){
-            ans.push_back(v[ind]);
-        }
-        else {
-        int temp=first;
-        for(int i=0;i<log2(v[ind])+1;){
-            int num=1;
-            num=num<<i;
-            if(num & v[ind]){
-               if(temp&num){
-                   temp=temp&~(1<<i);
-               }
-                else{
-                    temp=temp|num;
-                }
-            }
-            i++;
-        }
-        ans.push_back(temp);
-        }
-        calc(v,ans[ind+1],ind+1,ans);
-}
-
  vector<int> decode(vector<int>& encoded, int first) {
-        vector<int> ans;
-        ans.push_back(first);
-        calc(encoded,first,0,ans);
+        vector<int> ans(encoded.size()+1,0);
+        ans[0]=first;
+        for(int i=0;i<encoded.size();i++){
+            ans[i+1]=ans[i]^encoded[i];
+        }
         return ans;
     }
 
