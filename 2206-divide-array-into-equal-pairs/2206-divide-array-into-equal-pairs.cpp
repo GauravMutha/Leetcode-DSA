a39f6a0c23ad84182e5a10c7927e4dbada57b1aa
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-07-30 13:01:50 +0530

    Time: 22 ms (58.34%), Space: 13.1 MB (90.94%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        //TC--->O(n)
-        int n=*max_element(nums.begin(),nums.end());
-        vector<int>counter(n+1,0);
+        //TC--->O(nlogn)
+        sort(nums.begin(),nums.end());
         
-        for(int i=0;i<nums.size();i++){
-            counter[nums[i]]++;
-        }
-        
-        for(int i=0;i<n+1;i++){
-            if(counter[i]%2!=0){
+        for(int i=0;i<nums.size()-1;){
+            if(nums[i]==nums[i+1])
+                i+=2;
+            else
                 return false;
-            }
         }
+        
         return true;
+        
     }
 };
