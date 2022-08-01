113e3d2831aa1cee97e935494f403e36359553e6
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-01 18:09:39 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.24%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -5,14 +5,8 @@ public:
         int x=(int)log2(n)+1;
         int highest=pow(2,x)-1;
         helper<<=(x-1);
-        if(helper&n){
-            if((n^highest)==(n>>1))
+        if(((n^highest)==(n>>1)) || ((n^highest)==(n<<1)))
                 return true;
-        }
-        else{
-            if((n^highest)==(n<<1))
-                return true;
-        }
         return false;
     }
 };
