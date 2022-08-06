8350f994c1d5a204bf7b2d5f28ce8e8e7e44f5db
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-06 17:45:13 +0530

    Time: 50 ms (64.11%), Space: 21.4 MB (86.89%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
    vector<int> findErrorNums(vector<int>& nums) {
+       //TC--->O(n)
+       
        vector<int>ans;
-        for(int i = 0; i<nums.size(); i++){
-            while(nums[i] != nums[nums[i] - 1])swap(nums[i], nums[nums[i] - 1]);
-        }
-        for(int i = 0; i<nums.size() ; i++){
-            if(nums[i] != i + 1){ans={nums[i], i + 1}; break;}
-        }
+       for(int i=0;i<nums.size();i++){
+           while(nums[i]!=nums[nums[i]-1])
+                  swap(nums[i],nums[nums[i]-1]);
+           
+       }
+       for(int i=0;i<nums.size();i++){
+           if(nums[i]!=i+1){ans={nums[i],i+1};break;}
+       }
        return ans;
     }
 };
