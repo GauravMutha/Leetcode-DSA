73874ba38cd9e068d26bf90335895af19587c8f7
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-06 12:36:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (58.31%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    string converter(uint32_t num)
-{
-    string res = {};
-    vector<char> v = {'a', 'b', 'c', 'd', 'e', 'f'};
-    while (num)
-    {
-        int x = num % 16;
-        if (x < 10)
-            res += to_string(x);
-        else
-            res += (v[x % 10]);
-        num /= 16;
-    }
-    reverse(res.begin(), res.end());
-    return res;
-}
-
 string toHex(int num)
 {
-    if(num==0){return "0";}
-    
-    else if (num >= 0)
-    {
-        return converter(num);
+    if(num==0)return "0";
+    string res={};
+    vector<char>m={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    if(num>0)
+        {while(num!=0){
+        res+=m[num&15];
+        num>>=4;}
     }
-    else
-    {
-        uint32_t x = abs(num);
-        for (int i = 0; i < 32; i++)
-        {
-            (x & (1 << i)) ? (x &= ~(1 << i)) : (x |= (1 << i));
+    else{
+        int count=0;
+        while(count<8){
+            res+=m[num&15];
+            count++;
+            num>>=4;
         }
-        x += 1;
-        return converter(x);
     }
+    reverse(res.begin(),res.end());
+    return res;
 }
 };
