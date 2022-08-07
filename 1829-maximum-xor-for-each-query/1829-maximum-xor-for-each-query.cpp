743127fdec5c0fba6e621b8e246eb5f3b86c90b7
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-07 17:21:19 +0530

    Time: 227 ms (65.35%), Space: 97.1 MB (37.46%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        //Optimizing for space usage by removing the need of prefix array
         vector<int>ans={};
-        vector<int>prexor(nums.size(),nums[0]);
+        int total=nums[0];
         for(int i=1;i<nums.size();i++){
-            prexor[i]=prexor[i-1]^nums[i];
+            total^=nums[i];
         }
         int maxxor=pow(2,maximumBit)-1;
         for(int j=nums.size()-1;j>=0;j--){
-            ans.push_back(prexor[j]^maxxor);
+            ans.push_back(total^maxxor);
+            total^=nums[j];
         }
         return ans;
     }
