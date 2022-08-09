71bf96beaff31ab95cea523d4f37259f950c5cda
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-09 10:33:09 +0530

    Time: 3 ms (84.91%), Space: 7.6 MB (37.93%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        //TC --->O(n^3) SC--->O(n)
+        //TC --->O(n^2) SC--->O(n)
         //This method uses prefix array/vector to store and access xor of subarray pairs
         int n=arr.size(),count=0;
         vector<int>prexor(n);
@@ -10,10 +10,8 @@ public:
             prexor[i]=prexor[i-1]^arr[i];
         }
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j;k<n;k++){
-                    if((prexor[i]^arr[i]^prexor[j-1])==(prexor[k]^prexor[j]^arr[j]))count++;
-                }
+            for(int k=i+1;k<n;k++){
+                if((prexor[k]^prexor[i]^arr[i])==0){count+=(k-i);}
             }
         }
         return count;
