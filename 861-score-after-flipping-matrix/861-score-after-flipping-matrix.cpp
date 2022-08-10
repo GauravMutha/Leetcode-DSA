8db3d09706e3e6adadfa91b45bdb1e5052df6155
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-10 11:33:12 +0530

    Time: 3 ms (82.52%), Space: 8.3 MB (20.81%) - LeetHub

--- 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
+++ 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 int matrixScore(vector<vector<int>>& grid) {
     //TC-->O(m*n) SC-->O(n)
+    //Two pass solution
         int m=grid.size();
         int n=grid[0].size();
         vector<int>vect(n,0);
@@ -11,19 +12,18 @@ int matrixScore(vector<vector<int>>& grid) {
             if(grid[i][0]==0){
                 for(int j=0;j<n;j++){
                     grid[i][j]=(!grid[i][j])?1:0;
+                    vect[j]+=grid[i][j];
                 }
             }
-        }
-        for(int j=0;j<n;j++){
-            int count=0;
-            for(int i=0;i<m;i++){
-                if(grid[i][j]==1)count++;
+            else{
+                for(int j=0;j<n;j++){
+                    vect[j]+=grid[i][j];
+                } 
             }
-            if(count>m/2)vect[j]++;
         }
         //column toggle
         for(int j=0;j<n;j++){
-            if(!vect[j]){
+            if(vect[j]<=(m/2)){
                 for(int i=0;i<m;i++){
                     grid[i][j]=(!grid[i][j])?1:0;
                     total+=pow(2,n-j-1)*grid[i][j];
