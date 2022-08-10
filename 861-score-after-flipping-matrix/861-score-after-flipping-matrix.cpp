31c553d7b0c5a8449441b8fde442b42d4d8b1e99
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-10 10:33:00 +0530

    Time: 6 ms (46.37%), Space: 8.3 MB (20.81%) - LeetHub

--- 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
+++ 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
@@ -5,6 +5,7 @@ int matrixScore(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         vector<int>vect(n,0);
+        int total=0;
         //row toggle
         for(int i=0;i<m;i++){
             if(grid[i][0]==0){
@@ -24,18 +25,16 @@ int matrixScore(vector<vector<int>>& grid) {
         for(int j=0;j<n;j++){
             if(!vect[j]){
                 for(int i=0;i<m;i++){
-                    grid[i][j]=(!grid[i][j])?1:0;     
+                    grid[i][j]=(!grid[i][j])?1:0;
+                    total+=pow(2,n-j-1)*grid[i][j];
+                }
+            }
+            else{
+                for(int i=0;i<m;i++){
+                    total+=pow(2,n-j-1)*grid[i][j];
                 }
             }
         }
-        int total=0;
-        for(int i=0;i<m;i++){
-            int num=0;
-            for(int j=0;j<n;j++){
-                num+=pow(2,n-1-j)*grid[i][j];
-            }
-            total+=num;
-        }
-        return total;
+        return  total;
     }
 };
