af643a94ca908c4d0d106684d372fc52977c1ab5
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-11 12:47:08 +0530

    Time: 208 ms (20.22%), Space: 8.4 MB (56.91%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
+    //Using Bit manipulation
+    //This solution has TC---->O(n*2^n) same as last one.
+    //But here we use bit manipulation so that resuces SC to O(1)
     int res=0,maxi=INT_MIN;
     int countMaxOrSubsets(vector<int>& nums) {
-        vector<int>ds;
-        calc(nums,ds,0);
+        calc(nums);
         return res+1;
     }
-    void calc(vector<int>&nums,vector<int>&ds,int ind){
-        int x=0;
-        if(ind==nums.size()){
-            if(ds.size()==0)
-                return;
-            else if(ds.size()==1)
-                x=ds[0];
-            else {
-                x=ds[0];
-                for(int i=1;i<ds.size();i++)
-                    x|=ds[i];
-            }
-            checker(x);
-            return;
+   void calc(vector<int> &nums)
+{
+    for (int num = 1; num < pow(2, nums.size()); num++)
+    {
+        int x = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (num & (1 << i))
+                x |= nums[i];
         }
-        ds.push_back(nums[ind]);
-        calc(nums,ds,ind+1);
-        ds.pop_back();
-        calc(nums,ds,ind+1);
+        checker(x);
     }
+}
     
     void checker(int x){
         if(x>maxi){res=0; maxi=x;}
         else if(x<maxi)return;
         else if(x==maxi)res++;
-        return;
     }
 };
