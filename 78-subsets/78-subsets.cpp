b8552481c30b6bfe0fbbc9c2e7d68dc11d26e4f2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-13 11:12:33 +0530

    Time: 7 ms (35.77%), Space: 7.3 MB (51.27%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>>ans;
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>ans;
-        for(int x=0;x<pow(2,n);x++){
-            vector<int>ele={};
-            for(int i=0;i<n;i++){
-                if(x&(1<<i))ele.push_back(nums[i]);
-            }
-            ans.push_back(ele);
-        }
+        backtrack(nums,0);
         return ans;
+    }
+    void backtrack(vector<int>&nums,int i){
+        static vector<int>ds;
+        if(i==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(nums[i]);
+        backtrack(nums,i+1);
+        ds.pop_back();
+        backtrack(nums,i+1);
     }
 };
