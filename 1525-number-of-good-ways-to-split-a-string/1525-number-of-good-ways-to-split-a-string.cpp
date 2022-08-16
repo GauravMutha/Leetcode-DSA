84629c38ddc1156223ccdf968b644c3fde59ea75
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-16 14:43:28 +0530

    Time: 28 ms (84.95%), Space: 12 MB (22.55%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int numSplits(string s) {
-    int count=0;
-    unordered_map<char,int> m;
-    vector<int>prexor(s.size()); 
-    vector<int>suffxor(s.size());
-    for(int i=0;i<s.size();i++){
-        m[s[i]]++;
-        prexor[i]=m.size();
+        int res =0;
+        int H=0,x=0,count=0;
+        int n=s.size ();
+        vector<int>prexor(n,0);
+        vector<int>suffxor(n,0);
+        for(int i=0;i<n;i++){
+            x=1;
+            x<<=(s[i]-97);
+            if((H&x)==0){
+                count++;
+                H|=x;
+            }
+            prexor[i]=count;
+        }
+        H=0;count=0;
+        for(int i=n-1;i>=0;i--){
+            x=1;
+            x<<=(s[i]-97);
+            if((H&x)==0){
+                count++;
+                H|=x;
+            }
+            
+            suffxor[i]=count;
+        }
+        for(int i=0;i<n-1;i++){
+            if(prexor[i]==suffxor[i+1])
+                res++;
+        }
+        return res;
     }
-    m.clear();
-    for(int i=s.size()-1;i>=0;i--){
-        m[s[i]]++;
-        suffxor[i]=m.size();
-    }
-    for(int i=0;i<s.size()-1;i++){
-        if(prexor[i]==suffxor[i+1])
-            count++;
-       
-}
-        return count;
-}
 };
-
-//TC--->O(n)
-//SC--->O(n)
