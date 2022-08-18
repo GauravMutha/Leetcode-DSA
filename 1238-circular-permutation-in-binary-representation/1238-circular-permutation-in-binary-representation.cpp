5a2ab28ea72d8d3a0ec87f21739454abc5a613f1
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-18 19:42:36 +0530

    Time: 24 ms (74.91%), Space: 17.6 MB (82.69%) - LeetHub

--- 1238-circular-permutation-in-binary-representation/1238-circular-permutation-in-binary-representation.cpp
+++ 1238-circular-permutation-in-binary-representation/1238-circular-permutation-in-binary-representation.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
 vector<int> circularPermutation(int n, int start) {
         vector<int>res;
-        vector<int>gc;
-        int value,i=0,j=0;
         int x=pow(2,n);
-        for(int j=0;j<x;j++){
-            gc.push_back((j^(j>>1)));
-            if((j^(j>>1))==start)
-                value=j;
+        for(int i=0;i<x;i++){
+            res.push_back(start^i^i>>1);
         }
-        i=value;
-        do{
-            res.push_back(gc[i%x]);
-            i++;
-            j++;
-        }while(j<x);
-        return res;
+    return res;
     } 
 };
