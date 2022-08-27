89026a9fd2063a25e4a5fb884b7867e2a2ff104a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-27 19:39:38 +0530

    Time: 8 ms (86.55%), Space: 9.4 MB (86.96%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res=0,count=0;
+        int tn=-1,tnp1=0,tnp2=0;
         
-        for(int i=0;i<32;i++){
-            count=0;
-            int mask=1<<i;
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]&mask)count++;
-            }
-            if(count%3)res|=mask;
+        for(int i=0;i<nums.size();i++){
+            int cwtn=tn & nums[i];
+            int cwtnp1=tnp1 & nums[i];
+            int cwtnp2=tnp2 & nums[i];
+            
+            tn=tn & (~cwtn);
+            tnp1=tnp1 | cwtn;
+            
+            tnp1=tnp1 & (~cwtnp1);
+            tnp2=tnp2 | cwtnp1;
+            
+            tnp2=tnp2 & (~cwtnp2);
+            tn=tn | cwtnp2;
         }
-        return res;
+        return tnp1;
     }
 };
-//TC--->O(32*n)~O(n)
+//TC--->O(n)
 //SC--->O(1)
