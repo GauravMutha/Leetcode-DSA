f5f8a17f4ac187067658ca11ba76e3c12bb13994
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-08-29 18:40:45 +0530

    Time: 1 ms (44.31%), Space: 6.1 MB (15.78%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-string toHex(int num)
-{
-    if(num==0)return "0";
-    string res={};
-    vector<char>m={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-    if(num>0)
-        {while(num!=0){
-        res+=m[num&15];
-        num>>=4;}
-    }
-    else{
-        int count=0;
-        while(count<8){
-            res+=m[num&15];
-            count++;
-            num>>=4;
+    string toHex(int num) {
+        if(num==0)return "0";
+        vector<char>hexsymb={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        string res={};
+        if(num>0){
+            while(num!=0){
+                res+=hexsymb[num&15];
+                num>>=4;
+            }
         }
+        else{
+            int count=0;
+           while(count<8){
+               res+=hexsymb[num&15];
+               count++;
+               num>>=4;
+           } 
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
-    reverse(res.begin(),res.end());
-    return res;
-}
 };
