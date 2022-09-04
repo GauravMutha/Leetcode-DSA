f478a19c517b89ec594f0deef1b2b3f46efbab72
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-04 16:23:22 +0530

    Time: 545 ms (65.54%), Space: 98.7 MB (84.23%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int wordCount(vector<string>& startWords, vector<string>& targetWords) {
         int n=startWords.size(),m=targetWords.size(),count=0;
-        vector<int> tv(m,0);
         unordered_set<int>sv;
         
         for(int i=0;i<n;i++){
@@ -12,17 +11,14 @@ public:
             sv.insert(num);
         }
         
-//         for(int i=0;i<m;i++)
-//             for(auto ch :targetWords[i])
-//                 tv[i]|=(1<<(ch-'a'));
-        
         for(int i=0;i<m;i++){
+            int bin=0;
             for(auto ch :targetWords[i])
-                tv[i]|=(1<<(ch-'a'));
+                bin|=(1<<(ch-'a'));
             for(auto ch:targetWords[i]){
-                int temp=tv[i]^(1<<(ch-'a'));
+                int temp=bin^(1<<(ch-'a'));
                 if(sv.find(temp)!=sv.end()){count++;break;}
-                temp=tv[i]^(1<<(ch-'a'));
+                temp=bin^(1<<(ch-'a'));
             }
         }
         return count;
