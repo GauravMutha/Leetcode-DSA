5395add81c15c25a8b73e033c62ee50097161eae
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-06 18:06:11 +0530

    Time: 3 ms (89.27%), Space: 10.3 MB (60.73%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    string findDifferentBinaryString(vector<string>& nums) {
-        string str(nums.size(),' ');
-        back(str,nums.size(),0,nums);
-        return str;
+    string binstr(int num,int n){
+        string res;
+        while(num){
+            res+=(num%2)?'1':'0';
+            num/=2;
+        }
+        while(n-res.size())res+="0";
+        reverse(res.begin(),res.end());
+        return res;
     }
     
-    bool back(string& str,int n,int ind,vector<string>& nums){
-
-        if(ind>=n){
-            if(std::find(nums.begin(),nums.end(),str)==nums.end()){
-                return 0;
-            }
-            return 1;
-        }
-        int flag0=0,flag1=0;
-        str[ind]='0';
-        flag0=back(str,n,ind+1,nums);
-
-        if(flag0==1){
-            str[ind]='1';
-            flag1=back(str,n,ind+1,nums);
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n=nums.size(),res=0;
+        vector<int>vec;
+        for(auto str : nums){
+            vec.push_back(std::stoi(str,nullptr,2));
         }
         
-        return (flag0);
+        for(int i=0;i<pow(2,n);i++){
+            auto it=std::find(vec.begin(),vec.end(),i);
+            if(it==vec.end()){
+                res=i;
+                break;
+            }
+        }
+            return binstr(res,n);
     }
 };
+
+//Logic---> Converting Every string of nums into numbers then checking 
+//by generating all the binary numbers for n bits where n is size of nums.
+
+//TC-->
+//SC-->
