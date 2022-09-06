fe4b7c3d1146b1e162b684b079a4c726a73b4561
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-06 18:38:04 +0530

    Time: 8 ms (48.62%), Space: 10.3 MB (60.73%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    string binstr(int num,int n){
-        string res;
-        while(num){
-            res+=(num%2)?'1':'0';
-            num/=2;
-        }
-        while(n-res.size())res+="0";
-        reverse(res.begin(),res.end());
-        return res;
-    }
     
     string findDifferentBinaryString(vector<string>& nums) {
-        int n=nums.size(),res=0;
-        vector<int>vec;
-        for(auto str : nums){
-            vec.push_back(std::stoi(str,nullptr,2));
-        }
-        
+        int n=nums.size();
+        string test;
         for(int i=0;i<pow(2,n);i++){
-            auto it=std::find(vec.begin(),vec.end(),i);
-            if(it==vec.end()){
-                res=i;
-                break;
-            }
+            test=bitset<16>(i).to_string();
+            test=test.substr(16-n,n);
+            if(std::find(nums.begin(),nums.end(),test)==nums.end())break;
         }
-            return binstr(res,n);
+            return test;
     }
 };
 
-//Logic---> Converting Every string of nums into numbers then checking 
-//by generating all the binary numbers for n bits where n is size of nums.
+//Logic-->Converting all the possible n bit numbers into string and then
+//checking if its present in nums vector
 
-//TC-->
-//SC-->
+//TC-->n*2^n
+//SC-->1
