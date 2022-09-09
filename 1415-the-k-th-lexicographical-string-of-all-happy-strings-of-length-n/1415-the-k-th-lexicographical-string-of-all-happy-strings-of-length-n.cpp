9b4a3bb82d8da3dfd3b56f8e7df33babfb7002dc
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-09 18:37:43 +0530

    Time: 3 ms (85.93%), Space: 8 MB (55.57%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -3,21 +3,22 @@ public:
     string getHappyString(int n, int k) {
         vector<string>res;
         string temp="";
-        generate(res,temp,n);
-        if(res.size()<k)return "";
-        return res[k-1];
+        generate(res,temp,n,k);
+        
+        return (res.size()==k)?res.back():"";
     }
     
-    void generate(vector<string>& res,string& curr,int n){
+    void generate(vector<string>& res,string& curr,int n,int k){
         static vector<char>vect={'a','b','c'};
+        if(res.size()==k)return;
         if(curr.size()==n){
-           res.push_back(curr);
+            res.push_back(curr);
             return;
         }
         for(int i=0;i<vect.size();i++){
             if(curr.size() && (curr.back()==vect[i]))continue;
             curr+=vect[i];
-            generate(res,curr,n);
+            generate(res,curr,n,k);
             curr.pop_back();
         }
     }
