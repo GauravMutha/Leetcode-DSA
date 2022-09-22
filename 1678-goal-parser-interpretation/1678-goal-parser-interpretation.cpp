eb80186dcc1146aacf092b48e02dc0fcb0f7dc27
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-22 21:22:16 +0530

    Time: 4 ms (30.54%), Space: 6.2 MB (15.01%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -3,20 +3,14 @@ public:
     string interpret(string command) {
         string res;
         
-        for(int i=0;i<command.size();){
-            if(command[i]=='G'){
+        for(int i=0;i<command.size();i++){
+            if(command[i]=='G')
                 res+="G";
-                i++;
-            }
-            else{
-                if(command[i+1]==')'){
-                    res+="o";
-                    i+=2;
-                }
-                else{
-                    res+="al";
-                    i+=4;
-                }
+            else if(command[i]==')')
+                res+="o";
+            else if(command[i]=='a'){
+                res+="al";
+                i+=2;
             }
         }
         return res;
