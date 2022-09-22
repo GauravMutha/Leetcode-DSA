547409e1aa5bbd90d44fb30806a39fc083cbaf78
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-22 20:08:18 +0530

    Time: 2 ms (68.36%), Space: 6.2 MB (54.14%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,12 +2,21 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int res=0;
-        for(auto j :jewels){
-            for(auto s:stones){
-                if(s==j)res++;
+        
+        vector<int>upper(26,0);
+        vector<int>lower(26,0);
+        
+        for(auto c:jewels)
+            (c<=90)?upper[c-65]++:lower[c-97]++;
+        
+        for(auto c:stones){
+            if(c<=90){
+                if(upper[c-65])res++;
+            }
+            else{
+                if(lower[c-97])res++;
             }
         }
-        
         return res;
     }
 };
