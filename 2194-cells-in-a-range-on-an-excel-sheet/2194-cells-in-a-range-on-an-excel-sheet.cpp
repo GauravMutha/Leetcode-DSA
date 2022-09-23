873c97c71c4e98d3d4658e0aaf43b693aa515ea5
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-23 16:30:12 +0530

    Time: 7 ms (41.20%), Space: 7.8 MB (20.21%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
@@ -5,7 +5,7 @@ public:
         string str="";
         for(char j=s[0];j<=s[3];j++)
             for(char i=s[1];i<=s[4];i++)
-                res.push_back(string(1, j) + i);
+                res.push_back({j,i});
         return res;
     }
 };
