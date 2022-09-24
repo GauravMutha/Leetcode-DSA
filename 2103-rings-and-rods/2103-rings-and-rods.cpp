f4feac2c5a683c616c8ebc7e93454d28d6efc390
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-24 18:15:16 +0530

    Time: 6 ms (16.17%), Space: 6.3 MB (79.98%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        vector<int>vec(10,0);
+        int rod=0;
+        for(int i=1;i<rings.size();i+=2){
+            rod=rings[i]-'0';
+            if(rings[i-1]=='R'){
+                vec[rod]|=1;
+            }
+            else if(rings[i-1]=='G'){
+                vec[rod]|=2;
+            }
+            else if(rings[i-1]=='B'){
+                vec[rod]|=4;
+            }
+        }
+        
+        return count(vec.begin(),vec.end(),7);
+    }
+};
