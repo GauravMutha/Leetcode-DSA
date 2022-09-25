9ee897ff43d9f8e924d9cf2f7f4adc79665ae9c5
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-25 11:27:56 +0530

    Time: 21 ms (54.20%), Space: 10.6 MB (89.86%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -3,11 +3,11 @@ public:
     string destCity(vector<vector<string>>& paths) {
         unordered_set<string> uset;
         
-        for(auto start : paths){
+        for(auto &start : paths){
             uset.insert(start[0]);
         }
         
-        for(auto end:paths){
+        for(auto &end:paths){
             if(!uset.count(end[1])) return end[1]; 
         }
         
