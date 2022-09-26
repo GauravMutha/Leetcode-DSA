dec90d6b877f1118ab2f22f918c9b39f09679e82
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-26 19:53:39 +0530

    Time: 6 ms (42.44%), Space: 6.8 MB (16.88%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -11,18 +11,17 @@ public:
         if(vec.size()==s.size()) return res;
         
         for(j=0; j<s.size() ; j++){
-            if(j==vec[r]){
+            if(r<vec.size() && j==vec[r]){
                 l=r;
                 r++;
             }
             
-            if(r==vec.size()) break;
-            
-            res[j]= min(abs(j-vec[l]) , abs(j-vec[r]));
+            if(r==vec.size())
+                res[j]=j-vec[l];
+            else
+                res[j]= min(abs(j-vec[l]) , abs(j-vec[r]));
+
         }
-        
-        for(;j<s.size();j++)
-            res[j]=abs(j-vec[l]);
         
         return res;
     }
