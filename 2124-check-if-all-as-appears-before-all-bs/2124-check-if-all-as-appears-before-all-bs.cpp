67b69cbe793021fa8868cedd0b43fa3fa8549ba6
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-26 17:46:25 +0530

    Time: 4 ms (36.91%), Space: 6.3 MB (11.06%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool checkString(string s) {
-        for(int i=0;i<s.size()-1;i++)
-            if(s[i]=='b' && s[i+1]=='a') return false;
-        
-        return true;
+        return is_sorted(s.begin(),s.end());
     }
 };
