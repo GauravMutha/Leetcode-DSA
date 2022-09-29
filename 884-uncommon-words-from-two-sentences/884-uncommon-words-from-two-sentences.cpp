6dbaae6893dfb01bb986d6c65772cb1c7f66dbe0
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-29 19:01:47 +0530

    Time: 5 ms (44.08%), Space: 6.9 MB (20.78%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
@@ -3,17 +3,12 @@ public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string,int>m1;
         vector<string> ans;
-        string res="";
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]!=' ') res+=s1[i];
-            if((i+1)==s1.size() || s1[i+1]==' ')
-                m1[res]++ , res="";
-        }
+        string res="",combined=s1+" "+s2;
         
-        for(int i=0;i<s2.size();i++){
-            if(s2[i]!=' ') res+=s2[i];
-            if((i+1)==s2.size() || s2[i+1]==' ')
-                m1[res]++,res="" ;
+        for(int i=0;i<combined.size();i++){
+            if(combined[i]!=' ') res+=combined[i];
+            if((i+1)==combined.size() || combined[i+1]==' ')
+                m1[res]++ , res="";
         }
         
         for(auto &[key,value] : m1)
