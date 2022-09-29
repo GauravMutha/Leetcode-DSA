66d48623a114ec671dcbe5e705142afe34712a56
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-09-30 00:24:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (58.92%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int convertTime(string current, string correct) {
         int minute= ( stoi(correct.substr(0,2)) - stoi(current.substr(0,2)) )*60 + stoi(correct.substr(3))-stoi(current.substr(3));
-        int x[4]={60,15,5,1};
-        int count=0,i=0;
-        while(minute){
-            count+=(minute/x[i]);
-            minute%=x[i];
-            i++;
+        int factor[4]={60,15,5,1},count=0;
+        for(int x : factor){
+            count+=(minute/x);
+            minute%=x;
         }
         
         return count;
