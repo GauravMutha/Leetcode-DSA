03c3f4da7236b472c964d8a0a6eb5e09f2c0930c
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-01 15:53:23 +0530

    Time: 90 ms (5.12%), Space: 16.1 MB (5.12%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.cpp
@@ -1,12 +1,57 @@
 class Solution {
 public:
+    
+    void lpsmaker(string patt , vector<int>& lps){
+        int n=patt.size(),len=0,i=1;
+        
+        while(i<n){
+            if(patt[i]==patt[len]){
+                lps[i]=len+1;
+                i++, len++;
+            }
+            else{
+                if(len!=0)
+                    len=lps[len-1];
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+    }
+    
+    bool kmp(string patt, string text){
+        int i=0,j=0,m=text.size(),n=patt.size();
+        vector<int>lps(n);
+        
+        lpsmaker(patt,lps);
+        
+        while(i<text.size()){
+            if(text[i]==patt[j])
+                i++, j++;
+            else{
+                if(j!=0)
+                    j=lps[j-1];
+                else
+                    i++;
+            }
+            if(j==n) return true;
+        }
+        return false;
+    }
+    
     vector<string> stringMatching(vector<string>& words) {
         vector<string>res;
-        for(int i=0;i<words.size();i++)
-            for(int j=0;j<words.size();j++)
-                if(words[j].find(words[i])!=string::npos && words[i]!=words[j])
-                    {res.push_back(words[i]); break;}
         
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words.size();j++){
+                if(words[i]==words[j]) continue;
+                if(kmp(words[i],words[j])){
+                    res.push_back(words[i]);
+                    break;
+                }
+            }
+        }
         return res;
     }
 };
