1b1960cafe38de4df5ad1fc55d7bdebde651bb24
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-01 20:09:43 +0530

    Time: 7 ms (48.61%), Space: 6.9 MB (26.67%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maxPower(string s) {
-        int l=0,r=0,res=1;
+        int count=1,res=1;
         
-        while(r<s.size()){
-            if(s[l]==s[r]){
-                res=max(r-l+1,res);
-                r++;
-            }
-            else
-                l=r;
+        for(int i=1;i<s.size();i++){
+            if(s[i]==s[i-1])
+                res=max(res,++count);
+            else 
+                count=1;
         }
+        
         return res;
     }
 };
