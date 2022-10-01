514cd201c2bd4e03992454c70ca3b9b647250786
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-01 21:10:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (65.49%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.cpp
+++ 917-reverse-only-letters/917-reverse-only-letters.cpp
@@ -3,8 +3,8 @@ public:
     string reverseOnlyLetters(string s) {
         int l=0,r=s.size()-1;
         while(l<r){
-            while((l<s.size()) &&(!isalpha(s[l])) ) l++;
-            while((r>=0) &&(!isalpha(s[r])) ) r--;
+            while(l<r &&(!isalpha(s[l])) ) l++;
+            while(l<r &&(!isalpha(s[r])) ) r--;
             
             if(l>=r) return s;
             swap(s[l++],s[r--]);
