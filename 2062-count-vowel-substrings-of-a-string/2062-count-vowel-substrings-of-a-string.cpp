cac4eade56e08244953f219d4bce61be512f7236
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-03 11:13:38 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (60.95%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -1,17 +1,33 @@
+//SLIDING WINDOW - ATMOST TRICK
+//TC--->O(N) SC---->O(1)
 class Solution {
 public:
-    int countVowelSubstrings(string word) {
-        int hash=0,count=0;
-        vector<char>vec={'a','e','i','o','u'};
-        for(int i=0;i<word.size()-1;i++){
-            if(find(vec.begin(),vec.end(),word[i])==vec.end()) continue;
-            hash=(1<<(word[i]-'a'));
-            for(int j=i+1;j<word.size();j++){
-                if(find(vec.begin(),vec.end(),word[j])==vec.end()) break;
-                hash|=(1<<(word[j]-'a'));
-                if(hash==1065233) count++; 
+    bool isvowel(char c){
+        return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+    }
+    int atmost(string s , int k){
+        int i=0,j=0,count=0;
+        unordered_map<char,int>freq;
+        
+        while(j<s.size()){
+            if(!isvowel(s[j])){
+                freq.clear();
+                j++;
+                i=j;
+                continue;
             }
+            freq[s[j++]]++;
+            while(freq.size()>k){
+                freq[s[i]]--;
+                if(freq[s[i]]==0) freq.erase(s[i]);
+                i++;
+            }
+            count+=(j-i+1);
         }
         return count;
+    }
+    
+    int countVowelSubstrings(string word) {
+       return (atmost(word,5)-atmost(word,4));
     }
 };
