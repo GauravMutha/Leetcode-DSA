09ef5c11d8cdd24e2cd3a7828f3a428230a2db79
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-03 22:54:45 +0530

    Time: 3 ms (59.61%), Space: 6.4 MB (59.03%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.cpp
@@ -4,10 +4,10 @@ public:
         vector<int>start(26,-1);
         vector<int>end(26,-1);
         int ans=-1;
-        for(int i=s.size()-1;i>=0;i--)
-            start[s[i]-'a']=i;
-        for(int i=0;i<s.size();i++)
+        for(int i=0,k=s.size()-i-1;k>=0;i++,k--){
+            start[s[k]-'a']=k;
             end[s[i]-'a']=i;
+        }
         for(int i=0;i<26;i++)
             if(start[i]!=end[i]) ans=max(ans,end[i]-start[i]-1);
         
