9fe66866ab2af0db0fdef99b77db97ee67271543
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-03 15:35:29 +0530

    Time: 2 ms (88.73%), Space: 6.3 MB (72.70%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -1,33 +1,25 @@
-//SLIDING WINDOW - ATMOST TRICK
-//TC--->O(N) SC---->O(1)
 class Solution {
 public:
-    bool isvowel(char c){
-        return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
-    }
-    int atmost(string s , int k){
-        int i=0,j=0,count=0;
-        unordered_map<char,int>freq;
-        
-        while(j<s.size()){
-            if(!isvowel(s[j])){
-                freq.clear();
-                j++;
-                i=j;
-                continue;
-            }
-            freq[s[j++]]++;
-            while(freq.size()>k){
-                freq[s[i]]--;
-                if(freq[s[i]]==0) freq.erase(s[i]);
-                i++;
-            }
-            count+=(j-i+1);
-        }
-        return count;
-    }
-    
     int countVowelSubstrings(string word) {
-       return (atmost(word,5)-atmost(word,4));
+        int i=0,j=0,k=0,vow=0,res=0;
+        unordered_map<char,int>m={{'a',0},{'e',0},{'i',0},{'o',0},{'u',0}};
+        for(int i=0;i<word.size();i++){
+            if(m.count(word[i])){
+                m[word[i]]++;
+                vow+= (m[word[i]]==1);
+                
+                while(vow==5){
+                    m[word[k]]--;
+                    vow-=(m[word[k]]==0);
+                    k++;
+                }
+                res+=(k-j);
+            }
+            else{
+                m['a'] = m['e'] = m['i'] = m['o'] = m['u'] = vow = 0;
+                j = k = i + 1;
+            }
+        }
+        return res;
     }
 };
