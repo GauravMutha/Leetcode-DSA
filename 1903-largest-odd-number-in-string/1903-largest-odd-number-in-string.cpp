692009e46c431d0161e54ee72042d2a3f9767c56
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-05 19:45:45 +0530

    Time: 57 ms (37.40%), Space: 14.5 MB (84.18%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,8 +4,8 @@ public:
         if((num.back()-'0')%2) return num;
         int i=0;
         for(i=num.size()-1;i>=0;i--)
-            if((num[i]-'0')%2) break;
+            if((num[i]-'0')%2) return num.substr(0,i+1);
         
-        return (i<0) ? "" : num.substr(0,i+1);
+        return "";
     }
 };
