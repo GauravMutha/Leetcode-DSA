0ea29a150b706e7b180e839f98cd6202a07f4a84
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-06 18:33:22 +0530

    Time: 136 ms (70.71%), Space: 36.9 MB (61.58%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        int minsum=0;
+        int minsum=INT_MAX;
         unordered_map<string,int>m;
-        map<int,vector<string>>res;
+        vector<string>res;
         
         for(int i=0;i<list1.size();i++)
-            m[list1[i]]=i;
-        for(int i=0;i<list2.size();i++)
-            if(m.find(list2[i])!=m.end()) res[i+m[list2[i]]].push_back(list2[i]);
+            m[list1[i]]=i+1;
+        for(int i=0;i<list2.size();i++){
+            if(m.find(list2[i])!=m.end()){
+                m[list2[i]]+=i+1;
+                minsum=min(m[list2[i]],minsum);
+            }
+        }
         
-        return res.begin()->second;
+        for(int i=0;i<list1.size();i++)
+            if(m[list1[i]]>i+1 && m[list1[i]]==minsum) res.push_back(list1[i]);
+        
+        return res;
     }
 };
 
-//Another verson of hash set.
+//This solution removes the clearing and filling of vector again and again
+//without introducing another map
