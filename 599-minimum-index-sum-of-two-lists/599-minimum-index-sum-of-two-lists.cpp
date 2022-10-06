69f28401a6a4cce6a7dc643ef00c31121f8ba1cb
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-06 18:05:32 +0530

    Time: 224 ms (33.85%), Space: 39.1 MB (25.59%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        vector<string>res;
-        int minsum=INT_MAX;
+        int minsum=0;
         unordered_map<string,int>m;
+        map<int,vector<string>>res;
         
         for(int i=0;i<list1.size();i++)
             m[list1[i]]=i;
+        for(int i=0;i<list2.size();i++)
+            if(m.find(list2[i])!=m.end()) res[i+m[list2[i]]].push_back(list2[i]);
         
-        for(int i=0;i<list2.size();i++){
-            if(m.find(list2[i])==m.end()) continue;
-            if(m[list2[i]]+i<minsum){
-                res.clear();
-                res.push_back(list2[i]);
-                minsum=i+m[list2[i]];
-            }
-            else if(m[list2[i]]+i==minsum)
-                res.push_back(list2[i]);
-        }
-        return res;
+        return res.begin()->second;
     }
 };
 
-//Worst case TC--> O(nlogn + m^2)
-//SC--> O(n)
+//Another verson of hash set.
