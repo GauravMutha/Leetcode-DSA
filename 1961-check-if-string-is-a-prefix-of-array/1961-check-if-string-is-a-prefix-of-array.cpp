2f495f7ff72dbde8aed4935eeaef0daaa05378e3
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-06 12:32:51 +0530

    Time: 12 ms (37.44%), Space: 14.1 MB (36.69%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
@@ -5,6 +5,7 @@ public:
         for(auto str : words){
             res+=(str);
             if(res==s) return true;
+            if(res.size()>s.size()) return false;
         }
         return false;
     }
