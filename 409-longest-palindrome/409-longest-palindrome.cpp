5959c4e8a098f4d5b166017561ecb6d302738aa7
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-06 11:58:03 +0530

    Time: 3 ms (84.13%), Space: 6.5 MB (95.29%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char,int>m;
-        int res=0,flago=0;
-        for(auto c:s)
-            m[c]++;
-        for(auto &[key,value] : m){
-            if(value%2==1)
-                res+=(value-1),flago=1;
-            else
-                res+=value;
-        }
-        return (res+flago);
+        int odds=0;
+        for(char c ='A';c<='z';c++)
+            odds+=count(begin(s),end(s),c) & 1;
+        return s.size()-odds + (odds>0);
     }
 };
+
+//FD
+//Better Code , O(128n) ~ O(n) TC , SC is O(1) and this is the improvement.
