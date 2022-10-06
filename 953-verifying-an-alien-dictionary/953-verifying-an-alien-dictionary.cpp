c05905aa9ca2b279a6e5aa4dbe9c53029ee71602
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-06 19:55:08 +0530

    Time: 11 ms (44.88%), Space: 9.6 MB (42.84%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    bool issorted(string s1,string s2,unordered_map<char,int>m){
+    unordered_map<char,int>m;
+    bool issorted(string s1,string s2){
         int i=0;
-        for(i=0;i<s1.size() && i<s2.size();i++){
-            if(m[s1[i]]==m[s2[i]]) continue;
-            else if(m[s1[i]]<m[s2[i]]) return true;
-            else return false;
-        }
+        for(i=0;i<s1.size() && i<s2.size();i++)
+            if(m[s1[i]]!=m[s2[i]]) return m[s1[i]]<m[s2[i]];
         
-        return (i<s1.size() && i>=s2.size()) ? false : true;
+        return (s1.size()<=s2.size());
     }
     
     bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char,int>m;
         for(int i=0;i<order.size();i++)
             m[order[i]]=i;
         for(int i=0;i<words.size()-1;i++)
-            if(!issorted(words[i],words[i+1],m)) return false;
+            if(!issorted(words[i],words[i+1])) return false;
         return true;
     }
 };
+
+//TC -->O(nlogn)
