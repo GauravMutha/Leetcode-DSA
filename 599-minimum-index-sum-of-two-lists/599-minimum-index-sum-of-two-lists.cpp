85dbe100d9a4d818ed2c851fe69b0f0479daad69
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-06 17:35:44 +0530

    Time: 184 ms (50.09%), Space: 37 MB (40.32%) - LeetHub

--- 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        int minsum=INT_MAX;
         vector<string>res;
-        for(int i=0;i<list1.size();i++){
-            for(int j=0;j<list2.size();j++){
-                if(list1[i]==list2[j] && i+j<minsum){
-                    res.clear();
-                    res.push_back(list1[i]);
-                    minsum=i+j;
-                }
-                else if(list1[i]==list2[j] && i+j==minsum){
-                    res.push_back(list1[i]);
-                }
+        int minsum=INT_MAX;
+        unordered_map<string,int>m;
+        
+        for(int i=0;i<list1.size();i++)
+            m[list1[i]]=i;
+        
+        for(int i=0;i<list2.size();i++){
+            if(m.find(list2[i])==m.end()) continue;
+            if(m[list2[i]]+i<minsum){
+                res.clear();
+                res.push_back(list2[i]);
+                minsum=i+m[list2[i]];
             }
+            else if(m[list2[i]]+i==minsum)
+                res.push_back(list2[i]);
         }
         return res;
     }
 };
 
-//brute force
+//Worst case TC--> O(nlogn + m^2)
+//SC--> O(n)
