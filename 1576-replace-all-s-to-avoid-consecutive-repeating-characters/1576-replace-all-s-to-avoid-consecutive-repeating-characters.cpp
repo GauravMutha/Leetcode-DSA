d96dbb9d016404ebe94f15061ce32b830b0fb26f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-07 18:22:58 +0530

    Time: 0 ms (100.00%), Space: 6 MB (87.56%) - LeetHub

--- 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
-    char change(string& s , int i){
-        char c;
-        for(c='a';c<'z';c++){
-            if(c!=s[i-1] && c!=s[i+1]){
-                break;
-            }
-        }
-        return c;
-    }
     string modifyString(string s) {
         for(int i=0;i<s.size();i++){
-            if(isalpha(s[i])) continue;
-            
-            if(i+1==s.size() || s[i+1]=='?')
-                s[i]=(i-1>=0) ? change(s,i) : 'a';
-            else if(isalpha(s[i+1])){
-                if(i-1>=0){
-                    s[i]=change(s,i);
-                }
-                else 
-                    s[i]=(s[i+1]=='z') ? 'a' : s[i+1]+1;
+            if(s[i]=='?'){
+                for(s[i]='a';s[i]<='c';s[i]++)
+                    if((i==0 || s[i-1]!=s[i]) && (i==s.size()-1 || s[i]!=s[i+1]))
+                        break;
             }
         }
         return s;
