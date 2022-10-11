7efecb6a21a0c77fc2cfc89cdc1891f00eb4987a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-11 12:18:00 +0530

    Time: 3 ms (91.29%), Space: 6.6 MB (89.07%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int r=0,l=0;
-        vector<int>res;
-        unordered_map<char,int>m;
+        int pre_r=-1,r=0;
+        vector<int>res,m(26,0);
         for(int i=0;i<s.size();i++){
-            m[s[i]]=i;
+            m[s[i]-'a']=i;
         }
         
-        r=m[s[l]];
         for(int i=0;i<s.size();i++){
+            r=max(m[s[i]-'a'],r);
             if(i==r){
-                res.push_back(r-l+1);
-                l=i+1;
-                r=m[s[l]];
-            }
-            else if(m[s[i]]>r){
-                r=m[s[i]];
+                res.push_back(r-pre_r);
+                pre_r=r;
             }
         }
         return res;
     }
 };
+
+//using hash array of size 26 instead of map
