903f49d71812b1b586b1f1eebb7004ea2b7f7091
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-15 11:18:56 +0530

    Time: 242 ms (42.61%), Space: 16.4 MB (35.09%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -5,16 +5,17 @@ public:
         return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
     }
     int findTheLongestSubstring(string s) {
-        vector<int>freq(32,-1);
+        unordered_map<int,int>bitmap;\
+            bitmap[0]=-1;
         int currMask=0,maxi=0;
         
         for(int i=0;i<s.size();i++){
             if(isvowel(s[i])){
                 currMask^=charMap[s[i]-'a'];
             }
-            if(currMask!=0 && freq[currMask]==-1) freq[currMask]=i;
+            if(bitmap.count(currMask)==0) bitmap[currMask]=i;
             
-            maxi=max(maxi,i-freq[currMask]);
+            maxi=max(maxi,i-bitmap[currMask]);
         }
         return maxi;
     }
