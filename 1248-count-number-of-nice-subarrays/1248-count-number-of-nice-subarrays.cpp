016ea7847434b8f1dc2bf79fc4232505d2f126b1
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-18 18:12:00 +0530

    Time: 286 ms (61.99%), Space: 73.8 MB (41.98%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        int l=0,r=0,odd=0,res=0,count=0;
-        for(r=0;r<nums.size();r++){
-            if(nums[r]%2){
-                odd++;
-                if(odd>=k){
-                    count=1;
-                    while((nums[l++]%2)==0) count++;
-                    res+=count;
-                }
+    int atmost(vector<int>nums,int k){
+        int count=0,j=0,i=0,res=0;
+        for(j=0;j<nums.size();j++){
+            if(nums[j]%2) count++;
+            while(count>k){
+                if(nums[i++]%2) count--;
             }
-            else if(odd>=k) res+=count;
+            res+=(j-i+1);
         }
         return res;
     }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return (atmost(nums,k)-atmost(nums,k-1));
+    }
 };
-
-//     int numberOfSubarrays(vector<int>& nums, int k) {
-//         int j = 0, odd = 0, count = 0, total = 0;
-//         for (int i = 0; i < nums.size(); i++) {
-//             if (nums[i] & 1) {
-//                 odd++;
-//                 if (odd >= k) {
-//                     count = 1;
-//                     while (!(nums[j++] & 1)) count++;
-//                     total += count;
-//                 }
-//             } else if (odd >= k) total += count;
-//         }
-//         return total;
-//     }
-// };
