184e0d6bdd25945e9eb43553830685a07f41d283
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-21 12:37:53 +0530

    Time: 212 ms (51.02%), Space: 21 MB (46.19%) - LeetHub

--- 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -3,13 +3,12 @@ public:
     int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
         int res=0;
         unordered_map<string,int>freq;
-        for(int k=minSize;k<=maxSize;k++){
-            unordered_map<int,int>m;
+          unordered_map<char,int>m;
             for(int l=0,r=0;r<s.size();r++){
                 m[s[r]]++;
-                if(r-l+1==k){
+                if(r-l+1==minSize){
                     if(m.size()<=maxLetters){
-                        string temp=s.substr(l,k);
+                        string temp=s.substr(l,minSize);
                         freq[temp]++;
                     }
                     m[s[l]]--;
@@ -17,9 +16,10 @@ public:
                     l++;
                 }
             }
-        }
         for (auto it = freq.begin(); it != freq.end(); it++)
             if(it->second>res) res=it->second;
         return res;
     }
 };
+
+//sliding window but uses array
