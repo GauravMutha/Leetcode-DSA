4401a42f7094f013ff89b2642ab6e150bd4ac70b
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-21 20:09:33 +0530

    Time: 25 ms (44.76%), Space: 7 MB (85.01%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res=0,maxFreq=0,l=0,r=0;
+        vector<int>hash(26,0);
+        for(r=0;r<s.size();r++){
+            hash[s[r]-'A']++;
+            maxFreq=max(maxFreq,hash[s[r]-'A']);
+            
+            if(r-l+1 -maxFreq>k){
+                res=max(res,(r-1)-l+1);
+                hash[s[l]-'A']--;
+                l++;
+                maxFreq=*max_element(begin(hash),end(hash)); //*
+            }
+        }
+        return max(res,(r-1)-l+1);
+    }
+};
+
+//* It updates the maxFrequency every time the left pointer is shifted
