0b467a6d53c591cf185629f0cb22b145e8f8684f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-22 20:38:16 +0530

    Time: 61 ms (66.38%), Space: 7.8 MB (85.75%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    int minWindowSubstr(string s,string str){
-        vector<int>hash(26,0);
-        int r=0,l=0,res=INT_MAX,n=str.size();
-        for(auto c:str) hash[c-'A']++;
-        
-        while(r<s.size()){
-            hash[s[r]-'A']--;
-            if(hash[s[r]-'A']>=0) n--;
-            r++;
-            while(l<r && n==0){
-                res=min(res,r-l);
-                hash[s[l]-'A']++;
-                if(hash[s[l]-'A']>0) n++;
+    int balancedString(string s) {
+        unordered_map<char,int>count;
+        int r=0,l=0,res=INT_MAX,n=s.size(),m=n/4;
+        for(auto c:s) count[c]++;
+        while(r<n){
+            count[s[r]]--;
+            while(l<n && count['Q']<=m && count['W']<=m && count['E']<=m && count['R']<=m){
+                if(l>r) return 0;
+                count[s[l]]++;
+                res=min(res,r-l+1);
                 l++;
             }
+            r++;
         }
         return res;
-    }
-    int balancedString(string s) {
-        unordered_map<char,int>freq;
-        int len=s.size()/4;
-        for(auto c :s) freq[c]++;
-        
-        string str="";
-        for(auto it : freq){
-            if(it.second>len) 
-                str+=(string(it.second-len,it.first));
-        }
-        if(str.size()==0) return 0;
-        return minWindowSubstr(s,str);
     }
 };
