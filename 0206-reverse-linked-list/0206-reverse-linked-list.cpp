31c8f27453dc7cd7234130ce3f13bbf67535c48c
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-26 19:25:59 +0530

    Time: 18 ms (13.44%), Space: 8.6 MB (9.66%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,29 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// *
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        struct ListNode *p,*q{NULL},*r{NULL};
-        p=head;
-        
-        while(p!=NULL){
-            r=q;
-            q=p;
-            p=p->next;
-            
-            q->next=r;
+    void recReverse(ListNode* q,ListNode* p,ListNode* &j){
+        if(p!=NULL){
+            recReverse(p,p->next,j);
+            p->next=q;
         }
-        head=q;
-        return q;
+        else {
+            j=q;
+        }
+    }
+    ListNode* reverseList(ListNode* head) {
+        struct ListNode* q=NULL,*j=NULL;
+        recReverse(q,head,j);
+        head=j;
+        return head;
     }
 };
 
-//Method 1 -- constant SC  and it is one pass solution of O(n) TC 
+//Method 2 -- constant SC  and it is one pass solution of O(n) TC recusrive
