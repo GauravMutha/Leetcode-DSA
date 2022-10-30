7ee8a2b66decd5100c9fd1bde33c2e07f051f212
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-10-30 16:19:47 +0530

    Time: 14 ms (54.94%), Space: 14.7 MB (97.74%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,37 +10,21 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        struct ListNode *first=list1,*second=list2,*list3=NULL,*last=NULL;
-        if(list1 ==NULL && list2==NULL) return NULL;
-        else if(list1==NULL) return list2;
-        else if (list2==NULL) return list1;
-        if(list1->val < list2->val){
-            list3=last=first;
-            first=first->next;
-        }
-        else{
-            list3=last=second;
-            second=second->next;
-        }
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
         
-        while(first && second){
-            if(first->val < second->val){
-                last->next=first;
-                last=first;
-                first=first->next;
-            }
-            else {
-                last->next=second;
-                last=second;
-                second=second->next;
-            }            
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
         }
-        if(first!=NULL) last->next=first;
-        else if(second!=NULL) last->next=second;
-        
-        return list3;
+        else {
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
+        }
     }
 };
 
-//iterative
+//recursive
