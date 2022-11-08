80c640c6de459e5c4e1d3ab9c0b8d66b8c896948
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-08 11:51:57 +0530

    Time: 9 ms (45.41%), Space: 10.6 MB (74.94%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,18 +11,16 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        struct ListNode dummy(0,head), *p=head;
-        int len=0;
-        while(p){
-            len++;
-            p=p->next;
+        struct ListNode dummy(0,head), *f , *b;
+        f=b=&dummy;
+        
+        while(f && f->next){
+            if(n-- > 0) f=f->next;
+            else f=f->next , b=b->next;
         }
-        p=&dummy;
-        for(int i=1;i<=len-n;i++)
-            p=p->next;
-        p->next=p->next->next;
+        b->next=b->next->next;
         return dummy.next;
     }
 };
 
-//same as previous but static memory for dummy node
+//Two pointers + one pass solution
