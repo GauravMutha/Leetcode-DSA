5ec6f8c99c589cc989705ab40a6a0fb671141d1e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-08 17:01:05 +0530

    Time: 75 ms (33.23%), Space: 71.6 MB (11.94%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,18 +11,14 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        struct ListNode *ptr1=l1 , *ptr2=l2 , dummy(-1),*curr;
-        curr=&dummy;
-        int val1=0 , val2=0 , val3=0,carry=0;
-        while(ptr1 || ptr2 || carry){
-            val1=0 , val2=0;
-            if(ptr1) val1=ptr1->val , ptr1=ptr1->next;
-            if(ptr2) val2=ptr2->val , ptr2=ptr2->next;
-            val3=( (val1+val2 + carry) <10 ? val1+val2+carry : (val1+val2+carry)%10 ); 
-            curr->next=new ListNode(val3);
-            curr=curr->next;
-            carry=(val1+val2+carry)/10;
-        }
-        return dummy.next;
+        return recurser(l1,l2,0);
+    }
+    ListNode* recurser(ListNode *l1 , ListNode *l2 , int carry){
+        if(!l1 && !l2 && !carry) return NULL;
+        
+        int val3=(l1? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+        struct ListNode *curr = new ListNode(val3%10);
+        curr->next=recurser(l1 ? l1->next : NULL,l2 ? l2->next:NULL,val3/10);
+        return curr;
     }
 };
