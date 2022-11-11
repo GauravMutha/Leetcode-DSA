77b7f275201785169c95f19754d769d5ce753fc6
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-11 21:37:56 +0530

    Time: 11 ms (45.88%), Space: 10 MB (52.01%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -3,32 +3,18 @@ public:
     vector<int> finalPrices(vector<int>& vec) {
         vector<int>ans(vec.size(),-1);
         stack<int>st;
+        
         for(int i=vec.size()-1;i>=0;i--){
+            while(!st.empty() && st.top()>vec[i]) st.pop();
             if(st.empty()){
                 ans[i]=vec[i];
-                st.push(vec[i]);
             }
             else{
-                if(st.top()<vec[i]){
-                    ans[i]=vec[i]-st.top();
-                    st.push(vec[i]);
-                }
-                else if(st.top()>vec[i]){
-                    while(!st.empty() && st.top()>vec[i]) st.pop();
-                    if(st.empty()){
-                        ans[i]=vec[i];
-                        st.push(vec[i]);
-                    }
-                    else if(st.top()<vec[i]){
-                        ans[i]=vec[i]-st.top();
-                        st.push(vec[i]);
-                    }
-                    else if(st.top()==vec[i]) ans[i]=vec[i]-st.top();
-                }
-                else  ans[i]=vec[i]-st.top();
+                ans[i]=vec[i]-st.top();
             }
+            if(st.empty() || st.top()!=vec[i]) st.push(vec[i]);
         }
         return ans;
     }
 };
-//Redundant code
+//Refactoring previous code
