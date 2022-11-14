fcd6c521b281d87b64491dd6369e17349fbeb913
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-14 20:23:32 +0530

    Time: 69 ms (43.25%), Space: 9.7 MB (89.49%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     string removeDuplicates(string s, int k) {
         string res="";
-        stack<char>st;
         stack<int>cnt;
-        for(int i=s.size()-1;i>=0;i--){
-            if(st.empty() || st.top()!=s[i]){
-                st.push(s[i]);
+        for(int i=0;i<s.size();i++){
+            if(res.size()==0 || res.back()!=s[i]){
+                res.push_back(s[i]);
                 cnt.push(1);
             }
             else{
-                st.push(s[i]);
+                res.push_back(s[i]);
                 cnt.top()++;
                 if(cnt.top()==k){
                     int n=k;
-                    while(n--) st.pop();
+                    while(n--) res.pop_back();
                     cnt.pop();
                 }
             }
         }
-        while(!st.empty()) res+=st.top() , st.pop();
         return res;
     }
 };
 
-//2 pass + Using two Stacks
+//1 pass + 1 count Stack
 //TC==O(n*k)
-//SC-->O(1)
+//SC-->O(n) worst case
