f3889d2d420f4ffb1a90cec40f321991ab69f8ae
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-16 17:14:18 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (77.79%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -3,19 +3,18 @@ public:
     string reverseParentheses(string s) {
         stack<int>st;
         string res;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(') st.push(i);
-            else if(s[i]==')'){
-                reverse(s.begin()+st.top()+1,s.begin()+i);
+        for(auto c:s){
+            if(c=='(') st.push(res.length());
+            else if(c==')'){
+                reverse(res.begin()+st.top(),res.end());
                 st.pop();
             }
+            else res+=c;
         }
-        for(int i=0;i<s.size();i++)
-            if(isalpha(s[i])) res+=s[i];
         return res;
     }
 };
-
-//Brute Force , 2 pass
+//MORE CONCISE Brute Force
+//1 pass
 //TC-->O(n^2)
 //SC-->O(n)
