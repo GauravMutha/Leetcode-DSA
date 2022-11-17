819616d2e816515ea3192a9ce2567c0747e4d50a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-17 22:26:22 +0530

    Time: 41 ms (72.28%), Space: 11.5 MB (26.82%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long sum=0 , maxi=INT_MIN,mini=INT_MAX;
-        for(int i=0;i<nums.size();i++){
-            maxi=INT_MIN , mini=INT_MAX;
-            for(int j=i;j<nums.size();j++){
-                mini=min(mini,(long long)nums[j]);
-                maxi=max(maxi,(long long)nums[j]);
-                sum+=maxi-mini;
-            }
+        long long sum=0; 
+        int n=nums.size();
+        stack<int>st;
+        vector<int> sl(n,-1) , sr(n,n), gl(n,-1) , gr(n,n);
+        
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]>=nums[i]) st.pop();
+            if(!st.empty()) sl[i]=st.top();
+            st.push(i);
+        } 
+        while(!st.empty()) st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>nums[i]) st.pop();
+            if(!st.empty()) sr[i]=st.top();
+            st.push(i);
+        }        
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<=nums[i]) st.pop();
+            if(!st.empty()) gl[i]=st.top();
+            st.push(i);
+        } 
+        while(!st.empty()) st.pop();        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<nums[i]) st.pop();
+            if(!st.empty()) gr[i]=st.top();
+            st.push(i);
+        }
+        
+        for(int i=0;i<n;i++){
+            long long leftMin=i-sl[i] , rightMin=sr[i]-i;
+            long long leftMax=i-gl[i] , rightMax=gr[i]-i;
+            sum+=((leftMax*rightMax)-(leftMin*rightMin))*nums[i];
         }
         return sum;
     }
 };
-
-//O(n^2) Brute force
+//5 pass
+//TC-->O(n)
+//SC-->O(1)
