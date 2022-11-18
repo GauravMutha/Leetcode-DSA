d5b58509161392e945ff377ca51fdb21692fe3d3
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-18 22:41:55 +0530

    Time: 36 ms (94.70%), Space: 73.2 MB (25.83%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -11,24 +11,33 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        stack<ListNode*>st1 ,st2;
-        struct ListNode *l3=NULL;
-        while(l1 || l2){
-            if(l1) st1.push(l1), l1=l1->next;
-            if(l2) st2.push(l2), l2=l2->next;
+        stack<struct ListNode*>st1 ,st2;
+        struct ListNode *l3=NULL , *p=l1,*q=l2 , *dig1,*dig2;
+        while(p || q){
+            if(p) st1.push(p), p=p->next;
+            if(q) st2.push(q), q=q->next;
         }
         //imported from question 2.Add two numbers 
-        
+        bool flag1=(st1.size()>=st2.size());
+        l3=(flag1) ? l1 :l2;
         int val1=0 , val2=0 , val3=0,carry=0;
         while(!st1.empty() || !st2.empty() || carry){
             val1=0 , val2=0;
-            if(!st1.empty()) val1=st1.top()->val, st1.pop();
-            if(!st2.empty()) val2=st2.top()->val , st2.pop();
+            if(!st1.empty()) val1=st1.top()->val;
+            if(!st2.empty()) val2=st2.top()->val;
             val3=( (val1+val2 + carry) <10 ? val1+val2+carry : (val1+val2+carry)%10 ); 
             
-            struct ListNode* temp=new ListNode(val3);
-            temp->next=l3;
-            l3=temp;
+            if(st1.empty() && st2.empty()){
+                struct ListNode* temp=new ListNode(val3);
+                temp->next=l3;
+                l3=temp;
+            }
+            else{
+               if(flag1) st1.top()->val=val3;
+               else st2.top()->val=val3;
+            }
+            if(!st1.empty()) st1.pop();
+            if(!st2.empty()) st2.pop();
             carry=(val1+val2+carry)/10;
         }
         return l3;
