cc473a2579577ddedb1361d2ae4d7fe32f2fa9a2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-18 19:35:52 +0530

    Time: 78 ms (31.20%), Space: 73.8 MB (9.37%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,44 +10,31 @@
  */
 class Solution {
 public:
-    //imported from question 206.Reverse the linked list submission #2
-    ListNode* reverseList(ListNode** head) {
-        struct ListNode *p,*q{NULL},*r{NULL};
-        p=*head;
-        
-        while(p!=NULL){
-            r=q;
-            q=p;
-            p=p->next;
-            
-            q->next=r;
-        }
-        *head=q;
-        return q;
-    }    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        struct ListNode *head=NULL;
-        reverseList(&l1);
-        reverseList(&l2);
-        
+        stack<ListNode*>st1 ,st2;
+        struct ListNode *l3=NULL;
+        while(l1 || l2){
+            if(l1) st1.push(l1), l1=l1->next;
+            if(l2) st2.push(l2), l2=l2->next;
+        }
         //imported from question 2.Add two numbers 
+        
         int val1=0 , val2=0 , val3=0,carry=0;
-        while(l1 || l2 || carry){
+        while(!st1.empty() || !st2.empty() || carry){
             val1=0 , val2=0;
-            if(l1) val1=l1->val , l1=l1->next;
-            if(l2) val2=l2->val , l2=l2->next;
+            if(!st1.empty()) val1=st1.top()->val, st1.pop();
+            if(!st2.empty()) val2=st2.top()->val , st2.pop();
             val3=( (val1+val2 + carry) <10 ? val1+val2+carry : (val1+val2+carry)%10 ); 
             
-            struct ListNode *temp=new ListNode(val3);
-            temp->next=head;
-            head=temp;
-            
+            struct ListNode* temp=new ListNode(val3);
+            temp->next=l3;
+            l3=temp;
             carry=(val1+val2+carry)/10;
         }
-        return head;
+        return l3;
     }
 };
-//Reversing LL1 and LL2
-//3 pass basically
-//SC-->O(1)
+//Without Reversing -- > Uses 2 stack
+//2 pass
+//SC-->O(n)
 //TC--> O(n)
