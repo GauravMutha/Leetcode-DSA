ec43d160b1cfd7181958f64e8d1f39e1cdd4399e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-22 15:18:45 +0530

    Time: 42 ms (95.23%), Space: 17.7 MB (95.50%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -12,22 +12,28 @@ class Solution {
 public:
     void reorderList(ListNode* head) {
         if(head==NULL || head->next==NULL) return;
-        struct ListNode *p=head ,*q=head;
-        stack<ListNode*>st;
-        while(p){
-            st.push(p);
-            p=p->next;
+        struct ListNode *f=head ,*s=head;
+        while(f->next && f->next->next){
+            s=s->next;
+            f=f->next->next;
         }
-        int sz=st.size();
-        for(int i=0;i<sz/2;i++){
-            ListNode* twin=st.top();
-            st.pop();
-            twin->next=q->next;
-            q->next=twin;
-            q=q->next->next;
+        struct ListNode *preMid=s,*tail=s->next;
+        while(tail->next){
+            ListNode* temp=preMid->next;
+            preMid->next=tail->next;
+            tail->next=tail->next->next;
+            preMid->next->next=temp;
         }
-        q->next=NULL;
+        
+        struct ListNode *twin=preMid->next,*left=head;
+        while(left!=preMid){
+            preMid->next=twin->next;
+            twin->next=left->next;
+            left->next=twin;
+            left=twin->next;
+            twin=preMid->next;
+        }
     }
 };
-//SC-->O(n) Uses Stack
+//SC-->O(1) Without Stack
 //TC-->O(n)
