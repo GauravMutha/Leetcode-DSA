63c957ef82501a93b2e5d81d799af53a2c035423
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-27 23:26:55 +0530

    Time: 38 ms (21.48%), Space: 8.9 MB (24.72%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
-    int calculate(string s) {
-        int curr=0,intRes=0,ans=0;
-        char prevOp='+';
-        s+="##";
+    int pre(char c){
+        if(c=='*' || c=='/') return 1;
+        return 0;
+    }
+    string toPostFix(string s){
+        stack<char>st;
+        string post;
         for(auto c : s){
             if(c==' ') continue;
-            else if(isdigit(c)) curr=curr*10 + (c-'0');
+            else if(isdigit(c)) post+=c;
             else{
-                if(prevOp=='*') intRes*=curr;
-                else if(prevOp=='/') intRes/=curr;
-                else ans+=intRes , intRes=(prevOp=='+')?curr:-curr;
-                
-                prevOp=c;
-                curr=0;
+                post+='|';
+                while(!st.empty() && pre(c)<=pre(st.top()))
+                    post+=st.top() , st.pop();
+                st.push(c);
+            }
+            
+        }
+        post+='|';
+        while(!st.empty()) post+=st.top() , st.pop();
+        return post;
+    }
+    int calculate(string s) {
+        s=toPostFix(s);
+        stack<int>st;
+        int n1=0,n2=0;
+        for(int i=0;i<size(s);i++){
+            if(isdigit(s[i])){
+                int curr=0;
+                while(i<size(s) && isdigit(s[i]))
+                    curr=curr*10 + (s[i++]-'0');
+                st.push(curr);                
+            }
+            else{
+                n1=st.top() ,st.pop();
+                n2=st.top(),st.pop();
+                if(s[i]=='+') st.push(n2+n1);
+                else if(s[i]=='-') st.push(n2-n1);
+                else if(s[i]=='*') st.push(n2*n1);
+                else if(s[i]=='/') st.push(n2/n1);
             }
         }
-        return ans;
+        return st.top();
     }
 };
-
-//Without Stack
-//SC-->O(1)
-//TC-->O(n)
+//Using Stack
+//SC-->O(N)
+//TC-->O(N)
