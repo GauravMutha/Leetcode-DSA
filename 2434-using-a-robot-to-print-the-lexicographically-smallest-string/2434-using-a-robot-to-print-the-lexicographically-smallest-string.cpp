fffdb002a8949b11d72294566067a5d18e891c50
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-11-29 17:28:12 +0530

    Time: 371 ms (45.78%), Space: 29.7 MB (68.24%) - LeetHub

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    char checkSmaller(vector<int>freq){
+    char checkSmaller(vector<int> &freq){
         for(int i=0;i<26;i++)
             if(freq[i]!=0) return 'a'+i;
         return 'a';
     }
     string robotWithString(string s) {
-        string ans,robo;
+        string ans;
+        stack<char>robo;
         vector<int>freq(26,0);
         for(auto c : s){
             freq[c-'a']++;
         }
         for(auto c:s){
-            robo.push_back(c);
+            robo.push(c);
             freq[c-'a']--;
-            while(robo.size() && robo.back()<=checkSmaller(freq)){
-                char x=robo.back();
-                robo.pop_back();
+            while(!robo.empty() && robo.top()<=checkSmaller(freq)){
+                char x=robo.top();
+                robo.pop();
                 ans.push_back(x);
             }
         }
-        reverse(begin(robo),end(robo));
-        ans.append(robo);
+        while(!robo.empty()){
+            char x=robo.top();
+            robo.pop();
+            ans.push_back(x);
+        }
         return ans;
-        return ""; 
     }
 };
-
+//Just like previous solution but it uses stack robo instead of string robo
 //sc-->O(N)
 //tc-->O(N)
