b76e62d3804589c7c8717168fccdc33864e22722
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-12-02 16:40:23 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (55.26%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int countStudents(vector<int>& stu, vector<int>& sand) {
-        queue<int>q;
-        for(auto elem : stu) q.push(elem);
-        int k=0,count=0;
-        while(k<sand.size()){
-            int val=q.front();
-            q.pop();
-            if(sand[k]!=val) count++, q.push(val);
-            else k++,count=0;
-            
-            if(count==q.size()) break;
+        int front =0, ind=0 ,end=0 ,len=sand.size(), res=len;
+        while(res){
+            if(stu[front]==sand[ind]){
+                res--;
+                stu[front]=-1;
+                front=(front+1)%len;
+                ind++;
+            }
+            else{
+                end=front;
+                front=(front+1)%len;
+                while(stu[front]!=sand[ind] && front!=end) front=(front+1)%len;
+                if(front==end) break;
+            }
         }
-        return q.size();
+        return res;
     }
 };
 
-//Using a queue
-//SC-->O(n)
+//Using two pointer
+//No Queue
+//SC-->O(1)
 //TC-->O(n)
