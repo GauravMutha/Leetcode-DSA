68f84af6f5ece876835804c9a7520d37acab84b8
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-12-24 22:14:27 +0530

    Time: 15 ms (94.74%), Space: 21.8 MB (19.55%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -9,31 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//preorder
+
+//More concise preorder than previous one
 class Solution {
 public:
-    void helper(TreeNode* curr, TreeNode* parent , int target){
-        if(curr==NULL) return;
-        if(curr->left==NULL && curr->right==NULL && curr->val==target){
-            if(parent->left==curr) parent->left=NULL;
-            else if(parent->right==curr) parent->right=NULL;
-            
-            return;
-        }
-        helper(curr->left,curr,target);
-        helper(curr->right,curr,target);
-        if(curr->left==NULL && curr->right==NULL && curr->val==target){
-            if (parent==NULL) return;
-            if(parent->left==curr) parent->left=NULL;
-            else if(parent->right==curr) parent->right=NULL;
-            
-            return;
-        }
-        
-    }
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        helper(root,NULL,target);
-        if(root->val==target && root->left==NULL && root->right==NULL) return NULL;
+        if(root==NULL) return NULL;
+        if(root->left==NULL && root->right==NULL && root->val==target){
+            root=NULL;
+            return root;
+        }
+        TreeNode* lchild=removeLeafNodes(root->left,target);
+        TreeNode* rchild=removeLeafNodes(root->right,target);
+        root->left=lchild;
+        root->right=rchild;
+        if(root->left==NULL && root->right==NULL && root->val==target){
+            return NULL;
+        }
         return root;
     }
 };
