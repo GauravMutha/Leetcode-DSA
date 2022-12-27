6867f4f94ed54f8ed1dd8b5793f39d9018881b1a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-12-27 23:48:02 +0530

    Time: 139 ms (92.03%), Space: 90.2 MB (11.71%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -9,24 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//DFS
+//BFS
 class Solution {
 public:
-    int ans=0;
-    void helper(TreeNode* curr , int maxi){
-        if(curr==NULL) return;
-        maxi=max(curr->val,maxi);
-        if(curr->val>=maxi){
-            ans++;
+    int goodNodes(TreeNode* root) {
+        queue<pair<TreeNode*,int>>q;
+        int count=0,maxi=INT_MIN;
+        q.push({root,maxi});
+        while(!q.empty()){
+            TreeNode* curr=q.front().first;
+            int num=q.front().second;
+            q.pop();
+            if(curr->val>=num) count++;
+            maxi=max(curr->val,num);
+            if(curr->left) q.push({curr->left,maxi});
+            if(curr->right) q.push({curr->right,maxi});
         }
         
-        helper(curr->left,maxi);
-        helper(curr->right,maxi);
-
-    }
-    int goodNodes(TreeNode* root) {
-        int maxi=INT_MIN;
-        helper(root,maxi);
-        return ans;
+        return count;
     }
 };
