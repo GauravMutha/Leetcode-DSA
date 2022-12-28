88b61f3d7b7e9dff4b050ade426c8a88d0c9f808
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2022-12-28 16:38:37 +0530

    Time: 909 ms (98.46%), Space: 257.8 MB (97.36%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -9,31 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Concept same as previous one but we use only on map
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& des) {
         unordered_map<int,TreeNode*>getNode;
-        unordered_map<int,bool>isChild;
+        int xr=0;
         TreeNode* ans=NULL;
         for(auto &v : des){
             if(getNode.count(v[0])==0){
                 auto par=new TreeNode(v[0]);
                 getNode[v[0]]=par;
+                xr^=v[0];
             }
             if(getNode.count(v[1])==0){
                 auto ch=new TreeNode(v[1]);
                 getNode[v[1]]=ch;
+                xr^=v[1];
             }
             if(v[2]==1) getNode[v[0]]->left=getNode[v[1]];
             else getNode[v[0]]->right=getNode[v[1]];
-            isChild[v[1]]=true;
         }
         for(auto &v : des){
-            if(isChild[v[0]]!=true){
-                ans=getNode[v[0]];
-                break;
-            }
+            xr^=v[1];
         }
+        ans=getNode[xr];
         return ans;
     }
 };
