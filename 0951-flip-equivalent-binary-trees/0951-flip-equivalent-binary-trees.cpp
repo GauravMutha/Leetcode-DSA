a9ae5542283bfeccdede53506625864cf921e56a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-04 23:55:19 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (10.42%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -9,15 +9,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//DFS
+//BFS
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if(!root1 && !root2) return true;
-        if((root1==NULL)^(root2==NULL) || root1->val!=root2->val) return false;
-        
-        return (flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right))
-        || (flipEquiv(root1->right,root2->left) && flipEquiv(root1->left,root2->right));
+        queue<TreeNode*> q;
+        TreeNode* temp1,*temp2;
+        q.push(root1);
+        q.push(root2);
+        while(q.size()!=0)
+        {
+            temp1 = q.front();
+            q.pop();
+            temp2=q.front();
+            q.pop();
+            if(temp1==NULL && temp2==NULL)
+                continue;
+            else if(temp1==NULL || temp2==NULL || temp1->val!=temp2->val)
+                return false;
+            else if(issamechildren(temp1->left,temp2->left) && issamechildren(temp1->right,temp2->right))
+            {
+                q.push(temp1->left);
+                q.push(temp2->left);
+                q.push(temp1->right);
+                q.push(temp2->right);
+            }
+            else if(issamechildren(temp1->left,temp2->right) && issamechildren(temp1->right,temp2->left))
+            {
+                q.push(temp1->left);
+                q.push(temp2->right);
+                q.push(temp1->right);
+                q.push(temp2->left);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+
     }
-    
+    bool issamechildren(TreeNode* node1 ,  TreeNode* node2){
+        if(node1==nullptr && node2==nullptr) return true;
+        else if(node1==NULL || node2==NULL || node1->val!=node2->val) return false;
+        return true;
+    }
 };
