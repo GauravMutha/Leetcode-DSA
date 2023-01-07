acf5b1643e82f17e0d7cb3d79b011baa929ab25c
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-07 23:40:39 +0530

    Time: 102 ms (62.97%), Space: 36.9 MB (27.70%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -9,38 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//DFS postorder - Optimised than previous ones
 class Solution {
-private:
-    unordered_map<TreeNode*,TreeNode*>parent;
-    int ans=0;
 public:
-    void dfs(TreeNode* node , int steps , int& k ,unordered_map<TreeNode*,bool>&visited){
-        if(!node || steps>k || visited[node]) return;
-        if(!node->left && !node->right) ans++;
-        visited[node]=true;
-        dfs(node->left,steps+1,k,visited);
-        dfs(node->right,steps+1,k,visited);
-        dfs(parent[node],steps+1,k,visited);
-        visited[node]=false;
+    int ans=0;
+    vector<int> dfs(TreeNode* curr , int distance){
+        if(!curr) return {};
+        vector<int>ret;
+        vector<int>left=dfs(curr->left,distance);
+        vector<int>right = dfs(curr->right,distance);
+        
+        if(left.size()==0 && right.size()==0){
+            ret.push_back(1);
+            return ret;
+        }
+        
+        for(int i=0;i<left.size();i++){
+            for(int j=0;j<right.size();j++){
+                if(left[i]+right[j]<=distance) ans++;
+            }
+        }
+        
+        for(auto e : left)
+            ret.push_back(e+1);
+        for(auto e : right)
+            ret.push_back(e+1);
+        
+        return ret;
     }
-    int countPairs(TreeNode* root, int k) {
-        queue<TreeNode*>q;
-        vector<TreeNode*>leaf;
-        q.push(root);
-        TreeNode* temp;
-        while(!q.empty()){
-            temp=q.front() , q.pop();
-            if(!temp->left && !temp->right) leaf.push_back(temp);
-            if(temp->left) q.push(temp->left) , parent[temp->left]=temp;
-            if(temp->right) q.push(temp->right) , parent[temp->right]=temp;
-        }
-        
-        for(auto node : leaf){
-            unordered_map<TreeNode*,bool>visited;
-            visited[node]=true;
-            dfs(parent[node],1,k,visited);
-        }
-        
-        return (ans)/2;
+    int countPairs(TreeNode* root, int distance) {
+        dfs(root,distance);
+        return ans;
     }
 };
