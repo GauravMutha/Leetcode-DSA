169382adc34d23daffe28e6018d389db2b2d6299
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-08 18:55:10 +0530

    Time: 15 ms (96.14%), Space: 25 MB (64.43%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -9,28 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//DFS - Postorder
+//BFS
 class Solution {
 public:
-    TreeNode* dfs(TreeNode *&curr,int level,int k,int val){
-        if(!curr) return NULL;
-        auto lChild=dfs(curr->left,level+1,k,val);
-        auto rChild=dfs(curr->right,level+1,k,val);
-        
-        if(level==(k-1)){
-            auto L=new TreeNode(val,lChild,NULL);
-            auto R=new TreeNode(val,NULL,rChild);
-            curr->left=L;
-            curr->right=R;
-        }
-        
-        return curr;
-    }
-    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-        if (depth == 1) {
-            auto new_root = new TreeNode(val,root,NULL);
+    TreeNode* addOneRow(TreeNode* root, int val, int k) {
+        if(k==1){
+            auto new_root=new TreeNode(val,root,NULL);
             return new_root;
         }
-        return dfs(root,1,depth,val);
+        queue<TreeNode*>q;
+        q.push(root);
+        TreeNode* curr=NULL;
+        int level=1;
+        while(!q.empty()){
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                curr=q.front() , q.pop();
+                if(level == (k-1)){
+                    auto L=new TreeNode(val,curr->left,NULL);
+                    auto R=new TreeNode(val,NULL,curr->right);
+                    curr->left=L;
+                    curr->right=R;
+                }
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            level++;
+            if(level==k) break;
+        }
+        return root;
     }
 };
