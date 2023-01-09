cbd21264c0425f95a9fceea0230d937f7b6fbd25
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-09 18:55:55 +0530

    Time: 11 ms (83.39%), Space: 19.9 MB (59.85%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -9,21 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Slughtly optmisied DFS
+//We use only one vector vec and pass it by reference
+//we are able to do so because we pop back the node once we are done processing it i.e. we are following the backtracking method
 class Solution {
 public:
     vector<vector<int>>ans;
     vector<vector<int>> pathSum(TreeNode* root, int k) {
-        dfs(root,k,{});
+        vector<int>vec;
+        dfs(root,k,vec);
         return ans;
     }
-    void dfs(TreeNode* root , int k , vector<int>vec){
+    void dfs(TreeNode* root , int k , vector<int>&vec){
         if(!root) return;
         
         vec.push_back(root->val);
         if(!root->left && !root->right && (k==root->val))
             ans.push_back(vec);
-        
-        dfs(root->left ,k-root->val,vec);
-        dfs(root->right ,k-root->val,vec);
+        else{
+            dfs(root->left ,k-root->val,vec);
+            dfs(root->right ,k-root->val,vec);
+        }
+        vec.pop_back();
     }
 };
