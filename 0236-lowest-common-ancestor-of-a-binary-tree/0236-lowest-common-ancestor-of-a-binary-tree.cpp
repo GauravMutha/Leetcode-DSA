c6e37dfacd8139898e80b3a6d7426049b2706b53
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-09 16:59:17 +0530

    Time: 25 ms (56.18%), Space: 14.3 MB (27.75%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,36 +7,19 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-//Naive DFS - 3 pass
+//Optimised DFS postOrder
 class Solution {
 public:
-    unordered_map<int,TreeNode*>parent;
-    void parentTracker(TreeNode* curr){
-        if(!curr) return;
-        if(curr->left) parent[curr->left->val]=curr;
-        if(curr->right) parent[curr->right->val]=curr;
-        
-        parentTracker(curr->left);
-        parentTracker(curr->right);
-    }
-    void dfs(TreeNode* curr , vector<TreeNode*>&vec){
-        if(!curr) return;
-        vec.push_back(curr);
-        dfs(parent[curr->val],vec);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* ans=NULL;
-        vector<TreeNode*>vecP , vecQ;
-        parent[root->val]=NULL;
-        parentTracker(root);
-        dfs(p,vecP);
-        dfs(q,vecQ);
-        int i=vecP.size()-1 , j=vecQ.size()-1;
-        while(i>=0 && j>=0){
-            if(vecP[i]!=vecQ[j]) break;
-            ans=vecP[i];
-            i-- ,j--;
-        }
-        return ans;
+        if(!root) return NULL;
+        if(root->val==p->val || root->val==q->val) return root;
+        
+        auto leftRet=lowestCommonAncestor(root->left,p,q);
+        auto rightRet=lowestCommonAncestor(root->right,p,q);
+        
+        if(leftRet && rightRet) return root;
+        else if(!leftRet && !rightRet) return NULL;
+        
+        return leftRet==NULL?rightRet : leftRet;
     }
 };
