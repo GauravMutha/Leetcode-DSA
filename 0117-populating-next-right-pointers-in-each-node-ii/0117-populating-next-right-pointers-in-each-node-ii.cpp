c5744988e3229469eca5ef520a255b0bc4a6f4cf
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-11 17:31:26 +0530

    Time: 12 ms (90.99%), Space: 17.4 MB (86.15%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -15,26 +15,24 @@ public:
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 */
-//Purely iterative
+//Purely iterative - single while loop
 //real O(1)
 //No implicit stack space of recursion even
 class Solution {
 public:
     Node* connect(Node* root) {
         auto dummy=new Node(-1);
-        Node *head , *p ,*curr;
-        head=root , p=dummy;
-        while(head){
-            curr=head;
-            while(curr){
-                if(curr->left) p->next=curr->left , p=p->next;
-                if(curr->right) p->next=curr->right , p=p->next;
-                
-                curr=curr->next;
+        Node *p ,*curr;
+        p=dummy , curr=root;
+        while(curr){
+            if(curr->left) p->next=curr->left , p=p->next;
+            if(curr->right) p->next=curr->right , p=p->next;
+            if(curr->next) curr=curr->next;
+            else{
+                curr=dummy->next;
+                dummy->next=NULL;
+                p=dummy;
             }
-            head=dummy->next;
-            dummy->next=NULL;
-            p=dummy;
         }
         
         return root;
