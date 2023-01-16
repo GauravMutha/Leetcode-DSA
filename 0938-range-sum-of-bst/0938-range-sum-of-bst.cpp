8172e238ef7d921c4042d0ced0856b2c22edcbac
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-16 23:34:14 +0530

    Time: 118 ms (98.99%), Space: 64.9 MB (7.72%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -9,14 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//BST-OPTIMIZED DFS
+//BST BASED BFS
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(!root) return 0;
-        int ans=(root->val>=low && root->val<=high) ? root->val :0;
-        if(root->val>low) ans+= rangeSumBST(root->left,low,high);
-        if(root->val<high) ans+= rangeSumBST(root->right,low,high);
+        queue<TreeNode*>q;
+        q.push(root);
+        TreeNode* curr;
+        int ans=0;
+        while(!q.empty()){
+            curr=q.front() ,q.pop();
+            int v=curr->val;
+            ans+=(v>=low && v<=high) ? curr->val : 0;
+            
+            if(v>low && curr->left) q.push(curr->left); 
+            if(v<high && curr->right) q.push(curr->right); 
+        }
         
         return ans;
     }
