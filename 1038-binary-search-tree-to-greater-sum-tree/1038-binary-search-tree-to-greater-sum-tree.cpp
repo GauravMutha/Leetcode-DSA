2e463b47a773ce8e1fa531f9e25a26554c1ac2dc
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-17 17:04:48 +0530

    Time: 3 ms (67.02%), Space: 8.1 MB (57.06%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -9,17 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//BST BASED DFS- INORDER
+//BST BASED INORDER -ITERATIVE
 class Solution {
 public:
-    int pre=0;
     TreeNode* bstToGst(TreeNode* root) {
-        if(root->right) bstToGst(root->right);
-        
-        pre+=root->val;
-        root->val=pre;
-        
-        if(root->left) bstToGst(root->left);
+        TreeNode* curr=root, *temp=NULL;
+        int accum=0;
+        while(curr!=NULL){
+            if(curr->right==NULL){
+                accum+=curr->val;
+                curr->val=accum;
+                curr=curr->left;
+            }
+            else{
+                temp=curr->right;
+                while(temp->left!=NULL && temp->left!=curr)
+                    temp=temp->left;
+                if(temp->left==NULL){
+                    temp->left=curr;
+                    curr=curr->right;
+                }
+                else {
+                    accum+=curr->val;
+                    curr->val=accum;
+                    temp->left=NULL;
+                    curr=curr->left;
+                }
+            }
+        }
         
         return root;
     }
