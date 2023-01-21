f05106d067b38292db83b06c0f1c7b4064ae054b
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-21 16:15:27 +0530

    Time: 11 ms (95.98%), Space: 23.9 MB (79.00%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
@@ -9,21 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//DFS- BST OPTIMIZED
+//DFS- BST OPTIMIZED - MORE CONCISE
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if(!root) return NULL;
         
-        TreeNode* L= (root->val >low) ? trimBST(root->left,low,high) : NULL;
-        TreeNode* R= (root->val<high)? trimBST(root->right,low,high) : NULL;
+        if(root->val < low) return trimBST(root->right,low,high);
+        else if(root->val > high) return trimBST(root->left,low,high);
+        //if not above two that means values is within range
         
-        if(root->val<low) root->left=NULL , root=R;
-        else if(root->val>high) root->right=NULL , root=L;
-        else if(root->val>=low && root->val<=high){
-            root->left=L;
-            root->right=R;
-        }
+        root->left=trimBST(root->left,low,high);
+        root->right=trimBST(root->right,low,high);
         
         return root;
     }
