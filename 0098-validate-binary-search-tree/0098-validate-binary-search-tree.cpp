272f57f1612a9801b077ab9d67213a180a0a117f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-23 22:58:43 +0530

    Time: 25 ms (23.80%), Space: 21.7 MB (52.42%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,17 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//PREORDER
+//MORE CONCISE
+//UPPER BOUNDS LOWER BOUNDS CONCEPT IS USED RATHER THAN LONG_MIN AND PRE
 class Solution {
 public:
-    long pre=LONG_MIN;
     bool isValidBST(TreeNode* root) {
+        return dfs(root,NULL,NULL);
+    }
+    bool dfs(TreeNode* root, TreeNode* minNode , TreeNode* maxNode){
         if(!root) return true;
-        bool L= isValidBST(root->left);
-        if(pre!=LONG_MIN) L=L&& (root->val>pre);
-        pre=root->val;
+        if((minNode&& root->val<=minNode->val)||(maxNode && root->val>=maxNode->val))
+            return false;
         
-        bool R= isValidBST(root->right);
-        
-        return R&&L;
+        return dfs(root->left,minNode,root) && dfs(root->right,root,maxNode);
     }
 };
