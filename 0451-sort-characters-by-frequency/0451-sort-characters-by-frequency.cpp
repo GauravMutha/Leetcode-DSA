87f3106335b54069573781e93d9652b132a5c697
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-25 00:43:42 +0530

    Time: 16 ms (73.46%), Space: 8.5 MB (67.59%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,22 @@
-//Priority Queue + Hash Table
-//3 pass
+//Map+pair vector + comparator
 class Solution {
 public:
     string frequencySort(string s) {
-        priority_queue<pair<int,char>>maxh;
-        vector<int>hash(127,0);
-        int i=0;
-        for(auto c : s) hash[c]++;
+        unordered_map<char,int>m;
+        vector<pair<int,char>>vec;
+        for(auto c: s) m[c]++;
         
-        for(int i=0;i<127;i++) 
-            if(hash[i]) maxh.push({hash[i],i});
+        for(auto &[c,freq]:m)
+            vec.push_back({freq,c});
         
-        while(!maxh.empty()){
-            int k=maxh.top().first;
-            while(k--) s[i++]=maxh.top().second;
-            maxh.pop();
-        }
-        return s;
+        sort(begin(vec),end(vec),[](const auto &a,const auto &b){
+            return a.first>b.first;
+        });
+        
+        string ans;
+        for(auto &[freq,c] : vec)
+            ans.append(freq,c);
+        
+        return ans;
     }
 };
