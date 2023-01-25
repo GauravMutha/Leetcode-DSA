8b55ff980f5af65189678cfcd4fcf99f31c82d16
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-25 19:19:05 +0530

    Time: 0 ms (100.00%), Space: 6 MB (36.75%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,24 +1,12 @@
-//Priority Queue
+//MATH
+//O(1) TC
+//O(1) SC
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        priority_queue<int>maxh;
-        int num1,num2,score=0;
+        int largest=max({a,b,c});
+        int sumOfSmallestTwo=(a+b+c)- largest;
         
-        maxh.push(a) , maxh.push(b) , maxh.push(c);
-        
-        num1=maxh.top() , maxh.pop();
-        num2=maxh.top() , maxh.pop();
-        
-        while(num1!=0 && num2!=0) {
-            score++;
-            num1-- , num2--;
-            
-            maxh.push(num1) , maxh.push(num2);
-            num1=maxh.top() , maxh.pop();
-            num2=maxh.top() , maxh.pop();
-        }
-        
-        return score;
+        return (sumOfSmallestTwo<=largest) ? sumOfSmallestTwo : (a+b+c)/2;
     }
 };
