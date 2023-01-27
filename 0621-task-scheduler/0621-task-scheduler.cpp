00ebb69e4772b25d1f788ff3ed4cefccb1b407ec
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-28 00:32:07 +0530

    Time: 116 ms (57.60%), Space: 34.5 MB (77.67%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,21 +1,22 @@
+//TC-->O(N)
+//SC--> O(1)
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int idle) {
+    int leastInterval(vector<char>& tasks, int n) {
         vector<int>hash(26,0);
-        priority_queue<int>maxh;
-        int k=0,extra=0,ans=0,x=0;
-        for(auto &e : tasks) hash[e-'A']++;
-        for(auto &e : hash) if(e) maxh.push(e);
-        
-        while(maxh.size()){
-            vector<int>temp;
-            k=min(idle+1,(int)maxh.size());
-            for(int i=0;i<k;i++) temp.push_back(maxh.top()) , maxh.pop();
-            for(int i=0;i<temp.size();i++) if(--temp[i]>0) maxh.push(temp[i]);
-            extra=(maxh.empty()) ? 0 : idle+1-k;
-            ans+=(k+extra);
+        int max=0 , maxCount=0;
+        for(auto &c : tasks){
+            hash[c-'A']++;
+            if(max==hash[c-'A']) maxCount++;
+            else if(hash[c-'A'] > max) max=hash[c-'A'] , maxCount=1;
         }
         
-        return ans+maxh.size();
+        int partCount=max-1;
+        int partLength=n-(maxCount-1);
+        int emptySlots=partCount*partLength;
+        int availableTasks=tasks.size() - max*maxCount;
+        int idleSlots= ((emptySlots-availableTasks)<= 0)?0:emptySlots-availableTasks;
+        
+        return tasks.size()+idleSlots;
     }
 };
