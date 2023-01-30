841ddf2e8ac9eb31daac0f5d683b76d3ba355198
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-01-30 18:56:48 +0530

    Time: 258 ms (87.69%), Space: 97.3 MB (55.77%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,24 +1,21 @@
-//Priority Queue + comparator
-typedef pair<int,int>p;
-class compare{
-public:
-    bool operator()(const p &a ,const p &b){
-        return (a.first>b.first) || ( (a.first==b.first) && (a.second<b.second));
-    }
-};
+//Using Hash Map 
+//TC--> O(N)
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         vector<int>res;
-        priority_queue<p,vector<p>,compare> pq;
-        for(int r=0;r<nums.size();r++){
-            for(int c=0;c<nums[r].size();c++)
-                pq.push({r+c,r});
+        unordered_map<int,vector<int>>m;
+        int maxSum=INT_MIN,rows=nums.size();
+        for(int r=rows-1;r>=0;r--){
+            for(int c=0;c<nums[r].size();c++){
+                m[r+c].push_back(nums[r][c]);
+                maxSum=max(r+c,maxSum);
+            }
         }
-        while(!pq.empty()){
-            int i=pq.top().second , j=pq.top().first-i;
-            res.push_back(nums[i][j]);
-            pq.pop();
+        
+        for(int sum=0;sum<=maxSum;sum++){
+            for(auto &value : m[sum]) 
+                res.push_back(value);  
         }
         
         return res;
