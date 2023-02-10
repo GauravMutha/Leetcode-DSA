e5e12ce29b49322407111cba61c28914b182e207
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-10 19:59:10 +0530

    Time: 120 ms (71.73%), Space: 59.6 MB (94.88%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,5 +1,5 @@
-// another concise solution for handling out of bound edge cases faced in first wrong solution of this problem
-//handled better than 1st accepted solution
+//we let the bs flow
+//we know that once we have start==end we have our answer but we still let bs run
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
@@ -7,14 +7,13 @@ public:
         
         while(start<=end){
             mid=start+ (end-start)/2;
-            if((mid-1)>=0 && arr[mid-1]<arr[mid] && arr[mid+1]<arr[mid])
-                return mid;
+             
             if(arr[mid+1]>arr[mid])
                 start=mid+1;
             else if(arr[mid+1]<arr[mid]) 
                 end=mid-1;
         }
         
-        return -1;
+        return start;
     }
 };
