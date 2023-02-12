6e137f7bbe42346bbb0a4017ddda13a90f3d8b37
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-12 18:42:52 +0530

    Time: 141 ms (6.69%), Space: 17.1 MB (7.48%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,17 +1,26 @@
-//brute force + heap
-//O((n^2)*log(k)) 
+//min heap 
+//kth smallest amongst m sorted rows
+//SC-->O(N)
+//TC-->O((n+k)*logn)
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<vector<int>,vector<vector<int>>,greater<>> minh;
+        vector<int>vec;
+        //n equal to matrix[0].size() too as it is nXn matrix
+        int n=matrix.size(),ans=0; 
+        for(int r=0;r<min(n,k);r++)
+            minh.push({matrix[r][0],r,0});
         
-        priority_queue<int>maxh;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                maxh.push(matrix[i][j]);
-                if(maxh.size()>k) maxh.pop();
-            }
+        while(k--){
+            vec=minh.top(), minh.pop();
+            ans=vec[0];
+            int currR=vec[1];
+            int currCol=vec[2];
+            if(currCol+1 < n)
+                minh.push({matrix[currR][currCol+1],currR,currCol+1});
         }
         
-        return maxh.top();
+        return ans;
     }
 };
