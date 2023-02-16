a203c721c2246217fc9d30b97219b7081cb49490
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-16 17:25:05 +0530

    Time: 221 ms (5.32%), Space: 27.2 MB (5.02%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,21 +1,37 @@
-//almost exactly similar to 424
-//sliding window
+//a optimisation on brute force O(n*n)
+//Binary search
+//TC-->(n*log(n));
 class Solution {
-public:
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        int res=0,maxKey=0,tCount=0, fCount=0,r=0,l=0;
+private:
+    vector<int>tPre , fPre;
+    
+    int binarySearch(int i,int k,string &key){
+        int start=i,end=key.size()-1, j=0;
         
-        for(r=0;r<answerKey.size();r++){
-            (answerKey[r]=='T') ? tCount++:fCount++;
-            maxKey=max(tCount,fCount);
-            
-            if(r-l+1-maxKey>k){
-                res=max(res,(r-1)-l+1); //res=max(res,r-l+1)
-                (answerKey[l]=='T') ? tCount--:fCount--;
-                l++;
-            }
+        while(start<=end){
+            int mid=start  + (end-start)/2;
+            int tCount=(i>0) ? tPre[mid]-tPre[i-1] : tPre[mid];
+            int fCount=(i>0) ? fPre[mid]-fPre[i-1] : fPre[mid];
+            int maxKey=max(tCount,fCount);
+            if(mid-i+1-maxKey>k) end=mid-1;
+            else j=mid , start=mid+1;
         }
         
-        return max(res,(r-1)-l+1);
+        return j;
+    }
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        int res=0,fSum=0,tSum=0;
+        
+        for(auto & ch : answerKey){
+            tSum+=(ch=='T') , fSum+=(ch=='F');
+            tPre.push_back(tSum) , fPre.push_back(fSum);
+        }
+        for(int i=0;i<answerKey.size();i++){
+            int j=binarySearch(i,k,answerKey);
+            res=max(res,j-i+1);
+        }
+        
+        return res;
     }
 };
