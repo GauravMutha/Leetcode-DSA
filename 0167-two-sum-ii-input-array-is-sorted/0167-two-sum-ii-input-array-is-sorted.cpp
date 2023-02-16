7ff4bf015c72720906bcd5ee752c6836f3a69aee
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-16 19:12:13 +0530

    Time: 13 ms (74.85%), Space: 15.7 MB (54.17%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,27 +1,15 @@
-//binary search
-//TC-->O(n*log(n))
+//two pinters 
+//O(n)
 class Solution {
-private :
-    int binarySearch(int i,long require,vector<int>& nums){
-        int start=i ,end=nums.size()-1 ,j=-1;
-        
-        while(start<=end){
-            int mid=start  +(end-start)/2;
-            
-            if(nums[mid]<=require){
-                if(nums[mid]==require) j=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]>require) end=mid-1;
-        }
-        
-        return j;
-    }
 public:
     vector<int> twoSum(vector<int>& nums, int k) {
-        for(int i=0;i<nums.size();i++){
-            int j=binarySearch(i,long(k-nums[i]),nums);
-            if(j!=-1 && j!=i) return vector<int>({i+1,j+1});
+        int r=nums.size()-1,l=0;
+        
+        while(l<r){
+            if(long(nums[l]+nums[r])==k) return {l+1,r+1};
+            
+            else if(long(nums[l]+nums[r])>k) r--;
+            else l++;
         }
         
         return {};
