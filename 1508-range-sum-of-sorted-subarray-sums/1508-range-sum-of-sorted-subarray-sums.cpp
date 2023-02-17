b38c7b860b2183392999be0ef4446b244714df1f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-17 19:38:16 +0530

    Time: 142 ms (84.31%), Space: 8 MB (86.52%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,24 +1,30 @@
-//brute force
-//TC-->O(n^2+n*log(n))
-//SC-->O(n^2)
+//priority queue min heap
+//TC-->O(n^2log(n)) //and not right*log(n) because right can be n
+//SC-->O(n)
+typedef pair<int,int>p;
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int>sortedSubSum;
-        int res=0;
-        
-        for(int i=0;i<nums.size();i++){
-            int preSum=0;
-            for(int j=i;j<nums.size();j++){
-                preSum+=nums[j];
-                sortedSubSum.push_back(preSum);
+        int ans=0,mod=1e9+7;
+        priority_queue<p,vector<p>,greater<p>> minh;
+        for(int i=0;i<n;i++){
+            minh.push({nums[i],i+1});
+        }
+        for(int i=1;i<=right;i++){
+            auto x= minh.top();
+            minh.pop();
+            
+            if(i>=left) ans=(ans+x.first)%mod;
+            
+            if(x.second<n) {
+                x.first+=nums[x.second];
+                x.second++;
+                
+                minh.push({x});
             }
         }
         
-        sort(sortedSubSum.begin(),sortedSubSum.end());
-        res=accumulate(sortedSubSum.begin()+left-1,sortedSubSum.begin()+right,0LL) % 1000000007;
-        
-        return res;
+        return ans;
         
     }
 };
