bcea9d7f8683eedf8a320e594124ac8356d7264a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-19 18:33:23 +0530

    Time: 228 ms (42.03%), Space: 98.5 MB (83.05%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -1,17 +1,20 @@
 //two pointers
-//TC--> O(min(m,n))
+//TC--> O(m*log(n)) m is the size of nums1 , n is of nums2
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        int maxDist=0,i=0,j=0,sz1=nums1.size(),sz2=nums2.size();
+        int maxDist=0,low=0,high=0,sz1=nums1.size(),sz2=nums2.size(),res=0;
         
-        while(i<sz1 && j<sz2){
-            if(nums1[i]<=nums2[j]){
-                if(j>=i) maxDist=max(maxDist,j-i);
-                j++;
+        for(int i=0;i<nums1.size();i++){
+            int mid=0 , low=i,high=nums2.size()-1;
+            while(low<=high){
+                mid=low + (high-low)/2;
+                if(nums2[mid]>=nums1[i])
+                    maxDist=max(mid-i,maxDist) , low=mid+1;
+                else high=mid-1;
             }
-            else i++;
+            res=max(res,maxDist);
         }
-        return maxDist;
+        return res;
     }
 };
