e7a110a6da703605419b8ab1d9559851d65eed73
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-19 12:43:31 +0530

    Time: 105 ms (89.15%), Space: 28.5 MB (65.76%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,35 +1,21 @@
-//Binary search + prefix sum (DP-type)
+//Only prefix sum - No binary search
+//O(m*n)
 class Solution {
-private:
-    int m=0,n=0;
-    bool squareExists(vector<vector<int>>&prefixSum,int len,int k){
-        for(int i=len;i<=m;i++){
-            for(int j=len;j<=n;j++){
-                if(prefixSum[i][j]-prefixSum[i-len][j]-prefixSum[i][j-len]+prefixSum[i-len][j-len]<=k)
-                    return true;
-            }
-        }
-        return false;
-    }
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        m=mat.size() , n=mat[0].size();
-        vector<vector<int>>prefixSum(m+1,vector<int>(n+1));
+        int m=mat.size() , n=mat[0].size();
+        vector<vector<int>>preSum(m+1,vector<int>(n+1));
+        int ans=0, len=1;
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                prefixSum[i][j]=prefixSum[i-1][j]+prefixSum[i][j-1]-prefixSum[i-1][j-1]+mat[i-1][j-1];
+                preSum[i][j]=preSum[i-1][j]+preSum[i][j-1]-preSum[i-1][j-1]+mat[i-1][j-1];
+                
+                if(i>=len && j>=len && preSum[i][j]-preSum[i-len][j]-preSum[i][j-len]+preSum[i-len][j-len]<=threshold){
+                    ans=len;
+                    len++;
+                }
             }
         }
-        
-        int low=1,end=max(m,n),ans=0;
-        
-        while(low<=end){
-            int mid=low+(end-low)/2;
-            
-            if(squareExists(prefixSum,mid,threshold)) ans=mid , low=mid+1;
-            else end=mid-1;
-        }
-        
         return ans;
     }
 };
