616ee9d3bb0367fce62af0035ab42e27b0202d01
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-21 15:46:25 +0530

    Time: 305 ms (88.13%), Space: 113.7 MB (90.33%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,15 +1,16 @@
-//Linear search
-//TC-->O(max(N,M)) SC-->O(M) where M is number of unique elements in banned
+// We can reduce time complexity by using two pointers and sorting
+//O(MlogM) mi sisze of banned array
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        unordered_set<int>s;
-        for(int i=0;i<banned.size();i++){
-            if(banned[i]<=n) s.insert(banned[i]); 
-        }
-        int sum=0,count=0;
-        for(int i=1;i<=n;i++){
-            if(s.find(i)==s.end() && sum+i<=maxSum) sum+=i ,count++;
+        sort(begin(banned),end(banned));
+        int j=0,sum=0,count=0;
+        
+        for(int i=1;i<=n && sum+i<=maxSum;i++){
+            if(j>=banned.size() || i!=banned[j]) 
+                sum+=i , count++;
+            else 
+                while(j<banned.size() && i==banned[j]) j++;
         }
         
         return count;
