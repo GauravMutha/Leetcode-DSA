f6dffac878c83885d2c0f0aaa1e901b417fdcaf7
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-23 08:37:37 +0530

    Time: 776 ms (8.77%), Space: 13 MB (11.43%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,15 +1,26 @@
-//Binary search using lower_bound
+//Binary search
 //O(N^2(log(N))+ Nlog(N))
 class Solution {
+private:
+    int binarySearch(vector<int>&nums,int i,int j){
+         int low=j+1,high=nums.size()-1,ans=-1;
+         while(low<=high){
+             int mid=low+(high-low)/2;
+             if(nums[i]+nums[j]>nums[mid]) low=mid+1,ans=mid;
+             else high=mid-1;
+         }
+         return ans;
+    }
 public:
     int triangleNumber(vector<int>& nums) {
         int res=0;
         sort(begin(nums),end(nums));
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size()-1;j++){
-                auto it=lower_bound(begin(nums)+j+1,end(nums),nums[i]+nums[j]);
-                int k=it-nums.begin();
-                res+=(k-j-1);
+                int target=nums[i]+nums[j];
+                int k=binarySearch(nums,i,j);
+
+                if(k!=-1) res+=(k-j);
             }
         }
         return res;
