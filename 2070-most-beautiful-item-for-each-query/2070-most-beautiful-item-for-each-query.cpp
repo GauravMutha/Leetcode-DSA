946520bc893d9602a5117aabc3cfab7e1317004b
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-23 17:39:03 +0530

    Time: 429 ms (59.17%), Space: 90.3 MB (63.75%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -1,26 +1,23 @@
-//sorting + binary search(using lower bound)
+//sorting  + pair vector+ two pointer
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
         vector<int>res(queries.size());
+        
+        vector<pair<int,int>>queriesPairs;
+        for(int i=0;i< queries.size();i++)
+            queriesPairs.push_back({queries[i],i});
+        
+        sort(begin(queriesPairs),end(queriesPairs));
         sort(begin(items),end(items));
-        int maxBeauty=-1;
-        for(auto &p:items){
-            maxBeauty=max(maxBeauty,p[1]);
-            p.push_back(maxBeauty);
-        }
+        int maxBeauty=0,j=0;
         
-        //comparator function for upper_bound on vector of vectors
-        auto cmp = [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        };
-        
-        for(int i=0;i<queries.size();i++){
-            //making a temp vector to pass as val in upper_bound
-            vector<int>temp={queries[i],-1,-1};
-            auto it=upper_bound(begin(items),end(items),temp,cmp);
-            if(it==items.begin()) res[i]=0;
-            else res[i] = (*(--it))[2];
+        for(int i=0;i<queriesPairs.size();i++){
+            while(j<items.size() && items[j][0]<=(queriesPairs[i].first)){
+                maxBeauty=max(maxBeauty,items[j][1]);
+                j++;
+            }
+            res[queriesPairs[i].second]=maxBeauty;
         }
         
         return res;
