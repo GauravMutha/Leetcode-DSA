3ac40a2985cfba2847eb8a43cfd43d778f4ffa3c
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-24 17:46:28 +0530

    Time: 14 ms (18.57%), Space: 9 MB (8.63%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -1,12 +1,27 @@
-//Sliding window
+//Binary search
 class Solution {
+private :
+    vector<int>prefixCost;
+    int binarySearch(string &s,string &t,int i,int maxCost){
+        int low=i,high=s.size()-1,totalCost=0,ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            totalCost=(i>0)?prefixCost[mid]-prefixCost[i-1]:prefixCost[mid];
+            if(totalCost>maxCost) high=mid-1;
+            else ans=mid , low=mid+1;
+        }
+        return ans;
+    }
 public:
     int equalSubstring(string s, string t, int maxCost) {
-        int n=s.size(),maxLen=INT_MIN,l=0,r=0,cost=0;
-        for(;r<n;r++){
-            cost+=abs(s[r]-t[r]);
-            while(cost>maxCost) cost-=abs(s[l]-t[l]) , l++;
-             maxLen=max(maxLen,r-l+1);
+        int cost=0,maxLen=0,n=s.size();
+        for(int i=0;i<n;i++){
+            cost+=abs(s[i]-t[i]);
+            prefixCost.push_back(cost);
+        }
+        for(int i=0;i<n;i++){
+            int j=binarySearch(s,t,i,maxCost);
+            if(j!=-1)maxLen=max(maxLen,j-i+1);
         }
         
         return maxLen;
