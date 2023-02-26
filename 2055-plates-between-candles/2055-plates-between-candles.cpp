2ab2ed93e2f2f5bcfd6c263f7877eda4c71bb06f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-26 13:02:54 +0530

    Time: 612 ms (30.87%), Space: 158.6 MB (25.28%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.cpp
+++ 2055-plates-between-candles/2055-plates-between-candles.cpp
@@ -1,24 +1,19 @@
-//preifix and suffix
+//Binary search
 class Solution {
 public:
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
-        int LC,RC,sz=s.size(),preIndx=-1,suffIndx=sz,q=queries.size(),pCount=0;
-        vector<int>res(q),rMost(sz),lMost(sz),prePlate(sz);
-        
-        for(int i=0,j=sz-1;i<sz && j>=0;i++,j--){
-            if(s[i]=='|') preIndx=i;
-            else pCount++;
-            if(s[j]=='|') suffIndx=j; 
-            rMost[i]=preIndx;
-            lMost[j]=suffIndx;
-            prePlate[i]=pCount;
-        }
-        
-        for(int i=0;i<queries.size();i++){
-            int start=queries[i][0], end=queries[i][1];
-            RC=rMost[end] , LC=lMost[start];
-            if(RC<start || LC>end || (RC==LC)) res[i]=0;
-            else res[i]=(LC==0)?prePlate[RC]:prePlate[RC]-prePlate[LC-1];
+        vector<int>candleIndx,res;
+        for(int i=0;i<s.size();i++)
+            if(s[i]=='|') candleIndx.push_back(i);
+        for(auto q:queries){
+            int lMost=lower_bound(begin(candleIndx),end(candleIndx),q[0])-begin(candleIndx);
+            int rMost=upper_bound(begin(candleIndx),end(candleIndx),q[1])-begin(candleIndx)-1;
+            
+            int count=0;
+            if(lMost>=rMost) count=0;
+            else count=(candleIndx[rMost]-candleIndx[lMost]+1)-(rMost-lMost+1);
+            
+            res.push_back(count);
         }
         
         return res;
