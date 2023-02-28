30384ddc94132ba43099fe583e10935ce7a534b4
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-02-28 17:17:41 +0530

    Time: 160 ms (86.50%), Space: 56.1 MB (52.62%) - LeetHub

--- 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -1,25 +1,17 @@
-//Binary search
+//Suffix array O(N)
 class Solution {
 public:
     vector<int> smallestSubarrays(vector<int>& nums) {
-        vector<int>res(nums.size());
-        
-        unordered_map<int,vector<int>>setBitIndices;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<32;j++){
-                if(nums[i]&(1<<j)) setBitIndices[j].push_back(i);
-            }
-        }
-        
-        for(int i=0;i<nums.size();i++){
+        vector<int>res(nums.size()), Indices(32);
+        for(int i=nums.size()-1;i>=0;i--){
             int maxIndex=i;
             for(int j=0;j<32;j++){
-                auto it=lower_bound(begin(setBitIndices[j]),end(setBitIndices[j]),i);
-                if(it!=setBitIndices[j].end()) maxIndex=max(maxIndex,*it);
+                if(nums[i]&(1<<j)) Indices[j]=i;
+                
+                maxIndex=max(maxIndex,Indices[j]);
             }
             res[i]=maxIndex-i+1;
         }
-        
         return res;
     }
 };
