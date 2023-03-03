fadd40d25ece10ebaa6ccc6f6435cba506c4f6c4
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-03 16:57:36 +0530

    Time: 305 ms (56.61%), Space: 80.9 MB (83.21%) - LeetHub

---
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumRemovals(string s, string p, vector<int>& rem) {
+        vector<int>m(s.size(),INT_MAX);
+        int ans=0;
+        for(int i=0;i<rem.size();i++){
+            m[rem[i]]=i;
+        }
+        
+        int low=0,high=rem.size();
+        while(low<=high){
+            int mid=low+(high-low)/2,j=0;
+            
+            for(int i=0;i<s.size() && j<p.size();i++){
+                if(m[i]>=mid && s[i]==p[j]) j++;
+            }
+            if(j==p.size()) low=mid+1 , ans=mid;
+            else high=mid-1;
+        }
+        
+        return ans;
+    }
+};
