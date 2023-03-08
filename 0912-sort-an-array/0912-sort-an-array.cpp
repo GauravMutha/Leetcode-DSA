92b2bc788d70d33b725b98c672f857fe43b0d716
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-08 12:45:13 +0530

    Time: 114 ms (97.59%), Space: 68.5 MB (42.45%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,37 +1,21 @@
-//MERGE SORT
-//Iterative
-//Extra Space
+//Counting Sort
 class Solution {
 public:
-    void merge(vector<int>&arr,int low,int mid,int high){
-        vector<int>temp;
-        int i=low , j=mid+1;
-        while(i<=mid && j<=high){
-            if(arr[i]<=arr[j]){
-                temp.push_back(arr[i]);
-                i++;
-            }
-            else {
-                temp.push_back(arr[j]);
-                j++;
-            }
-        }
-        while(i<=mid) temp.push_back(arr[i]) , i++;
-        while(j<=high) temp.push_back(arr[j]) , j++;
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        int minVal=*min_element(begin(nums),end(nums));
+        int maxVal=*max_element(begin(nums),end(nums));
+        vector<int>freq(maxVal-minVal+1,0);
         
-        for(int i=low;i<=high;i++) arr[i]=temp[i-low];
-    }
-    vector<int> sortArray(vector<int>& arr) {
-        int currSize,n=arr.size(),pass=0;
-        for(currSize=1;currSize<n;currSize*=2){
-            for(int left=0; left<n-1; left+=2*currSize){
-                
-                int mid=min(left+currSize-1,n-1);
-                int right=min(left+2*currSize-1,n-1);
-                
-                merge(arr,left,mid,right);
-            }
+        for(int i=0;i<n;i++){
+            freq[nums[i]-minVal]++;
         }
-        return arr;
+        int j=0;
+        for(int i=0;i<freq.size();i++){
+            if(freq[i]==0) continue;
+            int x=freq[i];
+            while(x--) nums[j++]=i+minVal;
+        }
+        return nums;
     }
 };
