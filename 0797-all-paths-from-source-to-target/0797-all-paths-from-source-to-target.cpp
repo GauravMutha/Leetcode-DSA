56d8c1af1fe1f9f73040931e124cc9dad7e35cd2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-16 18:28:34 +0530

    Time: 19 ms (47.88%), Space: 14.8 MB (38.37%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,19 +1,27 @@
+//BFS traversal
 class Solution {
 public:
-    void dfs(int node,vector<vector<int>>graph,vector<int>& picker,vector<vector<int>>& ans){
-        picker.push_back(node);
-        if(node==graph.size()-1){
-            ans.push_back(picker);
-        }
-        for(auto n : graph[node])
-            dfs(n,graph,picker,ans);
-        picker.pop_back();
-        
-    }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<int>picker;
         vector<vector<int>>ans;
-        dfs(0,graph,picker,ans);
+        queue<vector<int>>q;
+        q.push({0});
+        while(!q.empty()){
+            auto path=move(q.front()); 
+            q.pop();
+            if(path.back()==graph.size()-1){
+                //the vector we just popped out has reached the target
+                ans.push_back(path);
+            }
+            else{
+                //new vectors are inserted here based on previous vector's neighbours
+                for(auto node : graph[path.back()]){
+                    path.push_back(node);
+                    q.push(path);
+                    path.pop_back();
+                }
+            }
+        }
+        
         return ans;
     }
 };
