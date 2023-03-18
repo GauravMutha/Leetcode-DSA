91df468f14dcac53e5eedca7af82cabea53ae5b3
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-18 17:57:11 +0530

    Time: 23 ms (81.87%), Space: 13.7 MB (77.86%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,25 +1,46 @@
-// O(N^2)
-//DFS only using given adjacecny matrix M
+//Using DSU
+//O(N^2(logN))
 class Solution {
+private:
+    int groups;
 public:
-    void dfs(int i,vector<vector<int>>& M,vector<bool>& visited){
-        visited[i]=true;
-        for(int j=0;j<M.size();j++){
-            if(i!=j && M[i][j]==1 && visited[j]==false)
-                dfs(j,M,visited);
+    //finding parent
+    int findParent(int node,vector<int>& parent){
+        if(parent[node]==node){
+            return node;
         }
+        //path compression optimisation before returning
+        int temp=findParent(parent[node],parent);
+        parent[node]=temp;
+        return temp;
+    }
+    void doUnion(int x,int y,vector<int>& parent , vector<int>& rank){
+        int xParent=findParent(x,parent);
+        int yParent=findParent(y,parent);
+        //If parents are equal we do nothing
+        if(xParent==yParent) return;
+        //otherwise we merge the parents using union by rank optimisation and update our answer i.e groups
+        groups--;
+        if(rank[xParent]<rank[yParent])
+        parent[xParent]=yParent;
+        else if(rank[yParent]<rank[xParent])
+            parent[yParent]=xParent;
+        else 
+            parent[yParent]=xParent , rank[xParent]++; //we can make any one of the node parent
     }
     int findCircleNum(vector<vector<int>>& M) {
-        int ans=0,n=M.size();
-        vector<bool>visited(n,false);
+        int n=M.size();
+        groups=n;
+        vector<int>parent(n),rank(n,0);
+        for(int i=0;i<n;i++) parent[i]=i;
         
+        //we do union of every two connected city
         for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                ans++;
-                dfs(i,M,visited);
+            for(int j=i+1;j<n;j++){
+                if(M[i][j]==1) doUnion(i,j,parent,rank);
             }
         }
         
-        return ans;
+        return groups;
     }
 };
