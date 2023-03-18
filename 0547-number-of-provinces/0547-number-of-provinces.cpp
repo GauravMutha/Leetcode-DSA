5a43b3930f8f0fdec7f2499a8c5f4031f475758e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-18 17:09:41 +0530

    Time: 19 ms (94.97%), Space: 13.5 MB (99.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,22 @@
 // O(N^2)
-//making adjacency list then performin dfs
+//DFS only using given adjacecny matrix M
 class Solution {
 public:
-    void dfs(int node,vector<vector<int>>& adjList,vector<bool>& visited){
-        visited[node]=true;
-        for(auto adjNode : adjList[node]){
-            if(visited[adjNode]==false)
-                dfs(adjNode,adjList,visited);
+    void dfs(int i,vector<vector<int>>& M,vector<bool>& visited){
+        visited[i]=true;
+        for(int j=0;j<M.size();j++){
+            if(i!=j && M[i][j]==1 && visited[j]==false)
+                dfs(j,M,visited);
         }
     }
     int findCircleNum(vector<vector<int>>& M) {
         int ans=0,n=M.size();
         vector<bool>visited(n,false);
-        vector<vector<int>>adjList(n);
-        //making adjacency list
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(M[i][j]==1) adjList[i].push_back(j);
-            }
-        }
         
         for(int i=0;i<n;i++){
             if(visited[i]==false){
-                dfs(i,adjList,visited);
                 ans++;
+                dfs(i,M,visited);
             }
         }
         
