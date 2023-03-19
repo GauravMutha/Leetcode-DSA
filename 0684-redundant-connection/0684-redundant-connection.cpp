bede51203f0816d789611cfefac7e97ad7fa51c2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-19 11:09:28 +0530

    Time: 18 ms (17.68%), Space: 9.6 MB (24.23%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,55 +1,33 @@
-//Single  dfs with graph making
-//Better solution than Union find because UF was O(N*log(N))
-//O(N)
+//Multiple  dfs with graph making
+//Less optimal than other two solutions (single dfs, UF)
+//O(N^2) 
 class Solution {
-private:
-    int  cycleStart=-1;
-    unordered_set<int>cycleNodes;
-    void makeGraph(vector<vector<int>>& graph,vector<vector<int>>& edges){
-        for(int i=0;i<edges.size();i++){
-            int v=edges[i][1];
-            int u=edges[i][0];
-            
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-        }
-    }
 public:
-    void dfs(int curr,int parent,vector<int>& visited,vector<vector<int>>& graph){
+    bool dfs(int curr,int parent,vector<int>& visited,vector<vector<int>>& graph){
         if(visited[curr]==true) {
-            cycleStart=curr;
-            return;
+            return true;
         }
         visited[curr]=true;
         for(auto &adjNode : graph[curr]){
-            if(adjNode==parent) 
-                continue;
-            //If cycle is not empty then no need for traversing further so need for anymore dfs calls
-            if(cycleNodes.empty()) 
-                dfs(adjNode,curr,visited,graph);
-            
-            if(cycleStart != -1) 
-                cycleNodes.insert(curr);
-            
-            if(cycleStart==curr){
-                cycleStart=-1;
-                return;
-            }
+            if(adjNode==parent) continue;
+            if(dfs(adjNode,curr,visited,graph)) return true;
         }
+        return false;
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
         vector<int>visited(n+1,false);
         vector<vector<int>>graph(n+1);
         
-        makeGraph(graph,edges);
-        
-        dfs(1,-1,visited,graph);
-        
-        for(int i=edges.size()-1;i>=0;i--){
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
             
-            if(cycleNodes.count(edges[i][0]) && cycleNodes.count(edges[i][1])) 
-                return edges[i];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+            
+            fill(visited.begin(),visited.end(),false);
+            if(dfs(u,-1,visited,graph)) return edges[i];
         }
         
         return {};
