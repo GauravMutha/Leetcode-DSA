8a926df18dcf6db317a3e75425a4baf34bb7d3c2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-21 16:53:03 +0530

    Time: 551 ms (5.05%), Space: 14.9 MB (71.24%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,45 +1,34 @@
-//#1(b) DFS - Explicitly making a graph
-//It is Counting components , 1(a) was counting nodes
-//TC-->O(n^2) SC-->O(n^2)
+//#1(a) DFS - without making making a graph
+//Counting nodes
+//TC-->O(n^2) SC-->O(1)
+typedef vector<vector<int>> vvi;
 class Solution {
-private:
-    int components=0;
 public:
-    void dfs(int curr,vector<vector<int>>& graph,vector<bool>&visited){
-        visited[curr]=true;
+    void dfs(int i,int& count,vvi& stones,vector<bool>&visited){
+        visited[i]=true;
+        count++;
         
-        for(auto& adjNode : graph[curr]){
-            if(!visited[adjNode])
-                dfs(adjNode,graph,visited);
+        for(int j=0;j<stones.size();j++){
+            int iRow=stones[i][0] , iCol=stones[i][1];
+            int jRow=stones[j][0] , jCol=stones[j][1];
+            
+            if(i!=j && (iRow==jRow || jCol==iCol) && !visited[j]){
+                dfs(j,count,stones,visited);
+            }
         }
     }
-    int removeStones(vector<vector<int>>& stones) {
-        if(stones.size()==1) return 0; //only one stone, cannot be removed
-        int n=stones.size();
-        vector<vector<int>> graph(n);
+    int removeStones(vvi& stones) {
+        int n=stones.size() , res=0;
+        vvi graph(n);
         vector<bool>visited(n,false);
         
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int iRow=stones[i][0] , iCol=stones[i][1];
-                int jRow=stones[j][0] , jCol=stones[j][1];
-                
-                if(iRow==jRow || jCol==iCol) {
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
-            }
-        }
-        
-        for(int i=0;i<n;i++){
+            int count=0; //nodes counter in a single component
             if(!visited[i]){
-                dfs(i,graph,visited);
-                components++;
+                dfs(i,count,stones,visited);
+                res+=(count-1); //root of any component will not be deleted hence -1
             } 
         }
-        /*component count is bascially counting roots of components
-        which will be left behind always , so n-components is all
-        the stones that has beem removed*/
-        return n-components;
+        return res;
     }
 };
