351b714f39b37e7b31d3b4f5ae4bc5e3c9019e4f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-21 16:56:18 +0530

    Time: 546 ms (5.05%), Space: 14.9 MB (71.03%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,34 +1,32 @@
-//#1(a) DFS - without making making a graph
-//Counting nodes
+//#1(d) DFS - without making making a graph
+//Counting components
 //TC-->O(n^2) SC-->O(1)
 typedef vector<vector<int>> vvi;
 class Solution {
 public:
-    void dfs(int i,int& count,vvi& stones,vector<bool>&visited){
+    void dfs(int i,vvi& stones,vector<bool>&visited){
         visited[i]=true;
-        count++;
         
         for(int j=0;j<stones.size();j++){
             int iRow=stones[i][0] , iCol=stones[i][1];
             int jRow=stones[j][0] , jCol=stones[j][1];
             
             if(i!=j && (iRow==jRow || jCol==iCol) && !visited[j]){
-                dfs(j,count,stones,visited);
+                dfs(j,stones,visited);
             }
         }
     }
     int removeStones(vvi& stones) {
-        int n=stones.size() , res=0;
+        int n=stones.size()  ,components=0;
         vvi graph(n);
         vector<bool>visited(n,false);
         
         for(int i=0;i<n;i++){
-            int count=0; //nodes counter in a single component
             if(!visited[i]){
-                dfs(i,count,stones,visited);
-                res+=(count-1); //root of any component will not be deleted hence -1
+                dfs(i,stones,visited);
+                components++;
             } 
         }
-        return res;
+        return n-components;
     }
 };
