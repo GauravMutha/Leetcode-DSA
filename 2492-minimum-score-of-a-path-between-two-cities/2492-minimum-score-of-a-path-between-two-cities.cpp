733a3942f0d2f9c9696e087b163597a80e2b3286
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-23 13:03:22 +0530

    Time: 432 ms (92.64%), Space: 125.5 MB (72.09%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,33 +1,28 @@
-//Simple DFS
+//BFS
 class Solution {
-private:
-    int res=INT_MAX;
 public:
-    void dfs(int curr,vector<vector<pair<int,int>>>& graph,vector<bool>& visited){
-        visited[curr]=true;
-        for(auto &[adjNode,dist] : graph[curr]){
-            /*we might not want to visit a visited
-            node but we must consider the untravelled 
-            edges as they may have minimum path
-            hence we update ans here too*/
-            res=min(res,dist); 
-            if(!visited[adjNode]){
-                res=min(res,dist);
-                dfs(adjNode,graph,visited);
-            }
-        }
-    }
     int minScore(int n, vector<vector<int>>& roads) {
+        int res=INT_MAX;
         vector<vector<pair<int,int>>>graph(n+1);
         vector<bool>visited(n+1,false);
+        queue<int>q;
         for(int i=0;i<roads.size();i++){
             int u=roads[i][0] ,v=roads[i][1], dist=roads[i][2];
             
             graph[u].push_back({v,dist});
             graph[v].push_back({u,dist});
         }
-        
-        dfs(1,graph,visited);
+        visited[1]=true;
+        q.push(1);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto &[adjNode,dist] : graph[node]){
+                res=min(res,dist);
+                if(!visited[adjNode]) 
+                    q.push(adjNode) , visited[adjNode]=true;
+            }
+        }
         
         return res;
     }
