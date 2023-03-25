b8f4c3d5e7934186352ebb3d46e9af257831df4e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-25 09:28:33 +0530

    Time: 122 ms (63.33%), Space: 42.7 MB (70.83%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.cpp
+++ 0851-loud-and-rich/0851-loud-and-rich.cpp
@@ -1,34 +1,38 @@
-//DFS (it corrects the first submission that gave wrong answer)
-//O(N^2)
+//Toplogical Sort using BFS - Kahn's Algorithm
 class Solution {
 public:
-    int dfs(int curr,vector<vector<int>>& graph,vector<int>& quiet,vector<bool>& visited,vector<int>& ans){
-        int bestNode=curr;
-        if (visited[curr]) return ans[curr];
-        visited[curr]=true;
-        for(auto &adjNode: graph[curr]){
-            int retNode=dfs(adjNode,graph,quiet,visited,ans);
-            if(quiet[retNode]<quiet[bestNode])
-                bestNode=retNode;
-        }
-        ans[curr]=bestNode;
-        return bestNode;
-    }
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
         int n=quiet.size();
         vector<vector<int>>graph(n);
-        vector<int>ans(n);
-        vector<bool>visited(n);
+        vector<int>ans(n,-1);
+        vector<int>indeg(n);
+        queue<int>q;
         
+        //making graph and indegree vector
         for(int i=0;i<richer.size();i++){
             int u=richer[i][0] ,v=richer[i][1];
-            graph[v].push_back(u);
+            
+            graph[u].push_back(v);
+            indeg[v]++;
+        }
+        //pushig 0 indegree nodes in queue AND initialising ans vector
+        for(int i=0;i<n;i++){
+            ans[i]=i;
+            
+            if(indeg[i]==0) q.push(i);
         }
         
-        for(int i=0;i<n;i++){
-            if(!visited[i])
-                dfs(i,graph,quiet,visited,ans);
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            for(auto &adjNode : graph[curr]){
+                if(quiet[ans[adjNode]]>quiet[ans[curr]])
+                    ans[adjNode]=ans[curr];
+                indeg[adjNode]--;
+                if(indeg[adjNode]==0) q.push(adjNode);
+            }
         }
+        
         return ans;
     }
 };
