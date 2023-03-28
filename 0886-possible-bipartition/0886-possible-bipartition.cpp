ce5b75dae57e244c3ca4049ea18277d18ef7c8fd
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-28 16:10:31 +0530

    Time: 201 ms (76.07%), Space: 65.1 MB (54.45%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,31 +1,20 @@
-//DSU
+//Odd loop detection - Similar to previous dfs
 class Solution {
 public:
-    int findParent(int node,vector<int>& parent){
-        if(parent[node]==node){
-            return node;
+    bool dfs(int curr,vector<int>& dist,vector<vector<int>>& graph){
+        for(auto &adjNode : graph[curr]){
+            if(dist[adjNode]>-1 && (dist[adjNode]-dist[curr]+1)%2)
+                return false;
+            else if(dist[adjNode]==-1){
+                dist[adjNode] = dist[curr] + 1;
+                if(!dfs(adjNode,dist,graph)) return false;
+            }
         }
-        int temp=findParent(parent[node],parent);
-        parent[node]=temp;
-        return temp;
-    }
-    void doUnion(int x,int y,vector<int>& parent,vector<int>& rank){
-        int xParent=findParent(x,parent);
-        int yParent=findParent(y,parent);
-        
-        if(xParent==yParent){
-            return;
-        }
-        if(rank[xParent]<rank[yParent])
-            parent[xParent]=yParent;
-        else if(rank[yParent]<rank[xParent])
-            parent[yParent]=xParent;
-        else 
-            parent[yParent]=xParent , rank[xParent]++;
+        return true;
     }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<vector<int>>graph(n+1);
-        vector<int>parent(n+1),rank(n+1,0);
+        vector<int>dist(n+1,-1);
         
         for(int i=0;i<dislikes.size();i++){
             int u=dislikes[i][0] , v=dislikes[i][1];
@@ -34,15 +23,10 @@ public:
             graph[v].push_back(u);
         }
         
-        
-        for(int i=0;i<=n;i++) parent[i]=i;
-        
         for(int i=0;i<=n;i++){
-            for(int j=0;j<graph[i].size();j++){
-                if(parent[i]==parent[graph[i][j]]) return false;
-                
-                doUnion(graph[i][0],graph[i][j],parent,rank);
-            }
+            if(dist[i]>-1) continue;
+            dist[i]=0;
+            if(!dfs(i,dist,graph)) return false;
         }
         
         return true;
