22ac78f12ef07a203ca242e98081b72e15a0016e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-30 15:31:49 +0530

    Time: 123 ms (92.98%), Space: 40 MB (90.53%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -1,32 +1,31 @@
-//Similar to graph coloring concept in problem 785 and 886
+//NO DFS OR BFS
 class Solution {
 public:
-    void dfs(int curr,vector<vector<int>>& graph,vector<int>& ans){
-        int nextNode=-1,flowerType;
-        bool available[5]={false};
-        for(auto adjNode : graph[curr]){
-            if(ans[adjNode-1]==-1) nextNode=adjNode;
-            else available[ans[adjNode-1]]=true;
-        }
-        for(int i=1;i<5;i++) if(available[i]==false) flowerType=i;
-        ans[curr-1]=flowerType;
-        if(nextNode!=-1) dfs(nextNode,graph,ans);
-    }
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
-        vector<vector<int>>graph(n+1);
+        vector<vector<int>>graph(n);
         vector<int>ans(n,-1);
         
         for(int i=0;i<paths.size();i++){
             int u=paths[i][0] , v=paths[i][1];
             
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+            graph[u-1].push_back(v-1);
+            graph[v-1].push_back(u-1);
+        }
+        for(int i=0;i<n;i++){
+            bool available[5]={false};
+            int flowerType=1;
+            for(auto &adjNode: graph[i]){
+                if(ans[adjNode]!=-1) 
+                    available[ans[adjNode]]=true;
+            }
+            for(int i=1;i<5;i++){
+                if(available[i]==true) continue;
+                flowerType=i;
+                break;
+            }
+            ans[i]=flowerType;
         }
         
-        for(int i=1;i<=n;i++){
-            if(ans[i-1]==-1)
-                dfs(i,graph,ans);
-        }
         return ans;
     }
 };
