b97c9fca1bf0ad1748b790941a7138ad5a0ade47
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-30 17:22:27 +0530

    Time: 28 ms (35.00%), Space: 13.7 MB (71.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,30 +1,38 @@
 //Cycle detection in directed graph
-//BFS Kahn's Algorithm
+//Simple DFS
+//This answer is not based on TopoSort criteria/kahn's approach
+//Although we can apply kahn's approach using DFS too.
 class Solution {
 public:
+    bool dfs(int curr,vector<bool>&visited,vector<bool>&pathVis,vector<vector<int>>& graph){
+        visited[curr]=true;
+        pathVis[curr]=true;
+        
+        for(auto adjNode: graph[curr]){
+            if(visited[adjNode]==true && pathVis[adjNode]==true)
+                return false;
+            else if(visited[adjNode]==false){
+                if(!dfs(adjNode,visited,pathVis,graph)) return false;
+            }
+        }
+        pathVis[curr]=false;
+        return true;
+    }
     bool canFinish(int n, vector<vector<int>>& pre) {
         int topSortedCount=0; //count nodes that are sorted topologically
         vector<vector<int>>graph(n);
-        vector<int>indeg(n);
-        queue<int>q;
+        vector<bool>visited(n,false),pathVisited(n,false);
         for(int i=0;i<pre.size();i++){
             int u=pre[i][0] , v=pre[i][1];
             graph[v].push_back(u);
-            indeg[u]++;
         }
-        
-        for(int i=0;i<n;i++) if(indeg[i]==0) q.push(i);
-        
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-            topSortedCount++;
-            
-            for(auto &adjNode: graph[curr]){
-                indeg[adjNode]--;
-                if(indeg[adjNode]==0) q.push(adjNode);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(!dfs(i,visited,pathVisited,graph))
+                    return false;
             }
         }
-        return (topSortedCount==n);
+        
+        return true;
     }
 };
