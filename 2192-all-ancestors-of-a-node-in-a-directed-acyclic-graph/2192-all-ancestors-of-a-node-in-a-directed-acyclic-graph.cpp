34a27829cb992b1c6f6247fe26b503f1300228b3
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-30 10:04:08 +0530

    Time: 616 ms (18.11%), Space: 140.5 MB (38.70%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,25 +1,36 @@
-//Simple DFS WITHOUT visited array
-//Brute force type
-//TC-> O(N^2)
+//BFS Kahn's Algorithm
 class Solution {
 public:
-    void dfs(int curr,const int start,vector<vector<int>>& graph,vector<vector<int>>& ans){
-        for(auto ch:graph[curr]){
-            if(ans[ch].size()==0 || ans[ch].back()!=start){
-                ans[ch].push_back(start);
-                dfs(ch,start,graph,ans);
-            }
-        }
-    }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>>graph(n),ans(n);
+        vector<set<int>>setVector(n); //initial res but is a vector of set
+        vector<int>indeg(n);
+        queue<int>q;
+        vector<vector<int>>graph(n),res2(n); //this res2 is what we will return
         for(int i=0;i<edges.size();i++){
             int u=edges[i][0] , v=edges[i][1];
             graph[u].push_back(v);
+            indeg[v]++;
         }
         for(int i=0;i<n;i++){
-            dfs(i,i,graph,ans);
+            if(indeg[i]==0) q.push(i);
         }
-        return ans;
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            
+            for(auto adjNode: graph[curr]){
+                setVector[adjNode].insert(curr);
+                for(auto it:setVector[curr])
+                    setVector[adjNode].insert(it);
+                
+                indeg[adjNode]--;
+                if(indeg[adjNode]==0) q.push(adjNode);
+            }
+            
+        }
+        for(int i=0;i<n;i++){
+            res2[i]=vector<int>(setVector[i].begin(),setVector[i].end());
+        }
+        return res2;
     }
 };
