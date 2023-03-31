13ff20c2a5096cc9fabb5c57f8cfcf18d491aae0
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-31 07:12:13 +0530

    Time: 29 ms (25.75%), Space: 14.2 MB (32.89%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,30 +1,38 @@
-//BFS kahn's
+//DFS
 class Solution {
+private:
+    vector<int>res; //count nodes that are sorted topologically
 public:
+    bool dfs(int curr,vector<bool>&visited,vector<bool>&pathVis,vector<vector<int>>& graph){
+        visited[curr]=true;
+        pathVis[curr]=true;
+        
+        for(auto adjNode: graph[curr]){
+            if(visited[adjNode]==true && pathVis[adjNode]==true)
+                return false;
+            else if(visited[adjNode]==false){
+                if(!dfs(adjNode,visited,pathVis,graph)) return false;
+            }
+        }
+        pathVis[curr]=false;
+        res.push_back(curr);
+        return true;
+    }
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        vector<int>res; //stores nodes that are sorted topologically
         vector<vector<int>>graph(n);
-        vector<int>indeg(n);
-        queue<int>q;
+        vector<bool>visited(n,false),pathVisited(n,false);
         for(int i=0;i<pre.size();i++){
             int u=pre[i][0] , v=pre[i][1];
             graph[v].push_back(u);
-            indeg[u]++;
         }
-        
-        for(int i=0;i<n;i++) if(indeg[i]==0) q.push(i);
-        
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-            res.push_back(curr);
-            
-            for(auto &adjNode: graph[curr]){
-                indeg[adjNode]--;
-                if(indeg[adjNode]==0) q.push(adjNode);
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                if(!dfs(i,visited,pathVisited,graph))
+                    return vector<int>();
             }
         }
-        return (res.size()==n)?res:vector<int>();
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
 /*Exactly similar question 207.Course Schedule I
