e5c1d0562f902d4c60feb4ddb83e669fbcb19c14
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-03-31 09:08:24 +0530

    Time: 196 ms (87.50%), Space: 60.6 MB (86.11%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,33 +1,40 @@
-//DFS Reversing Graph
+//BFS Kahn's
+//Mapping all nodes to their ancestors using isReachable
 class Solution {
 public:
-    bool dfs(int curr,int target,vector<bool>& visited,vector<vector<int>>& revGraph){
-        visited[curr]=true;
-        if(curr==target) return true;
-        for(auto adjNode : revGraph[curr]){
-            if(!visited[adjNode]){
-                if(dfs(adjNode,target,visited,revGraph)) return true;
-            }
-        }
-        return false;
-    }
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& queries) {
-        vector<vector<int>>revGraph(n);
         vector<bool>res(queries.size(),false);
+        queue<int>q;
+        vector<int>indeg(n,0);
+        vector<vector<int>>graph(n);
+        vector<vector<bool>>isReachable(n,vector<bool>(n,false));
         for(int i=0;i<pre.size();i++){
             int u=pre[i][0] , v=pre[i][1];
             
-            revGraph[v].push_back(u);
+            graph[u].push_back(v);
+            indeg[v]++;
         }
+        for(int i=0;i<n;i++)
+            if(indeg[i]==0) 
+                q.push(i);
         
+        while(!q.empty()){
+            int parent=q.front();
+            q.pop();
+            for(auto &child : graph[parent]){
+                isReachable[parent][child]=true;
+                //But also parent of current parent should be set true;
+                for(int i=0;i<n;i++)
+                    if(isReachable[i][parent]) isReachable[i][child]=true;
+                indeg[child]--;
+                if(indeg[child]==0) q.push(child);
+            }
+        }
         for(int i=0;i<queries.size();i++){
-            int start=queries[i][1] , target=queries[i][0];
-            vector<bool>visited(n,false);
-            res[i]=dfs(start,target,visited,revGraph);
+            res[i]=isReachable[queries[i][0]][queries[i][1]];
         }
-        
         return res;
     }
 };
-//Concept of starting dfs traversal from a node and checking if target can be reached
-//Similar to 2192.All Ancestors of a node in a directed Acyclic Graph
+
+//Very similar to 2192. All ancestors of a node directed acyclic graph
