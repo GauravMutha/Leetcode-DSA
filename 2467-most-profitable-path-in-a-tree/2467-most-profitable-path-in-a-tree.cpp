fd75d858392ccb0bff318ff3653dd19f26e1298c
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-03 08:42:40 +0530

    Time: 420 ms (99.00%), Space: 156.4 MB (70.33%) - LeetHub

--- 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
@@ -1,51 +1,43 @@
-//2 DFS
+//1 DFS
 class Solution {
 public:
-    int dfs2(int curr,int par,vector<int>& amount , vector<vector<int>>& graph){
-        int currVal=amount[curr];
-        int maxVal=INT_MIN;
+    int dfs(int curr,int par,int d,int bob,vector<int>&bobDist,vector<int>&amount,vector<vector<int>>& graph){
+        int sum=0; //decided by comparison between depth aka d(alice distance from 0) and bobDist[curr]
+        int maxRet=INT_MIN; //the sum that child of curr will return
         
-        for(auto adj : graph[curr]){
-            if(adj!=par)
-                maxVal=max(maxVal,dfs2(adj,curr,amount,graph));
+        if(curr==bob) bobDist[curr]=0; //so nodes in the path upwards, from bob, have distance values starting 0;
+        
+        for(auto adj: graph[curr]){
+            if(adj!=par){
+                maxRet=max(maxRet,dfs(adj,curr,d+1,bob,bobDist,amount,graph));
+                //nodes in bob to 0 path upwards get dist value wrt to bob which is 0, so 0,1,2....
+                bobDist[curr]=min(bobDist[curr],bobDist[adj]+1);
+            }
         }
-        //if curr was a leaf node , it never entered for loop to update maxVal
-        if(maxVal==INT_MIN) return currVal;
-        else return maxVal+currVal;
-    }
-    void dfs1(int curr,int par,int d,vector<int>& parent,vector<int>& distances,vector<vector<int>>& graph){
-        distances[curr]=d;
-        parent[curr]=par;
-        for(auto adj : graph[curr]){
-            if(adj!=par) dfs1(adj,curr,d+1,parent,distances,graph);
-        }
+        
+        //if alice meet bob at curr node then half of the nodes value will be added into sum
+        if(d==bobDist[curr]) sum+=amount[curr]/2;
+        
+        //If current node's distance is more than its d (distance of node form 0) then Alice reaches it first
+        else if(d<bobDist[curr]) sum+=amount[curr];
+        
+        if(maxRet==INT_MIN) return sum;
+        else return sum+maxRet;
     }
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
-        int n=amount.size();
+        int n=amount.size(),depth=0; //depth also represent distance of alice from 0
         vector<vector<int>>graph(n);
-        vector<int>dist(n),parent(n);
+        
+        /*distance of every node from bob init to maximum
+         possible value i.e. n */
+        vector<int>bobDist(n,n); 
+        
         for(int i=0;i<edges.size();i++){
             int u=edges[i][0],v=edges[i][1]; 
             graph[u].push_back(v);
             graph[v].push_back(u);
         }
         
-        dfs1(0,0,0,parent,dist,graph);
-        
-        int b=bob;
-        int aliceDist=0;
-        while(b!=0){
-            //if alice ever meets bob at a node then that node amount is made half
-            if(aliceDist==dist[b])
-                amount[b]/=2;
-            
-            //if bob has crossed a node and alice has not
-            else if(aliceDist<dist[b])
-                amount[b]=0;    
-            b=parent[b];
-            aliceDist++;
-        }
-        
-        return dfs2(0,0,amount,graph);
+        return dfs(0,0,depth,bob,bobDist,amount,graph);
     }
 };
