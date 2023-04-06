7825df86e9120412a735f9645aa8475bb9f9edcc
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-06 10:51:21 +0530

    Time: 2 ms (78.63%), Space: 7.1 MB (56.00%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,34 +1,17 @@
+//>>>//
+
 class Solution {
 public:
-    int sum=0;
-    int subsetXORSum(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ds;
+    int subsetXORSum(vector<int>& nums,int i=0,int sum=0) {
         
-        calc(nums,ds,0);
-        return sum;
-    }
-    void calc(vector<int>&nums,vector<int>&ds,int ind){
-        int x=0;
-        if(ind==nums.size()){
-            if(ds.size()==0){
-                return; 
-            }
-            else if(ds.size()==1){
-                x=ds[0];
-            }
-            else{
-                x=ds[0];
-                for(int i=1;i<ds.size();i++){
-                    x^=ds[i];
-                }
-            }
-            sum+=x;
-            return;
+        if(i>=nums.size()){
+            return sum;
         }
-        ds.push_back(nums[ind]);
-        calc(nums,ds,ind+1);
-        ds.pop_back();
-        calc(nums,ds,ind+1);    
+        sum^=nums[i];
+        int sumIfPicked=subsetXORSum(nums,i+1,sum);
+        sum^=nums[i]; //XORing with the same number is equivalent to popping
+        int sumIfNotPicked=subsetXORSum(nums,i+1,sum);
+        
+        return sumIfPicked + sumIfNotPicked;
     }
 };
