15cf4f39b502f0cfea9194f26e402f97cf51946b
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-07 15:49:02 +0530

    Time: 5 ms (83.57%), Space: 9 MB (71.87%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,25 +1,31 @@
 //Re//
-//Swapping method
+//visited array method (+sorting)
 class Solution {
 private:
 vector<vector<int>>ans;
 public:
-    void helper(int pos,vector<int>& nums){
-        if(pos>=nums.size()){
-            ans.push_back(nums);
+    void helper(vector<int>& ds,vector<bool>& visited,vector<int>& nums){
+        if(ds.size()>=nums.size()){
+            ans.push_back(ds);
             return;
         }
-        unordered_map<int,bool> vis;
-        for(int i=pos;i<nums.size();i++){
-            if(vis.find(nums[i])!=vis.end()) continue;
-            vis[nums[i]]=true;
-            swap(nums[i],nums[pos]);
-            helper(pos+1,nums);
-            swap(nums[i],nums[pos]);
+        
+        for(int i=0;i<nums.size();i++){
+            if(visited[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !visited[i-1]) continue;
+//so we take the duplicate only if its previous similar element has been taken
+            visited[i]=true;
+            ds.push_back(nums[i]);
+            helper(ds,visited,nums);
+            visited[i]=false;
+            ds.pop_back();
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        helper(0,nums);
+        sort(begin(nums),end(nums));
+        vector<bool>visited(nums.size());
+        vector<int>ds;
+        helper(ds,visited,nums);
         
         return ans;
     }
