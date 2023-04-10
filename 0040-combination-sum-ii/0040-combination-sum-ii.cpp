9ccd89a770e5467eb44cccec6d795a21e2aea53d
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-10 11:48:11 +0530

    Time: 3 ms (90.73%), Space: 10.8 MB (41.82%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,21 +1,23 @@
 //Re//
 //Concept of both, Subsets II + Combination I
+//Uses "single recursive call inside for loop" method
 class Solution {
     vector<vector<int>>ans;
 public:
-    void helper(int i,int target,vector<int>& ds,vector<int>& nums){
+    void helper(int pos,int target,vector<int>& ds,vector<int>& nums){
         if(target==0){
             ans.push_back(ds);
             return;
         }
-        if(i>=nums.size() || target<0) return;
-        ds.push_back(nums[i]);
-        helper(i+1,target-nums[i],ds,nums);
-        ds.pop_back();
-        int x=nums[i];
-        while(i<nums.size()-1 && x==nums[i+1]) 
-            i++;
-        helper(i+1,target,ds ,nums);
+        if(pos>=nums.size() || target<0) return;
+        for(int i=pos;i<nums.size();i++){
+            if(i!=pos && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            target-=nums[i];
+            helper(i+1,target,ds,nums);
+            ds.pop_back();
+            target+=nums[i];
+        }
     }
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
