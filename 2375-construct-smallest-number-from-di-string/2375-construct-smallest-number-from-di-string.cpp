2e4286c00bbc15dd2834f1e4ed00d386e8e14bb8
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-15 08:26:39 +0530

    Time: 3 ms (54.26%), Space: 6.1 MB (44.59%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,37 +1,31 @@
-//Naive , Brute force
+//Slight optimisation
 class Solution {
 private:
-    vector<string>ans;
+    string ans="";
 public:
-    void helper(string & ds,vector<bool>& visited,int szLimit){
+    void helper(string & ds,vector<bool>& visited,int szLimit,string& patt){
+        if(ans.size()) return;
         if(ds.size()==szLimit){
-            ans.push_back(ds);
+            ans=ds;
             return;
         }
-        for(int i=1;i<=9;i++){
-            if(visited[i]) continue;
-            ds.push_back(i+'0');
-            visited[i]=true;
-            helper(ds,visited,szLimit);
+        for(char c='1';c<='9' && ans.size()==0;c++){
+            if(visited[c]) continue;
+            if(ds.size()>0 && ((patt[ds.size()-1]=='I' && c<ds.back()) || (patt[ds.size()-1]=='D' && c>ds.back())))
+                break;
+            ds.push_back(c);   
+            visited[c]=true;
+            helper(ds,visited,szLimit,patt);
             ds.pop_back();
-            visited[i]=false;
+            visited[c]=false;
         }
     }
-    string smallestNumber(string pattern) {
-        int szLimit=pattern.size()+1;
+    string smallestNumber(string patt) {
+        int szLimit=patt.size()+1;
         string ds;
-        vector<bool>visited(10,false);
-        helper(ds,visited,szLimit);
-        for(auto & str : ans){
-            int i=0;
-            for(i=0;i<pattern.size();i++){
-                if((pattern[i]=='I' && str[i+1]>str[i]) || (pattern[i]=='D' && str[i+1]<str[i]) )
-                    continue;
-                break;
-            }
-            if(i>=pattern.size()) return str;
-        }
-        
-        return "";
+        vector<bool>visited(127,false);
+        helper(ds,visited,szLimit,patt);
+
+        return ans;
     }
 };
