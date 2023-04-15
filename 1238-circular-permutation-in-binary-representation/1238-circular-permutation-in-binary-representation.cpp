82eae88a4147edfc3d9ffdf7aae059f0a3bb542b
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-15 14:58:27 +0530

    Time: 30 ms (21.78%), Space: 20.8 MB (22.67%) - LeetHub

--- 1238-circular-permutation-in-binary-representation/1238-circular-permutation-in-binary-representation.cpp
+++ 1238-circular-permutation-in-binary-representation/1238-circular-permutation-in-binary-representation.cpp
@@ -1,11 +1,29 @@
+//Genrate all gray codes first then filling 'ans' from 'start'
+//Similar to 89.Gray Codes
 class Solution {
 public:
-vector<int> circularPermutation(int n, int start) {
-        vector<int>res;
-        int x=pow(2,n);
-        for(int i=0;i<x;i++){
-            res.push_back(start^i^i>>1);
+    vector<int> generate(int n){
+        if(n==0) return {0};
+        vector<int> vec=generate(n-1);
+        int mask=1<<(n-1) , sz=vec.size();
+        for(int i=sz-1;i>=0;i--){
+            int temp=vec[i];
+            temp|=mask;
+            vec.push_back(temp);
         }
-    return res;
-    } 
+        return vec;
+    }
+    vector<int> circularPermutation(int n, int start) {
+        vector<int>gCodes=generate(n);
+        vector<int>ans;
+        bool startFound=false;
+        for(int i=0;ans.size()<gCodes.size();){
+            if(gCodes[i]==start) startFound=true;
+            if(startFound) ans.push_back(gCodes[i]);
+            i++;
+            i%=gCodes.size();
+        }
+        
+        return ans;
+    }
 };
