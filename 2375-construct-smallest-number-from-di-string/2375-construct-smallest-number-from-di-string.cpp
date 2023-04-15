858cfddd256a31d29010d8b263b8875b021204fc
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-15 10:10:26 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (44.59%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -1,31 +1,24 @@
-//Slight optimisation
+//Reversing the D blocks
+//Using stack
 class Solution {
-private:
-    string ans="";
 public:
-    void helper(string & ds,vector<bool>& visited,int szLimit,string& patt){
-        if(ans.size()) return;
-        if(ds.size()==szLimit){
-            ans=ds;
-            return;
+string smallestNumber(string pattern) {
+        string result;
+        stack<int> s;
+        for (int i = 0; i <= pattern.length(); i++)
+        {
+            s.push(i + 1);
+            if (i == pattern.length() || pattern[i] == 'I')
+            {
+                while (!s.empty())
+                {
+                    result += to_string(s.top());
+                    s.pop();
+                }
+            }
         }
-        for(char c='1';c<='9' && ans.size()==0;c++){
-            if(visited[c]) continue;
-            if(ds.size()>0 && ((patt[ds.size()-1]=='I' && c<ds.back()) || (patt[ds.size()-1]=='D' && c>ds.back())))
-                break;
-            ds.push_back(c);   
-            visited[c]=true;
-            helper(ds,visited,szLimit,patt);
-            ds.pop_back();
-            visited[c]=false;
-        }
+        
+        return result;
     }
-    string smallestNumber(string patt) {
-        int szLimit=patt.size()+1;
-        string ds;
-        vector<bool>visited(127,false);
-        helper(ds,visited,szLimit,patt);
 
-        return ans;
-    }
 };
