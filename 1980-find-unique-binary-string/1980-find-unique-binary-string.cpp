d040ba7be1a8455e3628cd32c946eedb1fe20a7d
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-16 09:13:25 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (24.35%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,14 +1,21 @@
+//TC-->O(2^n + n^2)
+//Brute force
 class Solution {
 public:
-    
     string findDifferentBinaryString(vector<string>& nums) {
-        string result;
-        for(int i=0;i<nums.size();i++)
-            result+=nums[i][i]=='1'?'0':'1';
-        return result;
+        int n=nums.size();
+        int x=0;
+        string ans;
+        vector<bool>hash(pow(2,n));
+        for(int i=0;i<nums.size();i++){
+            hash[stoi(nums[i],0,2)]=true;
+        }
+        for(int i=0;i<hash.size();i++){
+            if(hash[i]==false){
+                x=i;
+                break;
+            }
+        }
+        return bitset<16>(x).to_string().substr(16 - n);
     }
 };
-
-//Logic--->Cantor's Diagonalisation theorem
-//TC-->O(n)
-//SC-->O(i);
