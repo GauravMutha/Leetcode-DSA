e6d8b32e382426c4e2c8a31f99179ac0f1aa59ec
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-16 08:05:39 +0530

    Time: 11 ms (53.54%), Space: 12 MB (31.95%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -1,19 +1,12 @@
-//Using recursion + Bit manipulation
-//Pepcoding(Sumit Malik sir)
+//Using formula
 class Solution {
 public:
-    vector<int> generate(int n){
-        if(n==0) return {0};
-        vector<int> vec=generate(n-1);
-        int mask=1<<(n-1) , sz=vec.size();
-        for(int i=sz-1;i>=0;i--){
-            int temp=vec[i];
-            temp|=mask;
-            vec.push_back(temp);
-        }
-        return vec;
-    }
     vector<int> grayCode(int n) {
-        return generate(n);
+        vector<int>ans;
+        for(int i=0;i<pow(2,n);i++){
+            int code=i^(i>>1);
+            ans.push_back(code);
+        }
+        return ans;
     }
 };
