2327ad01a99ff429a7b65422292414aa4e39ddc0
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-17 09:02:22 +0530

    Time: 209 ms (12.43%), Space: 90.1 MB (5.67%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -1,24 +1,21 @@
 //Simple Backtracking similar to printing subsets.
-//Single recursion call inside a for loop
+//classic two recursion call pick not pick method
 class Solution {
 private: 
     vector<long long>ans;
 public:
-    void helper(long long start,vector<long long>& ds,long long target){
-        if(target<=0){
-            if(target==0)ans = vector<long long>(ds.begin(), ds.end());
-            return;
-        }
-        for(long long i=start;i<=target && ans.size()==0;i+=2){
-            ds.push_back(i);
-            helper(i+2,ds,target-i);
-            ds.pop_back();
-        }
+    bool helper(long long i,long long target){
+        if(target==0) return true;
+        if(target<i) return false;
+        ans.push_back(i);
+        if(helper(i+2,target-i)) return true;
+        ans.pop_back();
+        return helper(i+2,target);
     }
     vector<long long> maximumEvenSplit(long long finalSum) {
         if(finalSum%2) return {};
         vector<long long>ds;
-        helper(2,ds,finalSum);
+        helper(2,finalSum);
         
         return ans;
     }
