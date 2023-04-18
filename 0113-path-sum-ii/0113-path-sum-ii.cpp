972f739df4581c1927af1ce8f95ac43f7aca3f84
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-18 15:25:23 +0530

    Time: 17 ms (39.53%), Space: 20 MB (44.28%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -9,27 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//Slughtly optmisied DFS
-//We use only one vector vec and pass it by reference
-//we are able to do so because we pop back the node once we are done processing it i.e. we are following the backtracking method
+//Re//
+//Backtracking
 class Solution {
-public:
+private:
     vector<vector<int>>ans;
-    vector<vector<int>> pathSum(TreeNode* root, int k) {
-        vector<int>vec;
-        dfs(root,k,vec);
-        return ans;
-    }
-    void dfs(TreeNode* root , int k , vector<int>&vec){
-        if(!root) return;
+public:
+    void helper(TreeNode* currNode,vector<int>& ds,int sum,int target){
+        if(currNode==NULL) return;
         
-        vec.push_back(root->val);
-        if(!root->left && !root->right && (k==root->val))
-            ans.push_back(vec);
-        else{
-            dfs(root->left ,k-root->val,vec);
-            dfs(root->right ,k-root->val,vec);
+        sum+=currNode->val;
+        ds.push_back(currNode->val);
+        
+        if(!currNode->left && !currNode->right && sum==target){
+            ans.push_back(ds);
         }
-        vec.pop_back();
+        
+        helper(currNode->left,ds,sum,target);
+        helper(currNode->right,ds,sum,target);
+        
+        sum-=currNode->val;
+        ds.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int>ds;
+        helper(root,ds,0,targetSum);
+        
+        return ans;
     }
 };
