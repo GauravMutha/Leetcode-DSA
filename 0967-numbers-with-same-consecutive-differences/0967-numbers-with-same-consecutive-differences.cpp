f0d5c28e64124117e20f00dd8bbbf0c23d6f1802
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-18 07:57:22 +0530

    Time: 6 ms (32.49%), Space: 7.2 MB (85.02%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.cpp
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.cpp
@@ -1,25 +1,29 @@
 //Backtracking
+//Without String
 class Solution {
 private:
     vector<int>ans;
 public:
-    void helper(string &ds, int len,int k){
-        if(ds.size()==len){
-            ans.push_back(stoi(ds));
+    void helper(int &num,int digCnt, int len,int k){
+        if(digCnt==len){
+            ans.push_back(num);
             return;
         }
-        for(char i='0';i<='9';i++){
-            if(ds.size()==0 && i=='0') continue;
-            if(ds.size()==0 || abs(ds.back()-i)==k){
-                ds.push_back(i);
-                helper(ds,len,k);
-                ds.pop_back();
+        for(int i=0;i<=9;i++){
+            if(digCnt==0 && i==0) continue;
+            if(digCnt==0 || abs((num%10)-i)==k){
+                num*=10;
+                num+=i;
+                digCnt++;
+                helper(num,digCnt,len,k);
+                num/=10;
+                digCnt--;
             }
         }
     }
     vector<int> numsSameConsecDiff(int n, int k) {
-        string ds;
-        helper(ds,n,k);
+        int num=0;
+        helper(num,0,n,k);
         
         return ans;
     }
