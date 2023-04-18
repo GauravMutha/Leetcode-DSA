c6d724ec3e0f9007ae195fa0d1d5767a13751a3a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-18 08:50:55 +0530

    Time: 3 ms (70.04%), Space: 7.1 MB (96.69%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.cpp
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.cpp
@@ -1,29 +1,34 @@
-//Backtracking
-//Without String
+//bfs using queue
 class Solution {
-private:
-    vector<int>ans;
 public:
-    void helper(int &num,int digCnt, int len,int k){
-        if(digCnt==len){
-            ans.push_back(num);
-            return;
-        }
-        for(int i=0;i<=9;i++){
-            if(digCnt==0 && i==0) continue;
-            if(digCnt==0 || abs((num%10)-i)==k){
-                num*=10;
-                num+=i;
-                digCnt++;
-                helper(num,digCnt,len,k);
-                num/=10;
-                digCnt--;
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int>ans;
+        queue<int>q;
+        for(int i=1;i<=9;i++) q.push(i);
+        n--;
+        while(n--){
+            int qSz=q.size();
+            for(int i=0;i<qSz;i++){
+                int num=q.front();
+                q.pop();
+                int lastDig=num%10 , newDig=-1;
+                
+                if((lastDig-k)>=0){
+                    newDig=lastDig-k;
+                    q.push(num*10 + newDig);
+                }
+                /*For n-2,k=0 testcase , not
+                applying k!=0 condition results
+                in duplicate value insertion as
+                1-0 and 1+0  both gives 1 as newDig*/
+                if((lastDig+k)<=9 && k!=0){ 
+                    newDig=lastDig+k;
+                    q.push(num*10 + newDig);
+                }
             }
         }
-    }
-    vector<int> numsSameConsecDiff(int n, int k) {
-        int num=0;
-        helper(num,0,n,k);
+        
+        while(!q.empty()) ans.push_back(q.front()) , q.pop();
         
         return ans;
     }
