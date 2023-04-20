ffca0f74e701e7fd84d571ecd7a6a60a45c98bc3
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-20 16:19:43 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,25 @@
-//Backtracking
+//BFS
 class Solution {
-private:
-    vector<string>ans;
-    unordered_map<char,string>m;
 public:
-    void helper(int it,string& ds,string& digits){
-        if(it>=digits.size()){
-            ans.push_back(ds);
-        }
-        for(int i=0;i<m[digits[it]].size();i++){
-            ds.push_back(m[digits[it]][i]);
-            helper(it+1,ds,digits);
-            ds.pop_back();
-        }
-    }
     vector<string> letterCombinations(string digits) {
-        if(digits.size()==0) return {};
-        for(auto &ch : digits){
-            string letters="";
-            int number=ch-'0' , letterCnt=3;
-            char startChar=(number-2)*3+'a';
-            if(number==7) letterCnt++;
-            else if(number==8) startChar++;
-            else if(number==9) letterCnt++ , startChar++;
-            for(int i=1;i<=letterCnt;i++) letters.push_back(startChar++);
-            m[ch]=letters;
-        }
-        string ds="";
-        helper(0,ds,digits);
         
-        return ans;
+        if(digits.empty()) return vector<string>();
+        
+        deque<string>q;
+        vector<string>pad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        q.push_back("");
+        
+        for(auto &d : digits){
+            int padNum=d-'0';
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                string curr=q.front();
+                q.pop_front();
+                for(auto ch : pad[padNum])
+                    q.push_back(curr+ch);
+            } 
+        }
+        
+        return vector<string>(q.begin(),q.end());
     }
 };
