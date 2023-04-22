3e4adb8d466c2df174a9eab14f58c64d4934b720
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-04-22 08:22:23 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (76.48%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,38 +1,24 @@
-//Backtracking
-//Single recursive call in a for loop
-//Using integer bits as hash instead of bool arrays
-//Slightly optimised than before using preprocessing
+//Iterative 
+//Arry of bitsets , DP based
 class Solution {
-private:
-    int maxSz=0;
 public:
-    void helper(int pos,int currSz,int hash,vector<string>&arr,vector<int>& hashArr){
-        
-        maxSz=max(currSz,maxSz);
-        
-        for(int i=pos;i<arr.size();i++){
-            if(hashArr[i]&(1<<26)) continue;
-            if((hash & hashArr[i]) != 0) continue;
-            currSz+=arr[i].size();
-            helper(i+1,currSz,hash|hashArr[i],arr,hashArr);
-            currSz-=arr[i].size();
-        }
-    }
     int maxLength(vector<string>& arr) {
-        int hash=0;
-        vector<int>hashArr(arr.size(),0);
-        for(int i=0;i<arr.size();i++){
-            for(auto ch :arr[i]){
-                if(hashArr[i]&(1<<(ch-'a'))){
-                    hashArr[i]|=(1<<26);
-                    break;
-                }
-                hashArr[i]|=(1<<(ch-'a'));
+        vector<bitset<26>> results={bitset<26>()};
+        int res=0;
+        for(auto str : arr){
+            bitset<26>currStrHash;
+            for(auto ch : str)
+                currStrHash.set(ch-'a');
+            int charCount=currStrHash.count();
+            if(charCount< str.size()) continue; //string itself has duplicates
+            
+            for(int i=0;i<results.size();i++){
+                if((results[i]&currStrHash).any()) continue;
+                results.push_back(results[i]|currStrHash);
+                res=max(res,(int)results[i].count() + charCount);
             }
-        }  
+        }
         
-        helper(0,0,hash,arr,hashArr);
-        
-        return maxSz;
+        return res;
     }
 };
