37dc761209076ab5d902170e0e38019d1aefb713
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-04 18:39:24 +0530

    Time: 3 ms (38.43%), Space: 6 MB (77.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,20 @@
 //Tabulation(bottom up)
 //solving from lowest subproblem (final stair =0) to higher subproblems
+//removes the need for dp array
 //TC-->O(n)
-//SC-->O(n)
+//SC-->O(1)
 
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1);
-        dp[0]=dp[1]=1;
+        if(n==1) return 1;
+        int last=1,secondLast=1,curr=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            curr=last+secondLast;
+            secondLast=last;
+            last=curr;
         }
         
-        return dp.back();
+        return curr;
     }
 };
