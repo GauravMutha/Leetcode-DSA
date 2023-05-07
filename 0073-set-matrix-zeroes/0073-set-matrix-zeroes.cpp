ef0b0b6dca19d5ff711832f37895a7c699788077
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-07 16:42:16 +0530

    Time: 16 ms (52.83%), Space: 13.4 MB (38.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,33 +1,22 @@
-//best
-//SC-->O(1)
-//TC-->O(N)
-//2 Pass
+//Worst case space complexity is O(m+n)
+//TC-->O(m*n)
+//2 pass
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0=1,m=matrix.size(),n=matrix[0].size();
+        int m=matrix.size(),n=matrix[0].size();
+        set<int>rowBan,colBan;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    if(j!=0) matrix[0][j]=0;
-                    else col0=0;
-                }
+                if(matrix[i][j]==0)
+                    rowBan.insert(i) , colBan.insert(j);
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0)
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(rowBan.count(i) || colBan.count(j))
                     matrix[i][j]=0;
             }
-        }
-        if(matrix[0][0]==0){
-            for(int j=1;j<n;j++)
-                matrix[0][j]=0;
-        }
-        if(col0==0){
-            for(int i=0;i<m;i++)
-                matrix[i][0]=0;
         }
         
         return;
