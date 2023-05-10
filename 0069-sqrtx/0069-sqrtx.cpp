77961109c69a36a0b824c5890dededdb2136c5ac
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-10 10:05:37 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.23%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int mySqrt(int x) {
         if(x==0 || x==1) return x;
-        int start=1 ,mid,end=x;
+        int start=0 ,mid,end=x;
         while(start<=end){
             mid=start+(end-start)/2;
             long long prod =(long long)mid*mid;
-            cout<<start<<" "<<mid<<" "<<end<<endl;
+            // cout<<start<<" "<<mid<<" "<<end<<endl;
             if(prod == x) return mid;
             else if(prod>x) end=mid-1;
             else start=mid+1;
