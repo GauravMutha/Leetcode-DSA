cd3dfbe450f3a155e37a0144083e201c3cf50ae5
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-15 16:02:59 +0530

    Time: 6 ms (14.56%), Space: 7.3 MB (35.78%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,22 @@
+//Brte force
+//Using extra space
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<vector<int>>ans(n,vector<int>(n));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ans[j][n-1-i]=matrix[i][j];
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=ans[i][j];
+            }
+        }
+        
+        return;
+    }
+};
