dd3a86b5d1871c9d68fa0d6f3403577794f9001d
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-16 10:38:02 +0530

    Time: 10 ms (13.26%), Space: 9.4 MB (96.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,21 +1,24 @@
 //REVISE
-//Binary search
-//n+logm
+//Binary search without using STL method
+//log(n)+log(m)
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int r=matrix.size() , c=matrix[0].size();
-        // choosing row
-        int i=0;
-        for(i=0;i<r;i++){
-            if(matrix[i][c-1]>target)
-                break;
-            else if(matrix[i][c-1]==target) return true;
+        int r=matrix.size(),c=matrix[0].size();
+        
+        int start=0,end=r-1,mid=0;
+        // binary seach  on last column to select the row
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(matrix[mid][c-1]==target) return true;
+            else if(matrix[mid][c-1]>target) end=mid-1;
+            else if(matrix[mid][c-1]<target) start=mid+1;
         }
+        int i=start;
         if(i>=r) return false; //no row can contain the target
         
-        //performing binary search on chosen ith row
-        int start=0 , end=c-1 , mid=0;
+        //binary search on selected ith row
+        start=0 , end=c-1 , mid=0;
         
         while(start<=end){
             mid= start+ (end-start)/2;
