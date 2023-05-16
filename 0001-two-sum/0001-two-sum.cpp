e75799f49d55c0b7fb3bcb1f22e2bc7d59fc4cc5
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-16 15:04:07 +0530

    Time: 12 ms (79.84%), Space: 10.9 MB (37.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,27 @@
-//No sorting - Hash table
+//sorting - binary search
 class Solution {
 public:
+    int binarySearch(int i,long target, vector<pair<int,int>>& vec){
+        int start=i+1,end=vec.size()-1,mid=0;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if((long)vec[mid].first==target) return vec[mid].second;
+            else if((long)vec[mid].first>target) end=mid-1; 
+            else if((long)vec[mid].first<target) start=mid+1; 
+        }
+        
+        return -1;
+    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<long,int>m;
-        for(int i=0;i<nums.size();i++){
-            long require=(long)(target-nums[i]);
-            if(m.find(require)!=m.end())
-                return vector<int>({i,m[require]});
-            m[nums[i]]=i;
+        vector<pair<int,int>>vec;
+        for(int i=0;i<nums.size();i++)
+            vec.push_back({nums[i],i});
+        
+        sort(vec.begin(),vec.end());
+        
+        for(int i=0;i<vec.size();i++){
+            int j=binarySearch(i,target-vec[i].first,vec);
+            if(j!=-1) return vector<int>({vec[i].second,j});
         }
         
         return {};
