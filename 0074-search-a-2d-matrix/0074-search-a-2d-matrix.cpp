54dc167d4c48a07f3549b11b1f7ed608cb258cfe
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-16 11:21:13 +0530

    Time: 5 ms (55.02%), Space: 9.5 MB (75.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,15 +1,19 @@
 //Revise
-//Zig Zag method  / starting from top right corner
-//O(m) or O(n)
+//Binaery Search on matrix
+//Single pass binary search on entire matrix
+//O(log(m*n)) ~~ O(log(m)+log(n))
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int r=matrix.size() , c=matrix[0].size();
-        int i=0 , j=c-1;
-        while(i<r && j<c && i>=0 && j>=0){
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) j--;
-            else if(matrix[i][j]<target) i++;
+        int start=0 , end=(r*c)-1,mid=0;
+        
+        while(start<=end){
+            mid=start+(end-start)/2;
+            int curr=matrix[mid/c][mid%c];
+            if(curr==target) return true;
+            else if(curr>target) end=mid-1;
+            else if(curr<target) start=mid+1;
         }
         
         return false;
