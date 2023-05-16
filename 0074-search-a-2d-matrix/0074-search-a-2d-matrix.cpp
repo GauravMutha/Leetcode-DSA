64d21ee641a157e9167828bbf6b6ed0da81ef819
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-16 11:03:00 +0530

    Time: 3 ms (85.01%), Space: 9.5 MB (75.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,32 +1,17 @@
-//REVISE
-//Binary search without using STL method
-//log(n)+log(m)
+//Revise
+//Zig Zag method  / starting from top right corner
+//O(m) or O(n)
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int r=matrix.size(),c=matrix[0].size();
-        
-        int start=0,end=r-1,mid=0;
-        // binary seach  on last column to select the row
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(matrix[mid][c-1]==target) return true;
-            else if(matrix[mid][c-1]>target) end=mid-1;
-            else if(matrix[mid][c-1]<target) start=mid+1;
+        int r=matrix.size() , c=matrix[0].size();
+        int i=0 , j=c-1;
+        while(i<r && j<c && i>=0 && j>=0){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--;
+            else if(matrix[i][j]<target) i++;
         }
-        int i=start;
-        if(i>=r) return false; //no row can contain the target
         
-        //binary search on selected ith row
-        start=0 , end=c-1 , mid=0;
-        
-        while(start<=end){
-            mid= start+ (end-start)/2;
-            int curr=matrix[i][mid];
-            if(curr==target) return true;
-            else if(curr<target) start=mid+1;
-            else if(curr>target) end=mid-1;
-        }
         return false;
     }
 };
