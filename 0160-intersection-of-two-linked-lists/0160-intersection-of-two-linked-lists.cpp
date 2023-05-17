0dc74e754cbfda07e63d4da2141678434bae721e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-17 11:33:45 +0530

    Time: 48 ms (65.21%), Space: 14.5 MB (73.20%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,28 +6,17 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-//Better Implementation of the previous logic
+//Without Calculating difference of length
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if(!headA || !headB) return NULL;
         
-        int lenA=1,lenB=1;
         auto a=headA , b=headB;
-        while(a->next) a=a->next , lenA++;
-        while(b->next) b=b->next , lenB++;
-        
-        if(a!=b) return NULL;
-        
-        int diff=abs(lenA-lenB);
-        a=headA,b=headB;
-        if(lenA>lenB){
-            while(diff--) a=a->next;
+        while(a != b){
+            a=(a==NULL) ? a=headB : a->next;
+            b=(b==NULL) ? b=headA : b->next;
         }
-        else if(lenB>lenA){
-            while(diff--) b=b->next;
-        }
-        while(a!=b) a=a->next , b=b->next;
         return a;
     }
 };
