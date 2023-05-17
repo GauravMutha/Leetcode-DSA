c1d84ffbf4b73c882307ef436051d5d403f11f09
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-17 09:19:05 +0530

    Time: 7 ms (76.26%), Space: 11.7 MB (48.46%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -8,26 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//Same logic but using recursion
 class Solution {
 public:
+    void helper(ListNode* &p,int count,int target,ListNode* &tail,ListNode* &joint){
+        if(p==NULL) return;
+
+        count++;
+        if(p->next==NULL) joint=p;
+        if(count==target) tail=p;
+        
+        p=p->next;
+        helper(p,count,target,tail,joint);
+    }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==nullptr || head->next==nullptr || k==0) 
-            return head;
-        
-        int nodes=0;
-        
-        auto fast=head,slow=head;
-        while(fast) fast=fast->next , nodes++;
-        k%=nodes;
+        if(head==NULL || head->next==NULL || k==0) return head;
+        int total=0;
+        auto p=head;
+        while(p) p=p->next , total++;
+        k%=total;
         if(k==0) return head;
         
-        fast=head;
-        while(k--) fast=fast->next;
-        while(fast->next) fast=fast->next, slow=slow->next;
-        
-        fast->next=head;
-        head=slow->next;
-        slow->next=nullptr;
+        p=head;
+        ListNode *tail,*joint;
+        helper(p,0,total-k,tail,joint);
+        cout<<total-k;
+        joint->next=head;
+        head=tail->next;
+        tail->next=NULL;
         
         return head;
     }
