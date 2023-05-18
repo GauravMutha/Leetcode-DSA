f998012b4e7955da71e1af40a5d288e94b9cbc8f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-18 12:09:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (65.38%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,17 +1,17 @@
+//Brute force
 class Solution {
 public:
-    int strStr(string h, string n) {
-        int l=0,i=0,j=0;
-        for(int i=0;i<h.size();i++){
-            if(h[i]==n[j]){
-                l=i;
-                while(h[l]==n[j] && j<n.size() && i<h.size()) l++,j++;
-                if(j==n.size()) return i;
-                else j=0;
+    int strStr(string hay, string needle) {
+        int i=0,j=0,k=0;
+        for(int i=0;i<hay.size();i++){
+            if(hay[i]==needle[j]){
+                k=i;
+                while(hay[k]==needle[j] && k<hay.size() && j<needle.size())
+                    j++,k++;
+                if(j==needle.size()) return i;
+                j=0;
             }
         }
         return -1;
     }
 };
-
-//using brute force
