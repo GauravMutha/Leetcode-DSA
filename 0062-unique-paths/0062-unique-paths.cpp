4b6892d8515969227d8e665996f631a0f7bda7d2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-19 17:41:56 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (30.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,23 @@
-//Memoization
+//Tabulation
 class Solution {
 public:
-    int helper(int m,int n,vector<vector<int>>& dp){
+    int uniquePaths(int m, int n) {
         if(m==1 || n==1) return 1;
         
-        if(dp[m][n]!=-1) return dp[m][n];
+        vector<vector<int>>dp(m,vector<int>(n,-1));
         
-        int viaTop=helper(m-1,n,dp);
-        int viaLeft=helper(m,n-1,dp);
+        //initialising of the dp
+        for(int i=0;i<m;i++) dp[i][0]=1;
+        for(int j=0;j<n;j++) dp[0][j]=1;
+        dp[0][0]=0;
         
-        return dp[m][n]=(viaLeft+viaTop);
-    }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return helper(m,n,dp);
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
