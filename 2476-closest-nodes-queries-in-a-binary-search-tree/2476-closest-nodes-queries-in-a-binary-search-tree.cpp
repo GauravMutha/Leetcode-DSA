837a3a372ca05051d15a1abaa68839d0581d1d7e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-20 10:19:54 +0530

    Time: 405 ms (52.11%), Space: 157 MB (63.10%) - LeetHub

--- 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -12,8 +12,35 @@
 /*Previous submission gives TLE , because queries size is big,
 its better that we first create an sorted array of nodes and
 binary search there, , rather than searching tree every time*/
+
+//Implementing lower bound without STL
 class Solution {
 public:
+    int lowerBound(vector<int>& test,int target){
+        int start = 0, end = test.size() - 1, mid = 0;
+        bool found = false; // found is made true if the target itself gets found in the test array
+        while (start <= end)
+        {
+            mid = start + (end - start) / 2;
+            if (test[mid] == target)
+            {
+                found = true;
+                break;
+            }
+            else if (test[mid] > target)
+                end = mid - 1;
+            else if (test[mid] < target)
+                start = mid + 1;
+        }
+        int result;
+        if (found)
+            result = mid;
+        else if(start>=test.size())
+            result = -1;
+        else result=start;
+
+        return result;
+    }
     void inorder(TreeNode *curr, vector<int>& res) {
         if(curr==NULL) return;
         inorder(curr->left,res);
@@ -26,8 +53,8 @@ public:
         inorder(root,nodesArr);
         for(auto q:queries){
             
-            int cInd=lower_bound(nodesArr.begin(),nodesArr.end(),q)-nodesArr.begin();
-            int cVal=(cInd>=nodesArr.size())?-1 : nodesArr[cInd];
+            int cInd=lowerBound(nodesArr,q);
+            int cVal=(cInd==-1)?-1 : nodesArr[cInd];
             
             int fVal=-1;
             if(cVal==q) fVal=cVal;
