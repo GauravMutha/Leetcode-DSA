6e0582a6e636bd8bfbf1049dda39ba4554e6c9a6
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-22 16:16:01 +0530

    Time: 4 ms (43.28%), Space: 6 MB (75.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,21 @@
+//Recursive
 class Solution {
 public:
-    double myPow(double x, int nn) {
-        double ans=1.0;
-        long long n=nn;
-        if(n<0) n=-1.0*n;
-        while(n){
-            if(n%2){
-                ans=ans*x;
-                n=n-1;
-            }
-            else{
-                x=x*x;
-                n=n/2;
-            }
+    double helper(double x, int n){
+        if(n==0) return 1;
+        if((n%2)==0){
+            return helper(x*x,n/2);
         }
-        if(nn<0) ans=(double)(1.0)/(double)(ans);
+        else return x*helper(x*x,n/2);
+    }
+    double myPow(double x, int n) {
+        double ans=1.0;
+        if(n<0){
+            ans=helper(x,n);
+            return (double)(1.0)/(double)(ans);
+        }
         
-        return ans;
+        return helper(x,n);
     }
 };
+
