8fe655a7f3be6a21a2a70b51cf97c6938e3427e9
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-22 18:27:54 +0530

    Time: 4 ms (46.02%), Space: 7.8 MB (40.88%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,19 +1,9 @@
-//memoization
+//Tabulation
 class Solution {
 public:
-    int helper(int m,int n,vector<vector<int>>& dp,vector<vector<int>>& obstacles){
-        if(dp[m][n]!=-1) return dp[m][n];
-        
-        if(obstacles[m][n]==1) return 0;
-        
-        int viaTop=helper(m-1,n,dp,obstacles);
-        int viaLeft=helper(m,n-1,dp,obstacles);
-        
-        return dp[m][n]=(viaLeft+viaTop);
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacles) {
         int m=obstacles.size(),n=obstacles[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1)); 
+        vector<vector<int>>dp(m,vector<int>(n,0)); 
         
         //If the very first cell has obstacle,no other cell can be reached
         dp[0][0]=!obstacles[0][0];
@@ -21,7 +11,9 @@ public:
         
         /*here we pre-process DP so that first row and 
         column get defined about what they will return
-        .It helps to handle the base cases in recursion,too*/
+        based on obstacles array.Also such preprocessing
+        helps in base cases for memoization approach too
+        which was in the last submission*/
         for(int j=1;j<n;j++){
             if(dp[0][j-1]==0) dp[0][j]=0;
             else dp[0][j]=!obstacles[0][j];
@@ -32,6 +24,13 @@ public:
             else dp[i][0]=!obstacles[i][0];
         }
         
-        return helper(m-1,n-1,dp,obstacles);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacles[i][j]==1) continue;
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
