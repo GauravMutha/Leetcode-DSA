9c3736a65e7d47611fbc9f20c3527c0438753683
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-22 09:59:08 +0530

    Time: 5 ms (30.74%), Space: 6.7 MB (78.91%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,16 @@
+//Simple iterative
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>res;
+        for(int k=1;k<=numRows;k++){
+            vector<int>bag(k,1);
+            for(int i=0;k>1 && (i<res.back().size()-1) ;i++){
+                bag[i+1]=res.back()[i]+res.back()[i+1];
+            }
+            res.push_back(bag);
+        }
+        
+        return res;
+    }
+};
