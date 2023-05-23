3517fbe10e16183a932b0a8ed148ebf4ed6a6fe2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-23 18:11:08 +0530

    Time: 64 ms (78.59%), Space: 13.3 MB (63.23%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,36 +1,43 @@
-//using  the best approach of 3Sum 
-//O(n^3)
+//Using the generalized k-sum solution
+//Similar approach to the previous submission
 class Solution {
 private:
     vector<vector<int>>res;
 public:
-    void threeSum(int ind,vector<int>& nums,int firstFixed,long long target) {
-        
-        for(int i=ind+1;i<nums.size()-2;i++){
-            if(i!=(ind+1) && nums[i]==nums[i-1]) continue;
-            int l=i+1,r=nums.size()-1;
-            long long threeSumTarget=target-(long long)nums[i];
+    void dfs(int l,int r,long long target,int k,vector<int>& path,vector<int>& nums){
+        if(k==2){
             while(l<r){
                 long long sum=(long long)nums[l]+(long long)nums[r];
-                if(sum>threeSumTarget) r--;
-                else if(sum<threeSumTarget) l++;
+                if(sum>target) r--;
+                else if(sum<target) l++;
                 else{
-                    vector<int>quadruplets={firstFixed,nums[i],nums[l],nums[r]};
-                    res.push_back(quadruplets);
-                    while(l<r && nums[l]==quadruplets[2]) l++;
-                    while(l<r && nums[r]==quadruplets[3]) r--;
+                    path.push_back(nums[l]);
+                    path.push_back(nums[r]);
+                    res.push_back(path);
+                    path.pop_back();
+                    path.pop_back();
+                    
+                    while (l+1 < r && nums[l] == nums[l+1]) ++l;
+                    ++l; --r;
                 }
             }
         }
-        return;
+        for(int i=l;i<r;i++){
+            if(i>l && nums[i]==nums[i-1]) continue;
+            path.push_back(nums[i]);
+            dfs(i+1,r,target-(long long)nums[i],k-1,path,nums);
+            path.pop_back();
+        }
+    }
+    void kSum(vector<int>&nums,int target,int k){
+        vector<int>tmp;
+        dfs(0,(int)nums.size()-1,(long long)target,k,tmp,nums);
     }
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         if(nums.size()<4) return res; //an empty res is returned
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-3;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            threeSum(i,nums,nums[i],target-nums[i]);
-        }
+        
+        kSum(nums,target,4);
         
         return res;
     }
