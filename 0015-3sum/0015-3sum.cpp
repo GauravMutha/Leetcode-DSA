268abc8c19472dd63d756c13b6492a3b694c5574
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-23 16:06:05 +0530

    Time: 149 ms (79.46%), Space: 25.9 MB (57.46%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,25 @@
-//O(n^2*(log(n)))
-//better than O(n^3)
+//O(n^2)
 class Solution {
-private:
-    vector<vector<int>>res;
-    unordered_map<int,int>m;
 public:
-    void twoSum(vector<int>& nums, int target,int i) {
-        
-        for(int j=i+1;j<nums.size();j++){
-            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-            int require=target-nums[j];
-            if(m.find(require)!=m.end() && m[require]>j)
-                res.push_back({-target,nums[j],require});
-        }
-        
-        return;
-    }
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++) m[nums[i]]=i;
-        
+        vector<vector<int>>res;
         for(int i=0;i<nums.size()-2;i++){
-            if(i>0 && (nums[i]==nums[i-1]) ) continue;
-            if(nums[i]>0) return res;
-            
+            if(nums[i]>0) break;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int target=-nums[i];
-            twoSum(nums,target,i);
+            int l=i+1,r=nums.size()-1;
+            while(l<r){
+                if((nums[l]+nums[r])>target) r--;
+                else if((nums[l]+nums[r])<target) l++;
+                else{
+                    vector<int>triplets={nums[i],nums[l],nums[r]};
+                    res.push_back(triplets);
+                    while(l<r && nums[l]==triplets[1]) l++;
+                    while(l<r && nums[r]==triplets[2]) r--;
+                }
+            }
         }
-        
         return res;
     }
 };
