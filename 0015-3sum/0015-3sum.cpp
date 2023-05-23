dd927e1b2ec4767d74314874bcef3b618a61a214
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-23 13:21:05 +0530

    Time: 339 ms (39.31%), Space: 28.2 MB (47.66%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,31 +3,27 @@
 class Solution {
 private:
     vector<vector<int>>res;
+    unordered_map<int,int>m;
 public:
-    int binarySearch(int j,int target, vector<int>& nums){
-    int start=j+1,end=nums.size()-1,mid=0;
-    while(start<=end){
-        mid=start+(end-start)/2;
-        if(nums[mid]==target) return mid;
-        else if(nums[mid]>target) end=mid-1; 
-        else if(nums[mid]<target) start=mid+1; 
-    }
-    
-    return -1;
-    }
     void twoSum(vector<int>& nums, int target,int i) {
+        
         for(int j=i+1;j<nums.size();j++){
-            if(j>(i+1) && (nums[j]==nums[j-1]) ) continue;
-            int k=binarySearch(j,target-nums[j],nums);
-            if(k!=-1) res.push_back({-target,nums[j],nums[k]});
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+            int require=target-nums[j];
+            if(m.find(require)!=m.end() && m[require]>j)
+                res.push_back({-target,nums[j],require});
         }
         
         return;
     }
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++) m[nums[i]]=i;
+        
         for(int i=0;i<nums.size()-2;i++){
             if(i>0 && (nums[i]==nums[i-1]) ) continue;
+            if(nums[i]>0) return res;
+            
             int target=-nums[i];
             twoSum(nums,target,i);
         }
