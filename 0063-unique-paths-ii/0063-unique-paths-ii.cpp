3b393d0cbf4f76d1a5560f281bc8332830ba3abf
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-24 17:38:54 +0530

    Time: 4 ms (46.65%), Space: 7.8 MB (67.31%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,29 +1,22 @@
-//Tabulation+Space Optimisation
+//simplified memoization
 class Solution {
 public:
+    int helper(int m,int n,vector<vector<int>>& dp,vector<vector<int>>& obstacles){
+        if( (m>=0 && n>=0 && obstacles[m][n]==1) || (m<0 || n<0) ) 
+            return 0;
+        if(m==0 && n==0) return 1;
+        
+        if(dp[m][n]!=-1) return dp[m][n];
+        
+        int viaTop=helper(m-1,n,dp,obstacles);
+        int viaLeft=helper(m,n-1,dp,obstacles);
+        
+        return dp[m][n]=(viaLeft+viaTop);
+    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacles) {
         int m=obstacles.size(),n=obstacles[0].size();
-        vector<int>dp(n,0); 
+        vector<vector<int>>dp(m,vector<int>(n,-1)); 
         
-        //If the very first cell has obstacle,no other cell can be reached
-        dp[0]=!obstacles[0][0];
-        if(dp[0]==0) return 0;
-        
-        for(int j=1;j<n;j++){
-            if(dp[j-1]==0) dp[j]=0;
-            else dp[j]=!obstacles[0][j];
-        }
-        
-        for(int i=1;i<m;i++){
-            vector<int>tempDP(n,0);
-            for(int j=0;j<n;j++){
-                if(obstacles[i][j]==1) continue;
-                if(j==0) tempDP[0]=(dp[0]==0)?0:!obstacles[i][0];
-                else tempDP[j]=tempDP[j-1]+dp[j];
-            }
-            dp=move(tempDP);
-        }
-        
-        return dp[n-1];
+        return helper(m-1,n-1,dp,obstacles);
     }
 };
