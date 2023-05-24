d47440d808203ae1a9d7bfbe029290d0ecc6eccf
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-24 16:52:22 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (53.23%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,29 +1,23 @@
-//Re//
-//Sorting but the backtracking is slightly different
 class Solution {
 private:
-    vector<vector<int>>ans;
+    vector<vector<int>>res;
 public:
-    void helper(int pos,vector<int>& ds, vector<int>& nums){
-        ans.push_back(ds);
-        for(int i=pos;i<nums.size();i++){
-            if(i!=pos && nums[i]==nums[i-1]) continue;
+    void helper(int ind,vector<int>& ds,vector<int>& nums){
+        res.push_back(ds);
+        
+        for(int i=ind;i<nums.size();i++){
+            if(i!=ind && nums[i]==nums[i-1]) continue;
+            
             ds.push_back(nums[i]);
             helper(i+1,ds,nums);
             ds.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        /*
-        Although we apply subsequence approach of pick or
-        not pick but the question is of subsets so we can
-        sort nums.
-        */
         sort(nums.begin(),nums.end());
         vector<int>ds;
-        
         helper(0,ds,nums);
         
-        return ans;
+        return res;
     }
 };
