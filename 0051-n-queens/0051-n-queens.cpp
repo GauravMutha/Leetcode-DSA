0132656e0e249327b77e2be56bb2f43990985635
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-25 12:19:16 +0530

    Time: 8 ms (58.98%), Space: 7.4 MB (48.43%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,59 +1,35 @@
+//Time Complexity Optimised
 class Solution {
 private:
     vector<vector<string>>res;
 public:
-        bool safeRow(int row, int col, vector < string > board, int n) {
-      // check upper element
-      int duprow = row;
-      int dupcol = col;
-
-      while (row >= 0 && col >= 0) {
-        if (board[row][col] == 'Q')
-          return false;
-        row--;
-        col--;
-      }
-
-      col = dupcol;
-      row = duprow;
-      while (col >= 0) {
-        if (board[row][col] == 'Q')
-          return false;
-        col--;
-      }
-
-      row = duprow;
-      col = dupcol;
-      while (row < n && col >= 0) {
-        if (board[row][col] == 'Q')
-          return false;
-        row++;
-        col--;
-      }
-      return true;
-    }
-    void helper(int col,vector<string>& board,int n){
+    void helper(int col,vector<string>& board,int n,vector<int>&leftCheck,vector<int>&upDiaCheck,vector<int>&downDiaCheck){
         if(col==n){
             res.push_back(board);
             return;
         }
         
         for(int row=0;row<n;row++){
-            if(safeRow(row,col,board,n)){
+            if(leftCheck[row]==0 && upDiaCheck[n - 1 + col - row]==0 && downDiaCheck[row+col]==0){
                 board[row][col]='Q';
-                helper(col+1,board,n);
+                leftCheck[row]=1;
+                upDiaCheck[n - 1 + col - row]=1;
+                downDiaCheck[row+col]=1;
+                
+                helper(col+1,board,n,leftCheck,upDiaCheck,downDiaCheck);
+                
                 board[row][col]='.';
+                leftCheck[row]=0;
+                upDiaCheck[n - 1 + col - row]=0;
+                downDiaCheck[row+col]=0;
             }
         }
         return;
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector < string > board(n);
-        string s(n, '.');
-        for (int i = 0; i < n; i++) {
-          board[i] = s;
-        }
-        helper(0,board,n);
+        vector < string > board(n,string(n,'.'));
+        vector<int>leftCheck(n,0),upDiaCheck(2*n-1,0),downDiaCheck(2*n-1,0);
+        helper(0,board,n,leftCheck,upDiaCheck,downDiaCheck);
         return res;
     }
 };
