207dda9aa7d0d5c742953af47694aae829592b77
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-30 18:48:40 +0530

    Time: 6 ms (68.31%), Space: 9 MB (28.89%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,17 @@
-//Memoization
+//Tabulation
 class Solution {
 public:
-    int helper(int row,int col,vector<vector<int>>&dp,vector<vector<int>>& tri){
-        if(row==(tri.size()-1)) return tri[row][col];
-        if(dp[row][col]!=-1) return dp[row][col];
-        int down=tri[row][col]+helper(row+1,col,dp,tri);
-        int diaRight=tri[row][col]+helper(row+1,col+1,dp,tri);
-        return dp[row][col]=min(down,diaRight);
-    }
     int minimumTotal(vector<vector<int>>& tri) {
         int n=tri.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return helper(0,0,dp,tri);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        //Initialise the last row of dp
+        for(int i=0;i<n;i++) dp[n-1][i]=tri[n-1][i];
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                dp[i][j]=tri[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
