97482378d8d5701f4f2408fa9b4382577139b220
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-05-31 10:11:12 +0530

    Time: 4 ms (92.79%), Space: 14.8 MB (70.50%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,21 +10,27 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1==NULL)
-            return l2;
-        if(l2==NULL)
-            return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL) return list2;
+        else if(list2==NULL) return list1;
         
-        if(l1->val<=l2->val){
-            l1->next=mergeTwoLists(l1->next,l2);
-            return l1;
+        auto dummy = new ListNode(-1);
+        auto linker=dummy, ptr1=list1, ptr2=list2;
+        while(ptr1 && ptr2){
+            if(ptr1->val<=ptr2->val){
+                linker->next=ptr1;
+                ptr1=ptr1->next;
+                linker=linker->next;
+            }
+            else{
+                linker->next=ptr2;
+                ptr2=ptr2->next;
+                linker=linker->next;
+            }
         }
-        else {
-            l2->next=mergeTwoLists(l1,l2->next);
-            return l2;
-        }
+        if(ptr1==NULL) linker->next=ptr2;
+        else if(ptr2==NULL) linker->next=ptr1;
+        
+        return dummy->next;
     }
 };
-
-//recursive
