d28953b6adc239aac396d73bad4b3c04aa2cc542
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-01 16:31:32 +0530

    Time: 1586 ms (5.02%), Space: 318.7 MB (5.02%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,42 @@
+//Using nested loops without map
+class Solution{ 
+    
+    private:
+        bool doSolve(vector<vector<char>>& board, int row,int col) {
+        for (int i = row; i < 9; i++,col=0) { 
+            for (int j = col; j < 9; j++) {
+                if (board[i][j] != '.') continue;
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if(j<8){
+                            if (doSolve(board, i,j+1))
+                                return true;
+                        }
+                        else {
+                            if (doSolve(board, i+1,0))
+                                return true;
+                        }
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool isValid(vector<vector<char>> board, int row, int col, char num) {
+        int blkrow = (row / 3) * 3, blkcol = (col / 3) * 3; // Block no. is i/3, first element is i/3*3
+        for (int i = 0; i < 9; i++)
+            if (board[i][col] == num || board[row][i] == num || 
+                    board[blkrow + i / 3][blkcol + i % 3] == num)
+                return false;
+        return true;
+    }
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        doSolve(board, 0,0);
+    }
+
+};
