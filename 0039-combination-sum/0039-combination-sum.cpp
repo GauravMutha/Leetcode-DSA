b57748ddaba3d89826102eb0e7e0a714c7afadd3
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-01 09:48:35 +0530

    Time: 12 ms (41.31%), Space: 10.9 MB (49.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     vector<vector<int>>res;
-    void helper(int i,int target,vector<int>& ds,vector<int>& nums){
-        if(target==0){
-            res.push_back(ds);
+    void helper(int ind,int& sum,vector<int>& ds,vector<int>& nums,int target){
+        if(sum>=target){
+            if(sum==target) res.push_back(ds);
             return;
         }
-        if(i>=nums.size() || target<0) return;
-        
-        ds.push_back(nums[i]);
-        helper(i,target-nums[i],ds,nums);
-        ds.pop_back();
-        helper(i+1,target,ds,nums);
+        for(int i=ind;i<nums.size();i++){
+            ds.push_back(nums[i]);
+            sum+=nums[i];
+            helper(i,sum,ds,nums,target);
+            ds.pop_back();
+            sum-=nums[i];
+            
+            /*since we have sorted the array
+            if the sum is greater when any
+            nums[i] is added , we can break*/
+            
+            if((sum+nums[i]) > target) break;
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         vector<int>ds;
-        helper(0,target,ds,nums);
-        
+        int sum=0;
+        helper(0,sum,ds,nums,target);
         return res;
     }
 };
