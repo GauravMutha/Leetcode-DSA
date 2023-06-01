8636e9452776492d9d920fac0354e7e5f2a76fc6
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-01 09:12:30 +0530

    Time: 13 ms (38.30%), Space: 10.9 MB (56.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,24 @@
-//Re//
-//classic two recursive calls for pick and not pick
-//no for loops
 class Solution {
-private:
-    vector<vector<int>>ans;
 public:
-    void helper(int i,int target,vector<int>& ds,vector<int>& nums){
-        if(target==0){
-            ans.push_back(ds);
+    vector<vector<int>>res;
+    void helper(int ind,int& sum,vector<int>& ds,vector<int>& nums,int target){
+        if(sum>=target){
+            if(sum==target) res.push_back(ds);
             return;
         }
-        if(target<0 || i>=nums.size()) return;
-        
-        
-        ds.push_back(nums[i]);
-        helper(i,target-nums[i],ds,nums);
-        ds.pop_back();
-        helper(i+1,target,ds,nums);
+        for(int i=ind;i<nums.size();i++){
+            ds.push_back(nums[i]);
+            sum+=nums[i];
+            helper(i,sum,ds,nums,target);
+            ds.pop_back();
+            sum-=nums[i];
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
+        //No sorting
         vector<int>ds;
-        helper(0,target,ds,nums);
-        return ans;
+        int sum=0;
+        helper(0,sum,ds,nums,target);
+        return res;
     }
 };
