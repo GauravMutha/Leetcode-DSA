84fc55403a85d7e0d805cc893ccf03e46265992b
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-02 17:20:24 +0530

    Time: 19 ms (35.16%), Space: 10.2 MB (49.33%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,15 +1,15 @@
-//Memoization
+//Memoization- reverse direction(from last to first row)
 class Solution {
 public:
     int helper(int row,int col,vector<vector<int>>& dp,vector<vector<int>>& matrix){
         if(col>=matrix.size() || col<0) return INT_MAX;
-        if(row==(matrix.size()-1)) return matrix[row][col];
+        if(row==0) return matrix[row][col];
         
         if(dp[row][col]!=-1) return dp[row][col];
         
-        int down =helper(row+1,col,dp,matrix);
-        int diaLeft =helper(row+1,col-1,dp,matrix);
-        int diaRight =helper(row+1,col+1,dp,matrix);
+        int down =helper(row-1,col,dp,matrix);
+        int diaLeft =helper(row-1,col-1,dp,matrix);
+        int diaRight =helper(row-1,col+1,dp,matrix);
         
         return dp[row][col]=matrix[row][col]+min(down,min(diaLeft,diaRight));
     }
@@ -17,7 +17,7 @@ public:
         int res=INT_MAX,n=matrix.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
         for(int i=0;i<n;i++){
-            int retValue=helper(0,i,dp,matrix);
+            int retValue=helper(n-1,i,dp,matrix);
             res=min(res,retValue);
         }
         return res;
