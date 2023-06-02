ff488afde537c8d5e29dcf1c4df2e72ab44ca23d
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-02 11:05:14 +0530

    Time: 9 ms (94.73%), Space: 6.4 MB (73.26%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,5 +1,8 @@
-//Purely recursive
+//Purely recursive - optimisation
 class Solution {
+private:
+    //hashes
+    int rHash[9][10] , cHash[9][10] , cube[3][3][10];
 public:
     bool helper(int i,int j,vector<vector<char>>& board){
         if(i>=board.size()) return true;
@@ -8,25 +11,48 @@ public:
         if(board[i][j]!='.') return helper(i,j+1,board);
         
         for(char num='1';num<='9';num++){
-            if(check(num,i,j,board)==false) continue;
-            board[i][j]=num; 
-            if(helper(i,j+1 ,board)) return true;  
+            
+            if(check(num,i,j)==false) continue;
+            
+            rHash[i][num-'0']=1;
+            cHash[j][num-'0']=1;
+            cube[i/3][j/3][num-'0']=1;
+            board[i][j]=num;
+            
+            if(helper(i,j+1 ,board)) return true;
+            
+            rHash[i][num-'0']=0;
+            cHash[j][num-'0']=0;
+            cube[i/3][j/3][num-'0']=0;
             board[i][j]='.';
         }
 
         return false;
     }
-    bool check(char num ,int r,int c, vector<vector<char>>& board){
-        int r2=(r/3) *3;     
-        int c2=(c/3) *3;
-        for(int i=0;i<board.size();i++){
-            if(board[r][i]==num) return false;
-            if(board[i][c]==num) return false;
-            if(board[r2+(i/3)][c2+(i%3)]==num) return false;
-        }
+    bool check(char num ,int r,int c){
+        if(rHash[r][num-'0']==1) return false;
+        if(cHash[c][num-'0']==1) return false;
+        if(cube[r/3][c/3][num-'0']==1) return false;
+        
         return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
+        
+        /*Initialising Hashes by putting 1 where
+        there is a number (already filled) and
+        0 otherwise*/
+        memset(rHash, 0, sizeof(rHash));
+        memset(cHash, 0, sizeof(cHash));
+        memset(cube, 0, sizeof(cube));
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board.size();j++){
+                if(board[i][j]=='.') continue;
+                rHash[i][board[i][j]-'0']=1,
+                cHash[j][board[i][j]-'0']=1,
+                cube[i/3][j/3][board[i][j]-'0']=1;
+            }
+        }
+        
         helper(0,0,board);
     }
 };
