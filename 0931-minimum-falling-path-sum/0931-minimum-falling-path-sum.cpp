467f5f106273410024f9d251e8a6a435e7c68aa8
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-02 17:59:41 +0530

    Time: 17 ms (51.30%), Space: 10.3 MB (49.33%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,4 +1,4 @@
-//Tabulation- from last to first- corresponding to first memoization
+//Tabulation- from first to last- corresponding to second memoization
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
@@ -9,19 +9,19 @@ public:
         vector<vector<int>>dp(n,vector<int>(n,0));
         
         // initialising dp
-        for(int i=0;i<n;i++) dp[n-1][i]=matrix[n-1][i];
+        for(int i=0;i<n;i++) dp[0][i]=matrix[0][i];
         
         
-        for(int i=n-2;i>=0;i--){
+        for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                int down=dp[i+1][j];
-                int diaLeft=(j==0)?INT_MAX:dp[i+1][j-1];
-                int diaRight=(j==(n-1))?INT_MAX:dp[i+1][j+1];
+                int down=dp[i-1][j];
+                int diaLeft=(j==0)?INT_MAX:dp[i-1][j-1];
+                int diaRight=(j==(n-1))?INT_MAX:dp[i-1][j+1];
                 
                 dp[i][j]=matrix[i][j]+min(down,min(diaLeft,diaRight));
                 
                 //collecting our answer from lastly processed row(0th row)
-                if(i==0) res=min(res,dp[0][j]);
+                if(i==(n-1)) res=min(res,dp[n-1][j]);
             }
         }
         return res;
