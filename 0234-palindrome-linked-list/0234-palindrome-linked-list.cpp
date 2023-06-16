fbb680482d0bbb0e7a0fbcce83d67b7dc6087314
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-16 20:14:52 +0530

    Time: 238 ms (78.13%), Space: 114.2 MB (77.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,20 +8,31 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
+    void reverse(ListNode* &head) {
+       auto curr=head;
+        ListNode *pre=NULL,*front=NULL;
+       while(curr){
+           front=curr->next;
+           curr->next=pre;
+           pre=curr;
+           curr=front;
+       }
+       head=pre;
+    } 
     bool isPalindrome(ListNode* head) {
         if(head->next==NULL) return true;
         stack<int>st;
         ListNode *f=head, *s=head;
-        while(f && f->next){
-            st.push(s->val);
-            s=s->next ,f=f->next->next;
-        }
+        
+        while(f && f->next) s=s->next ,f=f->next->next;
         if(f) s=s->next;
-        for(;s;s=s->next){
-            if(s->val!=st.top()) return false;
-            st.pop();
+        reverse(s);
+        
+        for(auto p=head;s;s=s->next,p=p->next){
+            if(s->val!=p->val) return false;
         }
         
         return true;
