455a7af85a5f7aa1ab9d09ab89e2a23e2030221e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-18 12:33:23 +0530

    Time: 7 ms (31.50%), Space: 11.1 MB (17.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,24 @@
 class Solution {
-private:
-    int findFactor(vector<int>& nums) {
-        int low=0,high=nums.size()-1,ans=0;
+public:
+    int rotationFactor(vector<int>&nums){
+        int low=0 , high=nums.size()-1;
         while(low<high){
             int mid=low+(high-low)/2;
-            
-            if(nums[mid]>nums[high]) low=mid+1;
+            if(nums[mid]>nums.back()) low=mid+1;
+            //below ot is not mid-1 because what if mid itself is the minima?{5,1,3}
             else high=mid;
         }
         return low;
     }
-public:
     int search(vector<int>& nums, int target) {
-        //k is right shift factor
-        int k=findFactor(nums),n=nums.size();
+        int k=rotationFactor(nums), n=nums.size();
         int low=0,high=nums.size()-1;
-        
         while(low<=high){
-            int mid=low+ (high-low)/2;
-            
+            int mid=low+(high-low)/2;
             if(nums[(mid+k)%n]==target) return (mid+k)%n;
             else if(nums[(mid+k)%n]<target) low=mid+1;
             else high=mid-1;
         }
-        
         return -1;
     }
 };
