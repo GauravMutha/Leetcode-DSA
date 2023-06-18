8a51728c38a8e3461cc23177c7fb46de41398cdb
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-18 19:32:25 +0530

    Time: 177 ms (88.50%), Space: 75.2 MB (95.50%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& nums) {
-        stack<pair<int,int>>st;
-        int n=nums.size(), currArea=0,maxArea=INT_MIN;
-        vector<int>smallerToLeft(n,-1),smallerToRight(n,n);
+        stack<int>st;
+        int n=nums.size(),height=0,width=0,maxArea=-1;
         
-        for(int i=n-1;i>=0;i--){
-            while(st.size() && st.top().first>=nums[i]) st.pop();
-            if(!st.empty()) smallerToRight[i]=st.top().second;
-            st.push({nums[i],i});
-        }
-        while(st.size()) st.pop();
-        for(int i=0;i<n;i++){
-            while(st.size() && st.top().first>=nums[i]) st.pop();
-            if(!st.empty()) smallerToLeft[i]=st.top().second;
-            st.push({nums[i],i});
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && ((i==n)||(nums[st.top()]>=nums[i])) ){
+                height=nums[st.top()];
+                st.pop();
+                if(st.empty()) width=i; //in this case we cannot get the left smaller
+                else width=i-st.top()-1;
+                
+                maxArea=max(maxArea,height*width);
+            }
+            st.push(i);
         }
         
-        for(int i=0;i<n;i++){
-            currArea=nums[i]*((smallerToRight[i]-smallerToLeft[i])-1);
-            maxArea=max(currArea,maxArea);
-        }
         return maxArea;
     }
 };
