3718ca63e347ffc09845921e2d602801c639a1da
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-18 13:10:54 +0530

    Time: 5 ms (46.65%), Space: 11.1 MB (54.36%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int rotationFactor(vector<int>&nums){
-        int low=0 , high=nums.size()-1;
-        while(low<high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>nums.back()) low=mid+1;
-            //below ot is not mid-1 because what if mid itself is the minima?{5,1,3}
-            else high=mid;
-        }
-        return low;
-    }
     int search(vector<int>& nums, int target) {
-        int k=rotationFactor(nums), n=nums.size();
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[(mid+k)%n]==target) return (mid+k)%n;
-            else if(nums[(mid+k)%n]<target) low=mid+1;
-            else high=mid-1;
+        int start=0,end=nums.size()-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            double x;
+            if((nums[mid]<nums[0]) == (target<nums[0])) x=nums[mid];
+            else if(target<nums[0]) x=-INFINITY;
+            else if(target>=nums[0]) x=+INFINITY;
+            
+            if(target==x) return mid;
+            else if(x<target) start=mid+1;
+            else if(x>target) end=mid-1;
         }
         return -1;
     }
