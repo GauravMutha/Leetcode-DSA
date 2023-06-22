2af201935cf229da8de1a01561f886d06403e7d2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-22 20:58:33 +0530

    Time: 51 ms (32.36%), Space: 89.3 MB (79.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,31 +1,39 @@
+//Naive than previous
+//TC-->O(n)
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size() , n2=nums2.size();
-        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
-        
-        int low=0,high=n1;
-        while(low<=high){
-            int cut1=(low+high)/2;
-            int cut2=(n1+n2+1)/2 - cut1;
-            
-            int l1= (cut1==0) ? INT_MIN : nums1[cut1-1];
-            int l2= (cut2==0) ? INT_MIN : nums2[cut2-1];
-            
-            int r1= (cut1 == n1) ? INT_MAX : nums1[cut1];
-            int r2= (cut2 == n2) ? INT_MAX : nums2[cut2];
-            
-            if(l1<=r2 && l2<=r1){
-                if((n1+n2)%2==0) return (max(l1,l2)+min(r1,r2))/2.0;
-                else return max(l1,l2);
+        int ptr1=0 , ptr2=0 , n1=nums1.size() , n2=nums2.size(),mark=(n1+n2)/2+1;
+        int prev=-1 , curr=-1 , count=0;
+        while(ptr1<n1 && ptr2<n2 && count<mark){
+            count++;
+            if(nums1[ptr1]<=nums2[ptr2]){
+                prev=curr;
+                curr=nums1[ptr1];
+                ptr1++;
             }
-            else if(l1>r2){
-                high=cut1-1;
-            }
-            else if(l2>r1){
-                low=cut1+1;
+            else{
+                prev=curr;
+                curr=nums2[ptr2];
+                ptr2++;
             }
         }
-        return 0.0;
+        
+        while(ptr1<n1 && count<mark){
+            count++;
+            prev=curr;
+            curr=nums1[ptr1];
+            ptr1++;
+        }
+        while(ptr2<n2 && count<mark){
+            count++;
+            prev=curr;
+            curr=nums2[ptr2];
+            ptr2++;
+        }
+        
+        if((n1+n2)%2) return curr;
+        return (curr+prev)/2.0;
+        
     }
 };
