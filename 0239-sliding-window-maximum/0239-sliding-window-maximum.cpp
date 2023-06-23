ad063348fad389c4f7558205483c01a81cb10201
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-23 16:07:55 +0530

    Time: 297 ms (89.15%), Space: 134.7 MB (71.43%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,20 @@
-//Using ordered_map
+//using deque
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int>res;
-        int l=0 ,r=0 , n=nums.size();
-        map<int,int>m;
-        
+        deque<int>q;
+        int l=0,r=0,n=nums.size();
         for(r=0;r<n;r++){
             
-            m[nums[r]]++;
+            while(q.size() && q.back()<nums[r]) q.pop_back();
+            q.push_back(nums[r]);
+            
             if((r-l+1)<k) continue;
             
-            res.push_back(m.rbegin()->first);
+            res.push_back(q.front());
             
-            m[nums[l]]--;
-            if(m[nums[l]]==0) m.erase(nums[l]);
+            if(q.front()==nums[l]) q.pop_front();
             l++;
         }
         
