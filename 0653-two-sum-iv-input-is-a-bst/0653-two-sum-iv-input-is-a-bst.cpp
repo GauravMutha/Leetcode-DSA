ed701f9084fcc3bfbf51875035ed2589dcf7600a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-24 15:15:25 +0530

    Time: 52 ms (30.27%), Space: 38.7 MB (25.12%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,31 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//Searching tree for target of every node that makes up the sum
+//Using map for two sum here
 class Solution {
 public:
-    bool search(TreeNode* root,int target){
-        if(root==NULL) return false;
-        
-        bool left=false,right=false;
-        if((root->val)==target) return true;
-        else if(root->val>target) left=search(root->left,target);
-        else if(root->val<target) right=search(root->right,target);
-        
-        return (left||right);
-    }
-    bool dfs(TreeNode* curr,TreeNode* root,int sum){
+    set<int>st;
+    bool search(TreeNode* curr,int sum){
         if(curr==NULL) return false;
         
-        int target=sum-curr->val;
+        if(st.find(sum-curr->val)!=st.end()) return true;
+        else st.insert(curr->val);
         
-        if(target!=curr->val && search(root,target)) return true;
-        
-        return (dfs(curr->left,root,sum) || dfs(curr->right,root,sum));
-        
+        return search(curr->left, sum) || search(curr->right, sum);
     }
     bool findTarget(TreeNode* root, int k) {
-        TreeNode* curr=root;
-        return dfs(curr,root,k);     
+        return search(root,k);
     }
 };
