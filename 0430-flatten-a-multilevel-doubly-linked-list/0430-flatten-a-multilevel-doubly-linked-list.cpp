73fcd68f5fc5833dd795365fb6b8d18aef49536e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-25 17:25:45 +0530

    Time: 8 ms (25.75%), Space: 7.4 MB (11.40%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -9,28 +9,38 @@ public:
 };
 */
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
 class Solution {
 public:
     Node* helper(Node* curr) {
         if(curr==NULL || (curr->next==NULL && curr->child==NULL)) return curr;
         
-        Node* cn=curr->next;
         Node* lastChild=NULL;
         
         if(curr->child){
-            Node* cc=curr->child; //c ,means current's child
-            curr->next=curr->child;
+            
+            lastChild=helper(curr->child);
+            
+            if(lastChild) lastChild->next=curr->next;
+            if(curr->next) curr->next->prev=lastChild;
             curr->child->prev=curr;
+            curr->next=curr->child;
             curr->child=NULL;
             
-            lastChild=helper(cc);
-            
-            if(cn && lastChild) cn->prev=lastChild , lastChild->next=cn;
+            curr=lastChild;
         }
         
-        if(cn) return helper(cn);
-        
-        return lastChild;
+        return (curr->next) ? helper(curr->next) : lastChild;
     }
     Node* flatten(Node* head) {
         helper(head);
