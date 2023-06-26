e4fc4162809d0071676a9c2e782d4dadc87f9494
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-26 18:05:59 +0530

    Added solution - LeetHub

--- K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.cpp
@@ -8,31 +8,29 @@ class Solution{
     public:
     int kthElement(int nums1[], int nums2[], int n1, int n2, int k)
     {
-        int ptr1=0 , ptr2=0 ,count=0;
+        if(n1>n2) return kthElement(nums2,nums1,n2,n1,k);
         
-        while(ptr1<n1 && ptr2<n2 && count<k){
-            count++;
-            if(nums1[ptr1]<=nums2[ptr2]){
-                if(count==k) return nums1[ptr1];
-                ptr1++;
+        int low=max(0,k-n2),high=min(k,n1);
+        while(low<=high){
+            int cut1=(low+high)/2;
+            int cut2=k - cut1;
+            
+            int l1= (cut1==0) ? INT_MIN : nums1[cut1-1];
+            int l2= (cut2==0) ? INT_MIN : nums2[cut2-1];
+            
+            int r1= (cut1 == n1) ? INT_MAX : nums1[cut1];
+            int r2= (cut2 == n2) ? INT_MAX : nums2[cut2];
+            
+            if(l1<=r2 && l2<=r1){
+                return max(l1,l2);
             }
-            else {
-                if(count==k) return nums2[ptr2];
-                ptr2++;
+            else if(l1>r2){
+                high=cut1-1;
+            }
+            else if(l2>r1){
+                low=cut1+1;
             }
         }
-        
-        while(ptr1<n1 && count<k){
-            count++;
-            if(count==k) return nums1[ptr1];
-            ptr1++;
-        }
-        while(ptr2<n2 && count<k){
-            count++;
-            if(count==k) return nums2[ptr2];
-            ptr2++;
-        }
-        
         return 1;
     }
 };
