6dcd72e5c6ce0e20fc8f5c68ceec7cbf0f2c7980
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-06-28 16:49:37 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -7,30 +7,52 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	int findParent(int node,vector<int>& parent){
+	    if(parent[node]==node) return node;
+	    
+	    int temp=findParent(parent[node],parent);
+	    parent[node]=temp;
+	    return temp;
+	}
+	bool doUnion(int u,int v,vector<int>& parent,vector<int>& rank){
+	    int uParent=findParent(u,parent);
+	    int vParent=findParent(v,parent);
+	    
+	    if(uParent==vParent) return false;
+	    
+	    if(rank[uParent]<rank[vParent])
+	        parent[uParent]=vParent;
+	    else if(rank[uParent]>rank[vParent])
+	        parent[vParent]=uParent;
+	    else parent[vParent]=uParent , rank[uParent]++;
+	    
+	    return true;
+	}
     int spanningTree(int n, vector<vector<int>> adj[])
     {
-        int sum=0;
-        vector<bool>vis(n,false);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,0});
+        int res=0;
+        vector<pair<int,pair<int,int>>> edges;
+        vector<int>parent(n) , rank(n,0);
         
-        while(pq.size()){
-            auto p=pq.top();
-            pq.pop();
-            int wt=p.first , node=p.second;
-            
-            if(vis[node]) continue;
-            
-            sum+=wt;
-            vis[node]=true;
-            
-            for(auto adjacent:adj[node]){
-                int adjNode=adjacent[0] , adjWt=adjacent[1];
-                if(vis[adjNode]) continue;
-                pq.push({adjWt,adjNode});
+        for(int u=0;u<n;u++){
+            for(auto vec : adj[u]){
+                int v=vec[0] , wt=vec[1];
+                edges.push_back({wt,{u,v}});
             }
         }
-        return sum;
+        sort(edges.begin(),edges.end());
+        
+        for(int i=0;i<n;i++) parent[i]=i;
+        
+        for(int i=0;i<edges.size();i++){
+            auto p=edges[i];
+            int wt=p.first , u=p.second.first , v=p.second.second;
+            
+            if(doUnion(u,v,parent,rank)) res+=wt;;
+        }
+        
+        
+        return res;
     }
 };
 
