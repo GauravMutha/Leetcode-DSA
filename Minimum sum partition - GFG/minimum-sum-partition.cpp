2378856a3dc644ae8a2fa37c47f87d329288c58f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-15 17:40:14 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -4,25 +4,32 @@ using namespace std;
 
 // } Driver Code Ends
 
-//Using memoization
+//Using tabulation
 class Solution{
     
   public:
-  int helper(int ind,int sum1,int sum2,vector<vector<int>>& dp,int n,int arr[]){
-      if(ind==n) return abs(sum1-sum2);
-      
-      if(dp[ind][abs(sum1-sum2)]!=-1) return dp[ind][abs(sum1-sum2)];
-      
-      int pick=helper(ind+1,sum1+arr[ind],sum2,dp,n,arr);
-      int notPick=helper(ind+1,sum1,sum2+arr[ind],dp,n,arr);
-      
-      return dp[ind][abs(sum1-sum2)]=min(pick,notPick);
-  }
 	int minDifference(int arr[], int n)  {
-	    int total=accumulate(arr,arr+n,0);
-	    vector<vector<int>>dp(n,vector<int>(total+1,-1));
+	    int k=accumulate(arr,arr+n,0), res=INT_MAX;
 	    
-	    return helper(0,0,0,dp,n,arr);
+	    vector<vector<bool>>dp(n,vector<bool>(k+1,false));
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(arr[0]<=k) dp[0][arr[0]]=true;
+        
+        for(int ind=1;ind<n;ind++){
+            for(int target=1;target<=k;target++){
+                bool notPick= dp[ind-1][target];
+                bool pick=false;
+                if(arr[ind]<=target) pick= dp[ind-1][target-arr[ind]];
+
+                dp[ind][target]= (pick || notPick);
+            }
+        }
+        for(int i=0;i<dp[0].size();i++){
+            if(dp[n-1][i]==false) continue;
+            res=min(res,abs(k-2*i));
+        }
+        
+        return res;
 	} 
 };
 
