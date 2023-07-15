3d8af875f856c3a1c11e294bba386b379825b9ba
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-15 14:50:27 +0530

    Time: 330 ms (67.58%), Space: 12.8 MB (76.88%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,18 +1,6 @@
-//tabulation
+//tabulation space optimisation
 class Solution {
 public:
-    bool helper(int ind, int target,vector<vector<int>>& dp,vector<int>& nums){
-        if(target==0) return true;
-        if(ind==0) return (nums[0]==target);
-    
-        if(dp[ind][target]!=-1) return dp[ind][target];
-    
-        bool notPick= helper(ind-1,target,dp,nums);
-        bool pick=false;
-        if(nums[ind]<=target) pick= helper(ind-1,target-nums[ind],dp,nums);
-    
-        return dp[ind][target]= (pick || notPick);
-    }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         
@@ -20,19 +8,21 @@ public:
         if(total%2) return false; //two equal integers cannot add upto an odd number
         int k=total/2;
         
-        vector<vector<bool>>dp(n,vector<bool>(k+1,false));
-        for(int i=0;i<n;i++) dp[i][0]=true;
-        if(nums[0]<=k) dp[0][nums[0]]=true;
-        
+        vector<bool>dp(k+1,false);
+        dp[0]=true;
+        if(nums[0]<=k) dp[nums[0]]=true;
         for(int ind=1;ind<n;ind++){
+            vector<bool>tempDP(k+1,false);
+            tempDP[0]=true;
             for(int target=1;target<=k;target++){
-                bool notPick= dp[ind-1][target];
+                bool notPick= dp[target];
                 bool pick=false;
-                if(nums[ind]<=target) pick= dp[ind-1][target-nums[ind]];
+                if(nums[ind]<=target) pick= dp[target-nums[ind]];
 
-                dp[ind][target]= (pick || notPick);
+                tempDP[target]= (pick || notPick);
             }
+            dp=move(tempDP);
         }
-        return dp[n-1][k];
+        return dp[k];
     }
 };
