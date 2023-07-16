af754cfcf39c4743d868f64961ddd864a1efde38
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-16 11:46:37 +0530

    Time: 278 ms (44.44%), Space: 100.4 MB (88.89%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,24 +1,15 @@
-//Line sweep algorithm
+//sliding window
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
         
-        vector<pair<int,int>>intervals;
-        int ans=0,n=nums.size(),count=0;
-        map<int,int>m;
+        int ans=0 , f=0,b=0 , n=nums.size();
+        sort(nums.begin(),nums.end());
         
-        sort(begin(nums),end(nums));
-        
-        for(int i=0;i<n;i++){
-            intervals.push_back({nums[i]-k,nums[i]+k});
-            m[nums[i]-k]++;
-            m[nums[i]+k+1]--;
+        for(f=0;f<n;f++){
+            if((nums[f]-nums[b])>2*k) b++;
         }
         
-        for(auto &[point,mark]:m){
-            count+=mark;
-            ans=max(ans,count);
-        }
-        return ans;
+        return f-b;
     }
 };
