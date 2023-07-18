53edadf509f09aab3ecbea5752e07b67b65d115f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-18 19:58:02 +0530

    Time: 134 ms (41.47%), Space: 315.4 MB (5.01%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,27 @@
+//Somewhat follows "subset sum equals k",but here there is no notPick condition
+//Simple memoization
+//Using offset to handle negative indexes
+class Solution {
+public:
+    int helper(int ind,int target,vector<int>& nums,vector<vector<int>>& dp){
+        if(ind==0){
+            if(target==0 && (nums[0]==0)) return 2;
+            return ((target==nums[0]) || target==(-nums[0]));
+        }
+        
+        if(dp[ind][target+20000]!=-1)
+            return dp[ind][target+20000];
+        
+        int pickPos=0,pickNeg=0;
+        
+        pickPos=helper(ind-1,target-nums[ind],nums,dp);
+        pickNeg=helper(ind-1,target+nums[ind],nums,dp);
+        
+        return dp[ind][target+20000]=(pickPos+pickNeg);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>>dp(21,vector<int>(40001,-1));
+        return helper(n-1,target,nums,dp);
+    }
+};
