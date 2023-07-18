45873c5458be7e42d462d57f6416b29bd8af505c
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-18 14:43:00 +0530

    Time: 120 ms (55.52%), Space: 45.8 MB (20.04%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+//memoization
+class Solution {
+public:
+    int helper(int ind,int amount,vector<int>& coins,vector<vector<int>>& dp){
+        if(ind==0){
+            if((amount%coins[0])==0) return (amount/coins[0]);
+            else return 1e9;
+        }
+        
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
+        
+        int notPick=helper(ind-1,amount,coins,dp);
+        int pick=INT_MAX;
+        if(coins[ind]<=amount) pick=1+helper(ind,amount-coins[ind],coins,dp);
+        
+        return dp[ind][amount]=min(pick,notPick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int res= helper(n-1,amount,coins,dp);
+        
+        return (res==1e9) ? -1 : res;
+    }
+};
