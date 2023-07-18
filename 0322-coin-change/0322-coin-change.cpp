bba8afd3febc021cd535ef1553d5651e06c0cdc2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-18 15:36:42 +0530

    Time: 124 ms (51.67%), Space: 40.7 MB (40.74%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,31 +1,33 @@
-//Tabulation
+//Space optimisation
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        vector<int>dp(amount+1,0);
         
         for(int a=0;a<=amount;a++){
             if((a%coins[0])==0) 
-                dp[0][a]=(a/coins[0]);
-            else dp[0][a]=1e9;
+                dp[a]=(a/coins[0]);
+            else dp[a]=1e9;
         }
         
         for(int ind=1;ind<n;ind++){
+            vector<int>tempDP(amount+1,0);
             for(int a=0;a<=amount;a++){
                 
-                int notPick=0+dp[ind-1][a];
+                int notPick=0+dp[a];
                 
-                int pick=INT_MAX;
-                if(coins[ind]<=a) pick=1+dp[ind][a-coins[ind]];
+                int pick=1e9;
+                if(coins[ind]<=a) pick=1+tempDP[a-coins[ind]];
         
-                dp[ind][a]=min(pick,notPick);
+                tempDP[a]=min(pick,notPick);
             }
+            dp=move(tempDP);
         }
         
-        if(dp[n-1][amount]>=1e9) return -1;
+        if(dp[amount]>=1e9) return -1;
         
-        return dp[n-1][amount];
+        return dp[amount];
     }
 };
