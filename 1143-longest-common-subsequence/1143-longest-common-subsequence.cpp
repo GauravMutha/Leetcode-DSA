6c5e67ac3bb28672913d5f2c170311b79b27abba
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-20 22:57:50 +0530

    Time: 74 ms (52.45%), Space: 24.2 MB (67.46%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,24 +1,26 @@
-//Tabulation with index shifting
+//Space optimisation with index shifting
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n1=text1.size() , n2=text2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        vector<int>dp(n2+1,0);
         
         
         
         for(int ind1=1;ind1<=n1;ind1++){
+            vector<int>tempDP(n2+1,0);
             for(int ind2=1;ind2<=n2;ind2++){
                 int count=0;
-                if(text1[ind1-1]==text2[ind2-1]) count=1+dp[ind1-1][ind2-1];
-                else count= max(dp[ind1-1][ind2] , dp[ind1][ind2-1]);
+                if(text1[ind1-1]==text2[ind2-1]) count=1+dp[ind2-1];
+                else count= max(dp[ind2] , tempDP[ind2-1]);
                 
-                dp[ind1][ind2]=count;
+                tempDP[ind2]=count;
             }
+            dp=move(tempDP);
         }
         
         
         
-        return dp[n1][n2];
+        return dp[n2];
     }
 };
