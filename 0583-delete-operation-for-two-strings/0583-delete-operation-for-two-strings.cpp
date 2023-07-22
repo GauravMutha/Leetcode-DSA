55156e164cb6d3ed35d632efc9110b3e71e6c8f9
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-22 16:54:48 +0530

    Time: 30 ms (43.59%), Space: 12.3 MB (17.77%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -2,24 +2,25 @@
 on whatever result we get from 1143.longest 
 common subsequence,that's all*/
 
-//Memoization
+//Tabulation
 class Solution {
 public:
-    //below 2 functions are as it is from 1143.Longest common subsequecnce
-    int helper(int ind1,int ind2,string& text1,string& text2,vector<vector<int>>& dp){
-        if(ind1<0 || ind2<0) return 0;
-        
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        
-        if(text1[ind1]==text2[ind2]) return 1+helper(ind1-1,ind2-1,text1,text2,dp);
-        
-        return dp[ind1][ind2]=max(helper(ind1-1,ind2,text1,text2,dp),helper(ind1,ind2-1,text1,text2,dp));
-        
-    }
+    //below function is as it is from 1143.Longest common subsequecnce
     int longestCommonSubsequence(string& text1, string& text2) {
         int n1=text1.size() , n2=text2.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return helper(n1-1,n2-1,text1,text2,dp);
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        
+        for(int ind1=1;ind1<=n1;ind1++){
+            for(int ind2=1;ind2<=n2;ind2++){
+                int count=0;
+                if(text1[ind1-1]==text2[ind2-1]) count=1+dp[ind1-1][ind2-1];
+                else count= max(dp[ind1-1][ind2] , dp[ind1][ind2-1]);
+                
+                dp[ind1][ind2]=count;
+            }
+        }
+        
+        return dp[n1][n2];
     }
     
     int minDistance(string word1, string word2) {
