c00b96529b974564410a6a687e8b87d91bac4a4d
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-22 15:56:09 +0530

    Time: 343 ms (6.62%), Space: 72.9 MB (55.15%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,26 +1,30 @@
 /*We make it exactly same as 1143 LCS.
 We take reverse of given string as 
 s2,and then apply lcs on s1 and s2.
-"helper" function is exact same.
 */
-//Memoization
+
+//Tabulation
 class Solution {
 public:
-     int helper(int ind1,int ind2,string& text1,string& text2,vector<vector<int>>& dp){
-        if(ind1<0 || ind2<0) return 0;
-        
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        
-        if(text1[ind1]==text2[ind2]) return 1+helper(ind1-1,ind2-1,text1,text2,dp);
-        
-        return dp[ind1][ind2]=max(helper(ind1-1,ind2,text1,text2,dp),helper(ind1,ind2-1,text1,text2,dp));
-        
-    }
     int longestPalindromeSubseq(string s1) {
+        
         string s2(s1.rbegin(),s1.rend());
         int n=s1.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
         
-        return helper(n-1,n-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=n;ind2++){
+                int count=0;
+                if(s1[ind1-1]==s2[ind2-1]) count=1+dp[ind1-1][ind2-1];
+                else count= max(dp[ind1-1][ind2] , dp[ind1][ind2-1]);
+                
+                dp[ind1][ind2]=count;
+            }
+        }
+        
+        return dp[n][n];
     }
 };
