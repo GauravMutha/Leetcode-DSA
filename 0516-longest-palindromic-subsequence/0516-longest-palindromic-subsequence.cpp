4d63f3098448f499fa5d9289b96c6aff847bec0f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-22 15:58:21 +0530

    Time: 194 ms (46.90%), Space: 72.3 MB (69.71%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -3,7 +3,7 @@ We take reverse of given string as
 s2,and then apply lcs on s1 and s2.
 */
 
-//Tabulation
+//Space optimisation
 class Solution {
 public:
     int longestPalindromeSubseq(string s1) {
@@ -11,20 +11,25 @@ public:
         string s2(s1.rbegin(),s1.rend());
         int n=s1.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        vector<int>dp (n+1,0);
         
         
         
         for(int ind1=1;ind1<=n;ind1++){
+            vector<int>tempDP(n+1,0);
+            
             for(int ind2=1;ind2<=n;ind2++){
                 int count=0;
-                if(s1[ind1-1]==s2[ind2-1]) count=1+dp[ind1-1][ind2-1];
-                else count= max(dp[ind1-1][ind2] , dp[ind1][ind2-1]);
+                if(s1[ind1-1]==s2[ind2-1]) count=1+dp[ind2-1];
+                else count= max(dp[ind2] , tempDP[ind2-1]);
                 
-                dp[ind1][ind2]=count;
+                tempDP[ind2]=count;
             }
+            dp=move(tempDP);
         }
         
-        return dp[n][n];
+        
+        
+        return dp[n];
     }
 };
