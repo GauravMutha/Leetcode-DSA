60aef8682d3a8c091903952fb3fa6b2ae096aff2
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-22 16:55:53 +0530

    Time: 23 ms (69.78%), Space: 11.4 MB (71.05%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -2,25 +2,30 @@
 on whatever result we get from 1143.longest 
 common subsequence,that's all*/
 
-//Tabulation
+//Space optimisation
 class Solution {
 public:
     //below function is as it is from 1143.Longest common subsequecnce
     int longestCommonSubsequence(string& text1, string& text2) {
+        
         int n1=text1.size() , n2=text2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        vector<int>dp(n2+1,0);
         
         for(int ind1=1;ind1<=n1;ind1++){
+            vector<int>tempDP(n2+1,0);
             for(int ind2=1;ind2<=n2;ind2++){
                 int count=0;
-                if(text1[ind1-1]==text2[ind2-1]) count=1+dp[ind1-1][ind2-1];
-                else count= max(dp[ind1-1][ind2] , dp[ind1][ind2-1]);
+                if(text1[ind1-1]==text2[ind2-1]) count=1+dp[ind2-1];
+                else count= max(dp[ind2] , tempDP[ind2-1]);
                 
-                dp[ind1][ind2]=count;
+                tempDP[ind2]=count;
             }
+            dp=move(tempDP);
         }
         
-        return dp[n1][n2];
+        
+        
+        return dp[n2];
     }
     
     int minDistance(string word1, string word2) {
