2940f8aab32bbe9a3965c975654598b5d0c7b9d8
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-22 16:36:04 +0530

    Time: 98 ms (41.25%), Space: 27.9 MB (68.72%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,7 +2,7 @@
 on whatever answer we get from 516
 longest palindromic subsequence*/
 
-//Tabulation
+//Space optimisation
 class Solution {
 public:
     //below function is as it is in from 516.longest palindromic subsequence
@@ -11,21 +11,22 @@ public:
         string s2(s1.rbegin(),s1.rend());
         int n=s1.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        
-        
+        vector<int>dp (n+1,0);
         
         for(int ind1=1;ind1<=n;ind1++){
+            vector<int>tempDP(n+1,0);
+            
             for(int ind2=1;ind2<=n;ind2++){
                 int count=0;
-                if(s1[ind1-1]==s2[ind2-1]) count=1+dp[ind1-1][ind2-1];
-                else count= max(dp[ind1-1][ind2] , dp[ind1][ind2-1]);
+                if(s1[ind1-1]==s2[ind2-1]) count=1+dp[ind2-1];
+                else count= max(dp[ind2] , tempDP[ind2-1]);
                 
-                dp[ind1][ind2]=count;
+                tempDP[ind2]=count;
             }
+            dp=move(tempDP);
         }
         
-        return dp[n][n];
+        return dp[n];
     }
     
     int minInsertions(string s) {
