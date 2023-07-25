3db139eab245699b089f6a6b5ba645489c36851f
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-25 22:42:51 +0530

    Time: 87 ms (38.41%), Space: 12.4 MB (60.56%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,26 +1,30 @@
-//Tabulation
+//Space Optimisation
 class Solution {
 public:
     bool isMatch(string s, string t) {
         
         int n=s.size() , m=t.size();
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        vector<bool>dp(m+1,false);
         
-        //initialising dp 
-        dp[0][0]=true;
+        //initialising dp
+        dp[0]=true;
         for(int ind2=1;ind2<=m;ind2++){
-            dp[0][ind2]=(t.substr(0,ind2)==string(ind2,'*')); 
+            dp[ind2]=(t.substr(0,ind2)==string(ind2,'*')); 
         }
         
         for(int ind1=1;ind1<=n;ind1++){
+            
+            vector<bool>tempDP(m+1,false);
+            
             for(int ind2=1;ind2<=m;ind2++){
                 if(s[ind1-1]==t[ind2-1] || t[ind2-1]=='?')
-                        dp[ind1][ind2]=dp[ind1-1][ind2-1];
+                        tempDP[ind2]=dp[ind2-1];
                 else if(t[ind2-1]=='*') 
-                        dp[ind1][ind2]=(dp[ind1-1][ind2] || dp[ind1][ind2-1]);
+                        tempDP[ind2]=(dp[ind2] || tempDP[ind2-1]);
             }
+            dp=move(tempDP);
         }
         
-        return dp[n][m];
+        return dp[m];
     }
 };
