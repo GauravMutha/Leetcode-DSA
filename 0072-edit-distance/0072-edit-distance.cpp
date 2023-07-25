20c5bf52d7e2867e4fafa91a79fbddac808e3e05
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-25 16:51:41 +0530

    Time: 18 ms (50.52%), Space: 8.5 MB (77.96%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,31 @@
-//Tabulation
+//Space optimisation
 class Solution {
 public:
     int minDistance(string s1, string s2) {
         
         int n=s1.size() , m=s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<int>dp(m+1,0);
         
         //initialising dp
-        for(int ind1=0;ind1<=n;ind1++) dp[ind1][0]=ind1;
-        for(int ind2=0;ind2<=m;ind2++) dp[0][ind2]=ind2;
+        for(int ind2=0;ind2<=m;ind2++) dp[ind2]=ind2;
         
         for(int ind1=1;ind1<=n;ind1++){
+            vector<int>tempDP(m+1,0);
+            tempDP[0]=ind1;
             for(int ind2=1;ind2<=m;ind2++){
-                int count=0;
-                if(s1[ind1-1]==s2[ind2-1]) dp[ind1][ind2]=dp[ind1-1][ind2-1];
+                if(s1[ind1-1]==s2[ind2-1]) tempDP[ind2]=dp[ind2-1];
                 else {
-                    int replace=1+dp[ind1-1][ind2-1];
-                    int remove= 1+dp[ind1-1][ind2];
-                    int insert=1+dp[ind1][ind2-1];
+                    int replace=1+dp[ind2-1];
+                    int remove= 1+dp[ind2];
+                    int insert=1+tempDP[ind2-1];
                     
-                    dp[ind1][ind2]=min({replace,remove,insert});
+                    tempDP[ind2]=min({replace,remove,insert});
                 }
             }
+            dp=move(tempDP);
         }
         
-        return dp[n][m];
+        return dp[m];
         
     }
 };
