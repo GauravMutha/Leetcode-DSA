270cd82efe6eb4aa5704aa76989252781c0aaa60
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-25 19:18:16 +0530

    Time: 95 ms (34.07%), Space: 15 MB (60.04%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,25 +1,26 @@
-//Memoization
+//Tabulation
 class Solution {
 public:
-    bool helper(int ind1,int ind2,string &s,string &t,vector<vector<int>>& dp){
-        
-        if(ind2<0) return ind1<0;
-        if(ind1<0 && ind2>=0) return (t.substr(0,ind2+1)==string(ind2+1,'*'));
-        
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        
-        if(s[ind1]==t[ind2] || t[ind2]=='?')
-            return dp[ind1][ind2]=helper(ind1-1,ind2-1,s,t,dp);
-        else if(t[ind2]=='*') 
-            return dp[ind1][ind2]=(helper(ind1-1,ind2,s,t,dp) || helper(ind1,ind2-1,s,t,dp));
-        
-        return dp[ind1][ind2]=false; //characters are not matching
-    }
     bool isMatch(string s, string t) {
         
         int n=s.size() , m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
         
-        return helper(n-1,m-1,s,t,dp);
+        //initialising dp 
+        dp[0][0]=true;
+        for(int ind2=1;ind2<=m;ind2++){
+            dp[0][ind2]=(t.substr(0,ind2)==string(ind2,'*')); 
+        }
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s[ind1-1]==t[ind2-1] || t[ind2-1]=='?')
+                        dp[ind1][ind2]=dp[ind1-1][ind2-1];
+                else if(t[ind2-1]=='*') 
+                        dp[ind1][ind2]=(dp[ind1-1][ind2] || dp[ind1][ind2-1]);
+            }
+        }
+        
+        return dp[n][m];
     }
 };
