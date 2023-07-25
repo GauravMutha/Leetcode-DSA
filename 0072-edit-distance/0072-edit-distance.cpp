2ba1a17e3734c31acd8c9c64271e3c71a1cfe722
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-25 16:28:09 +0530

    Time: 32 ms (25.42%), Space: 9 MB (61.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,29 +1,30 @@
-//Memoization
+//Tabulation
 class Solution {
 public:
-    int helper(int ind1,int ind2,string s1,string s2,vector<vector<int>>& dp){
-        
-        if(ind2<0) return ind1+1;
-        if(ind1<0) return ind2+1;
-        
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        
-        if(s1[ind1]==s2[ind2]) return dp[ind1][ind2]= helper(ind1-1,ind2-1,s1,s2,dp);
-        
-        
-        int replace=1+helper(ind1-1,ind2-1,s1,s2,dp);
-        int remove= 1+helper(ind1-1,ind2,s1,s2,dp);
-        int insert=1+helper(ind1,ind2-1,s1,s2,dp);
-        
-        return dp[ind1][ind2]=min({replace,remove,insert});
-        
-    }
     int minDistance(string s1, string s2) {
         
         int n=s1.size() , m=s2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         
-        return helper(n-1,m-1,s1,s2,dp);
+        //initialising dp
+        for(int ind1=0;ind1<=n;ind1++) dp[ind1][0]=ind1;
+        for(int ind2=0;ind2<=m;ind2++) dp[0][ind2]=ind2;
+        
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                int count=0;
+                if(s1[ind1-1]==s2[ind2-1]) dp[ind1][ind2]=dp[ind1-1][ind2-1];
+                else {
+                    int replace=1+dp[ind1-1][ind2-1];
+                    int remove= 1+dp[ind1-1][ind2];
+                    int insert=1+dp[ind1][ind2-1];
+                    
+                    dp[ind1][ind2]=min({replace,remove,insert});
+                }
+            }
+        }
+        
+        return dp[n][m];
         
     }
 };
