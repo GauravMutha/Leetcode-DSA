5651495a18fa0490e5c5543d10ee8d635ed939cf
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 18:19:40 +0530

    Time: 13 ms (83.61%), Space: 13.5 MB (66.21%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,37 +1,40 @@
 // Exactly similar to its III version
 
-//Tabulation
+//Space Optimisation
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        
+    int maxProfit(int k,vector<int>& prices) {
         int n=prices.size();
         
-        vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
+        vector<int> dp(2*k+1,0);
         
         for(int ind=n-1;ind>=0;ind--){
+            
+            vector<int>tempDP(2*k+1,0);
+            
             for(int tid=2*k-1;tid>=0;tid--){
                 
                 int profit=0;
         
                 if(tid%2==0){
                     
-                    int buy=-prices[ind]+dp[ind+1][tid+1];
-                    int skipBuying= dp[ind+1][tid];
+                    int buy=-prices[ind]+dp[tid+1];
+                    int skipBuying= dp[tid];
 
                     profit=max(buy,skipBuying);
                 }
                 else{
-                    int sell= +prices[ind]+dp[ind+1][tid+1];
-                    int skipSelling= dp[ind+1][tid];
+                    int sell= +prices[ind]+dp[tid+1];
+                    int skipSelling= dp[tid];
 
                     profit=max(sell,skipSelling);
                 }
 
-                dp[ind][tid]=profit;
+                tempDP[tid]=profit;
             }
+            dp=move(tempDP);
         }
         
-         return dp[0][0];
+         return dp[0];
     }
 };
