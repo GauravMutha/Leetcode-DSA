28abaebdf7ace3d67e1c5c0403c80f59b9abfc28
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 18:42:28 +0530

    Time: 6 ms (53.03%), Space: 11.9 MB (37.11%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,35 @@
+/*Exactly similar , only change is that
+when we sell we go to ind+2*/
+
+//Memoization
+class Solution {
+public:
+    int helper(int ind,int canBuy,vector<int>& prices,vector<vector<int>>& dp){
+        if(ind>=(prices.size())) return 0;
+        
+        if(dp[ind][canBuy]!=-1) return dp[ind][canBuy];
+        
+        int profit=0;
+        if(canBuy==1){
+            int buy=-prices[ind]+helper(ind+1,0,prices,dp);
+            int skipBuying=helper(ind+1,1,prices,dp);
+            
+            profit=max(buy,skipBuying);
+        }
+        else{
+            int sell=+prices[ind]+helper(ind+2,1,prices,dp);
+            int skipSelling=helper(ind+1,0,prices,dp);
+            
+            profit=max(sell,skipSelling);
+        }
+        
+        return dp[ind][canBuy]=profit;
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        
+        return helper(0,1,prices,dp);
+    }
+};
