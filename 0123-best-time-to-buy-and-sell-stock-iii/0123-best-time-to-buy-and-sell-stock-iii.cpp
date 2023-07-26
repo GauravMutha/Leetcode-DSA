0d9aa994b48c78b2f531ec094177f42025037531
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 17:18:40 +0530

    Time: 943 ms (5.58%), Space: 218.6 MB (9.39%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,37 +1,40 @@
-//Tabulation
-typedef vector<vector<vector<int>>> v3d;
+//Space optimisation
+
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int>& prices){
+        
         int n=prices.size();
-        // 3d vector v3d , we did n+1 here , index shifting
-        v3d dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> dp (2,vector<int>(3,0));
         
         for(int ind=n-1;ind>=0;ind--){
+            
+            vector<vector<int>>tempDP(2,vector<int>(3,0));
+            
             for(int canBuy=0;canBuy<=1;canBuy++){
                 for(int cap=1;cap<=2;cap++){
                     
                     int profit=0;
                     
                     if(canBuy==1){
-                        int buy=-prices[ind]+dp[ind+1][0][cap];
-                        int skipBuying=dp[ind+1][1][cap];
+                        int buy=-prices[ind]+dp[0][cap];
+                        int skipBuying=dp[1][cap];
 
                         profit=max(buy,skipBuying);
                     }
                     else{
-                        int sell= +prices[ind]+dp[ind+1][1][cap-1];
-                        int skipSelling= dp[ind+1][0][cap];
+                        int sell= +prices[ind]+dp[1][cap-1];
+                        int skipSelling= dp[0][cap];
 
                         profit=max(sell,skipSelling);
                     }
 
-                    dp[ind][canBuy][cap]=profit;
-                    
+                    tempDP[canBuy][cap]=profit;
                 }
             }
+            dp=move(tempDP);
         }
         
-         return dp[0][1][2];
+         return dp[1][2];
     }
 };
