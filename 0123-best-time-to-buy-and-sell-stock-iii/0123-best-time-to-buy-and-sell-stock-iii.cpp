b1b86718b5cf86be60dba77fdce721585266085a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 17:59:17 +0530

    Time: 375 ms (64.47%), Space: 116.9 MB (65.24%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,38 +1,41 @@
 //Using transaction id tid ,replacing cap and buy
-//dp is of Nx4 , instead of Nx2x3
 
-//Tabulation
+//Space Optimisation
 typedef vector<vector<vector<int>>> v3d;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         
-        vector<vector<int>>dp(n+1,vector<int>(5,0));
+        vector<int> dp(5,0);
         
         for(int ind=n-1;ind>=0;ind--){
+            
+            vector<int>tempDP(5,0);
+            
             for(int tid=3;tid>=0;tid--){
                 
                 int profit=0;
         
                 if(tid%2==0){
                     
-                    int buy=-prices[ind]+dp[ind+1][tid+1];
-                    int skipBuying= dp[ind+1][tid];
+                    int buy=-prices[ind]+dp[tid+1];
+                    int skipBuying= dp[tid];
 
                     profit=max(buy,skipBuying);
                 }
                 else{
-                    int sell= +prices[ind]+dp[ind+1][tid+1];
-                    int skipSelling= dp[ind+1][tid];
+                    int sell= +prices[ind]+dp[tid+1];
+                    int skipSelling= dp[tid];
 
                     profit=max(sell,skipSelling);
                 }
 
-                dp[ind][tid]=profit;
+                tempDP[tid]=profit;
             }
+            dp=move(tempDP);
         }
         
-         return dp[0][0];
+         return dp[0];
     }
 };
