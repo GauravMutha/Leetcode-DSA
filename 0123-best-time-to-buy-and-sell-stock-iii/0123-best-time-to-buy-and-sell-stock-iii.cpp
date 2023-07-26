abcc2b61f2001892c0cd23386e82341e7cbcfb22
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 17:11:03 +0530

    Time: 763 ms (53.66%), Space: 207 MB (46.63%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,33 +1,37 @@
-//Memoization
+//Tabulation
 typedef vector<vector<vector<int>>> v3d;
 class Solution {
 public:
-    int helper(int ind,int canBuy,int cap,vector<int>& prices,v3d &dp){
-        if(ind==prices.size() || cap==0) return 0;
-        
-        if(dp[ind][canBuy][cap]!=-1) return dp[ind][canBuy][cap];
-        
-        int profit=0;
-        if(canBuy==1){
-            int buy=-prices[ind]+helper(ind+1,false,cap,prices,dp);
-            int skipBuying= helper(ind+1,true,cap,prices,dp);
-            
-            profit=max(buy,skipBuying);
-        }
-        else{
-            int sell= +prices[ind]+helper(ind+1,true,cap-1,prices,dp);
-            int skipSelling= helper(ind+1,false,cap,prices,dp);
-            
-            profit=max(sell,skipSelling);
-        }
-        
-        return dp[ind][canBuy][cap]=profit;
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        // 3d vector v3d
-        v3d dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-         
-        return helper(0,1,2,prices,dp);
+        // 3d vector v3d , we did n+1 here , index shifting
+        v3d dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int canBuy=0;canBuy<=1;canBuy++){
+                for(int cap=1;cap<=2;cap++){
+                    
+                    int profit=0;
+                    
+                    if(canBuy==1){
+                        int buy=-prices[ind]+dp[ind+1][0][cap];
+                        int skipBuying=dp[ind+1][1][cap];
+
+                        profit=max(buy,skipBuying);
+                    }
+                    else{
+                        int sell= +prices[ind]+dp[ind+1][1][cap-1];
+                        int skipSelling= dp[ind+1][0][cap];
+
+                        profit=max(sell,skipSelling);
+                    }
+
+                    dp[ind][canBuy][cap]=profit;
+                    
+                }
+            }
+        }
+        
+         return dp[0][1][2];
     }
 };
