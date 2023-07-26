64f28cbf19af7054220e70c039096210ce241d13
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 17:56:48 +0530

    Time: 403 ms (61.99%), Space: 135.1 MB (61.57%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,38 +1,38 @@
 //Using transaction id tid ,replacing cap and buy
 //dp is of Nx4 , instead of Nx2x3
 
-//Memoization
-
+//Tabulation
+typedef vector<vector<vector<int>>> v3d;
 class Solution {
 public:
-    int helper(int ind,int tid,vector<int>& prices,vector<vector<int>>&dp)   {
-        if(ind==prices.size() || tid==4) return 0;
-        
-        if(dp[ind][tid]!=-1) return dp[ind][tid];
-        
-        int profit=0;
-        
-        if(tid%2==0){
-            int buy=-prices[ind]+helper(ind+1,tid+1,prices,dp);
-            int skipBuying= helper(ind+1,tid,prices,dp);
-            
-            profit=max(buy,skipBuying);
-        }
-        else{
-            int sell= +prices[ind]+helper(ind+1,tid+1,prices,dp);
-            int skipSelling= helper(ind+1,tid,prices,dp);
-            
-            profit=max(sell,skipSelling);
-        }
-        
-        return dp[ind][tid]=profit;
-    }
-    
     int maxProfit(vector<int>& prices) {
-        
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(4,-1));
-         
-        return helper(0,0,prices,dp);
+        
+        vector<vector<int>>dp(n+1,vector<int>(5,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int tid=3;tid>=0;tid--){
+                
+                int profit=0;
+        
+                if(tid%2==0){
+                    
+                    int buy=-prices[ind]+dp[ind+1][tid+1];
+                    int skipBuying= dp[ind+1][tid];
+
+                    profit=max(buy,skipBuying);
+                }
+                else{
+                    int sell= +prices[ind]+dp[ind+1][tid+1];
+                    int skipSelling= dp[ind+1][tid];
+
+                    profit=max(sell,skipSelling);
+                }
+
+                dp[ind][tid]=profit;
+            }
+        }
+        
+         return dp[0][0];
     }
 };
