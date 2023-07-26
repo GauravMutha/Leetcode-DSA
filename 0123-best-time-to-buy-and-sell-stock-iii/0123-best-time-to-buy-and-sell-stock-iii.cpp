59e5713e56428a2b8bd375b60b8639ea840cacbe
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 17:44:42 +0530

    Time: 402 ms (62.09%), Space: 132.5 MB (64.46%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,40 +1,38 @@
-//Space optimisation
+//Using transaction id tid ,replacing cap and buy
+//dp is of Nx4 , instead of Nx2x3
+
+//Memoization
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices){
+    int helper(int ind,int tid,vector<int>& prices,vector<vector<int>>&dp)   {
+        if(ind==prices.size() || tid==4) return 0;
         
-        int n=prices.size();
-        vector<vector<int>> dp (2,vector<int>(3,0));
+        if(dp[ind][tid]!=-1) return dp[ind][tid];
         
-        for(int ind=n-1;ind>=0;ind--){
+        int profit=0;
+        
+        if(tid%2==0){
+            int buy=-prices[ind]+helper(ind+1,tid+1,prices,dp);
+            int skipBuying= helper(ind+1,tid,prices,dp);
             
-            vector<vector<int>>tempDP(2,vector<int>(3,0));
+            profit=max(buy,skipBuying);
+        }
+        else{
+            int sell= +prices[ind]+helper(ind+1,tid+1,prices,dp);
+            int skipSelling= helper(ind+1,tid,prices,dp);
             
-            for(int canBuy=0;canBuy<=1;canBuy++){
-                for(int cap=1;cap<=2;cap++){
-                    
-                    int profit=0;
-                    
-                    if(canBuy==1){
-                        int buy=-prices[ind]+dp[0][cap];
-                        int skipBuying=dp[1][cap];
-
-                        profit=max(buy,skipBuying);
-                    }
-                    else{
-                        int sell= +prices[ind]+dp[1][cap-1];
-                        int skipSelling= dp[0][cap];
-
-                        profit=max(sell,skipSelling);
-                    }
-
-                    tempDP[canBuy][cap]=profit;
-                }
-            }
-            dp=move(tempDP);
+            profit=max(sell,skipSelling);
         }
         
-         return dp[1][2];
+        return dp[ind][tid]=profit;
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(4,-1));
+         
+        return helper(0,0,prices,dp);
     }
 };
