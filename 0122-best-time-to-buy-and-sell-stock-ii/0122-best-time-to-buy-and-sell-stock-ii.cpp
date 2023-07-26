d5dd6e98543879c8a12bc2ea77ada7705df860e0
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-26 15:20:13 +0530

    Time: 22 ms (5.69%), Space: 17.1 MB (16.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,33 @@
-//greedy , no DP
+//DP
+//Memoization
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int profit=0 , n=prices.size();
+    int helper(int ind,int canBuy,vector<int>& prices,vector<vector<int>>& dp){
+        if(ind==(prices.size())) return 0;
         
-        for(int i=1;i<n;i++){
-            if(prices[i-1]>prices[i]) continue;
-            profit+=(prices[i]-prices[i-1]);
+        if(dp[ind][canBuy]!=-1) return dp[ind][canBuy];
+        
+        int profit=0;
+        if(canBuy==1){
+            int buy=-prices[ind]+helper(ind+1,0,prices,dp);
+            int skipBuying=helper(ind+1,1,prices,dp);
+            
+            profit=max(buy,skipBuying);
+        }
+        else{
+            int sell=+prices[ind]+helper(ind+1,1,prices,dp);
+            int skipSelling=helper(ind+1,0,prices,dp);
+            
+            profit=max(sell,skipSelling);
         }
         
-        return profit;
+        return dp[ind][canBuy]=profit;
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        
+        return helper(0,1,prices,dp);
     }
 };
