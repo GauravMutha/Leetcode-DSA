a48b6dc5e6695dc2b331aae03ad68fd0008fe5f3
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-27 20:00:46 +0530

    Time: 388 ms (57.10%), Space: 291 MB (34.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,23 +1,28 @@
-//Tabulation
+//Space optimisation
 //As per first to last memo
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>dp (n+1,0);
         
         for(int ind=n-1;ind>=0;ind--){
+            
+            vector<int>tempDP(n+1,0);
+            
             for(int preInd=ind-1;preInd>=-1;preInd--){
                 
                 int pick=0;
-                if(preInd==-1 || nums[ind]>nums[preInd]) pick=1+dp[ind+1][ind+1];
-                int notPick=dp[ind+1][preInd+1];
+                if(preInd==-1 || nums[ind]>nums[preInd]) pick=1+dp[ind+1];
+                int notPick=dp[preInd+1];
                 
-                dp[ind][preInd+1]=max(pick,notPick);
+                tempDP[preInd+1]=max(pick,notPick);
             }
+            
+            dp=move(tempDP);
         }
         
-        return dp[0][0];
+        return dp[0];
     }
 };
