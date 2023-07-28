773b61b6a9d7b2910468ad1ce81cc1ef5a9112df
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-28 17:49:33 +0530

    Time: 202 ms (40.74%), Space: 100 MB (31.45%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,8 +1,10 @@
-//Tabulation 2.0
+//Memoization
 
 class Solution {
 public:
     bool check(string &str1 , string &str2){
+        if(str2.size() != (str1.size()+1) ) return false;
+        
         int i=0,j=0,diff=0;
         while(j<str2.size() && i<str1.size()){
             if(str1[i]==str2[j]) i++ , j++;
@@ -16,9 +18,24 @@ public:
         return true;
         
     }
+    int helper(int ind,int preInd,vector<string>& words,vector<vector<int>>& dp){
+        
+        if(ind>=words.size()) return 0;
+        
+        if(dp[ind][preInd+1]!=-1) return dp[ind][preInd+1];
+        
+        int pick=0;
+        
+        if(preInd==-1 || check(words[preInd],words[ind])) 
+            pick=1+helper(ind+1,ind,words,dp);
+        int notPick=helper(ind+1,preInd,words,dp);
+        
+        return dp[ind][preInd+1]=max(pick,notPick);
+    }
     int longestStrChain(vector<string>& words) {
+        
         int n=words.size(),res=1;
-        vector<int>dp (n,1);
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
         
         //writing cmp function to sort the words in increasing order of sizes
         auto cmp = [](const std::string& a, const std::string& b) {
@@ -27,19 +44,7 @@ public:
         sort(words.begin(),words.end(),cmp);
         
         
-        //Actual calculation similar to tabulation 2.0 of question 300.LIS
-        for(int ind=0;ind<n;ind++){
-            for(int pre=0;pre<ind;pre++){
-                
-                if(words[ind].size() != (words[pre].size()+1) ) continue;
-                
-                if(check(words[pre],words[ind]))
-                    dp[ind]=max(1+dp[pre],dp[ind]);
-            }
-            res=max(res,dp[ind]);
-        }
-        
-        return res;
+        return helper(0,-1,words,dp);
         
     }
 };
