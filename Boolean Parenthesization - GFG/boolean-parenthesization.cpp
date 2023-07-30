db56ba974c983dbf246f56d994f7f29b05b3f283
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-30 18:52:44 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -21,36 +21,54 @@ public:
             
         if(dp[i][j][wantTrue]!=-1) return dp[i][j][wantTrue];
         
-        int ways=0;
-        for(int ind=i+1;ind<j;ind+=2){
+    }
+    
+    int countWays(int n, string str){
+        
+        vector<vector<vector<int>>>dp (n,vector<vector<int>>(n,vector<int>(2,0)));
+        
+        for(int i=0;i<n;i+=2){
+            if(str[i]=='T') dp[i][i][1]=1;
+            else dp[i][i][0]=1;
+        }
+        
+        for(int i=n-1;i>=0;i--){
             
-            int lt=helper(i,ind-1,1,str,dp);
-            int lf=helper(i,ind-1,0,str,dp);
-            int rt=helper(ind+1,j,1,str,dp);
-            int rf=helper(ind+1,j,0,str,dp);
-            
-            
-            if(str[ind]=='&'){
-                if(wantTrue) ways = (ways+ (lt*rt)%mod)%mod;
-                else ways = (ways+ (lt*rf)%mod + (lf*rt)%mod + (lf*rf)%mod)%mod;
-            }
-            else if(str[ind]=='|'){
-                if(wantTrue) ways = (ways+ (lt*rt)%mod+ (lf*rt)%mod + (lt*rf)%mod)%mod;
-                else ways = (ways+ (rf*lf)%mod)%mod;
-            }
-            else if(str[ind]=='^'){
-                if(wantTrue) ways = (ways+ (lt*rf)%mod + (lf*rt)%mod)%mod;
-                else  ways = (ways+ (lf*rf)%mod + (lt*rt)%mod)%mod;
+            for(int j=i+1;j<n;j++){
+                
+                
+                for(int wantTrue=0;wantTrue<=1;wantTrue++){
+                    
+                    int ways=0;
+                    
+                    for(int ind=i+1;ind<j;ind+=2){
+                        
+                        int lt=dp[i][ind-1][1];
+                        int lf=dp[i][ind-1][0];
+                        int rt=dp[ind+1][j][1];
+                        int rf=dp[ind+1][j][0];
+                        
+                        if(str[ind]=='&'){
+                            if(wantTrue) ways = (ways+ (lt*rt)%mod)%mod;
+                            else ways = (ways+ (lt*rf)%mod + (lf*rt)%mod + (lf*rf)%mod)%mod;
+                        }
+                        else if(str[ind]=='|'){
+                            if(wantTrue) ways = (ways+ (lt*rt)%mod+ (lf*rt)%mod + (lt*rf)%mod)%mod;
+                            else ways = (ways+ (rf*lf)%mod)%mod;
+                        }
+                        else if(str[ind]=='^'){
+                            if(wantTrue) ways = (ways+ (lt*rf)%mod + (lf*rt)%mod)%mod;
+                            else  ways = (ways+ (lf*rf)%mod + (lt*rt)%mod)%mod;
+                        }
+                        
+                    }
+                    dp[i][j][wantTrue]=ways;
+                }
+                
             }
         }
         
-        return dp[i][j][wantTrue]=ways;
-    }
-    int countWays(int n, string str){
-        
-        vector<vector<vector<int>>>dp (n,vector<vector<int>>(n,vector<int>(2,-1)));
-        
-        return helper(0,n-1,1,str,dp);
+        return dp[0][n-1][1];
     }
 };
 
