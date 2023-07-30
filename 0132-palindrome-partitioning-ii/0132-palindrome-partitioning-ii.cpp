7bea18b15d124cb9761bb7987efcfc35884444ac
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-30 20:59:30 +0530

    Time: 1658 ms (57.12%), Space: 6.6 MB (75.30%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,5 +1,4 @@
-
-//Memoization
+//Tabulation
 
 class Solution {
 public:
@@ -11,31 +10,30 @@ public:
         return true;
     }
     
-    int helper(int ind,string &s,vector<int>& dp){
-        
-        if(ind==s.size()) return 0;
-        
-        if(dp[ind]!=-1) return dp[ind];
-        
-        int minCuts=INT_MAX;
-        
-        for(int i=ind;i<s.size();i++){
-            
-            if(palinCheck(s,ind,i)==false) continue;
-            
-            int cuts=1+helper(i+1,s,dp);
-            
-            minCuts=min(cuts,minCuts);
-        }
-        
-        return dp[ind]=minCuts;
-    }
     int minCut(string s) {
         
         int n=s.size();
         
-        vector<int>dp(n,-1);
+        vector<int>dp(n+1,0);
+        dp[n]=0;
         
-        return helper(0,s,dp)-1;
+        for(int ind=n-1;ind>=0;ind--){
+            
+            int minCuts=INT_MAX;
+
+            for(int i=ind;i<s.size();i++){
+                
+                if(palinCheck(s,ind,i)==false) continue;
+
+                int cuts=1+dp[i+1];
+
+                minCuts=min(cuts,minCuts);
+                
+            }
+
+            dp[ind]=minCuts;
+        }
+        
+        return dp[0]-1;
     }
 };
