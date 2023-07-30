090589c6051fe6634e4613a3fd05b6a2157f2327
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-07-30 15:59:02 +0530

    Time: 255 ms (98.24%), Space: 10.3 MB (34.18%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,22 +1,6 @@
-//Memoization
+//Tabulation
 class Solution {
 public:
-    int helper(int i,int j,vector<int>& nums,vector<vector<int>>& dp){
-        
-        if(i>j) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int maxCoins=INT_MIN;
-
-        for(int ind=i;ind<=j;ind++){
-            int coins=(nums[i-1]*nums[ind]*nums[j+1]+
-                      helper(i,ind-1,nums,dp)+helper(ind+1,j,nums,dp));
-            maxCoins=max(coins,maxCoins);
-        }
-
-        return dp[i][j]=maxCoins;
-    }
     int maxCoins(vector<int>& nums) {
         
         int lastIndex=nums.size();
@@ -24,8 +8,23 @@ public:
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         
-        vector<vector<int>> dp(lastIndex+1,vector<int>(lastIndex+1,-1));
+        vector<vector<int>> dp(lastIndex+2,vector<int>(lastIndex+2,0));
 
-        return helper(1,lastIndex,nums,dp);
+        for(int i=lastIndex;i>=1;i--){
+            for(int j=i;j<=lastIndex;j++){
+                
+                int maxCoins=INT_MIN;
+
+                for(int ind=i;ind<=j;ind++){
+                    int coins=(nums[i-1]*nums[ind]*nums[j+1]+ 
+                              dp[i][ind-1]+dp[ind+1][j]);
+                    maxCoins=max(coins,maxCoins);
+                }
+
+                dp[i][j]=maxCoins;
+            }
+        }
+        
+        return dp[1][lastIndex];
     }
 };
