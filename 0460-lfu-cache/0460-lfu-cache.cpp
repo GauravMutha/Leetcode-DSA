db276ab6a09c63106e488c31eaae7cf10bdad847
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-01 21:45:48 +0530

    Time: 491 ms (93.42%), Space: 205.5 MB (18.71%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,27 +1,28 @@
+//revision
+
 struct Node{
-    //The count of a node denotes its frequency
-    int cnt,key,value;
-    Node* next , *prev;
-    Node(int k , int v){
-        key=k , value=v , cnt=1;
-    }
+    int key,val,count;
+    Node *prev, *next;
+    Node(int k,int v){ key=k,val=v, count=1; };
 };
 struct List{
-    Node* head , *tail;
-    int size; //The size of list (excluding the head and tail ofcourse)
+    int size;
+    Node* head, *tail;
     List(){
         head=new Node(-1,-1);
         tail=new Node(-1,-1);
         
-        head->prev=NULL;
         head->next=tail;
+        head->prev=NULL;
+        tail->next=NULL;
         tail->prev=head;
-        tail->next=head;
         
         size=0;
     }
-    void addNodeAtListFront(Node* node){
+    
+    void addNode(Node* node){
         auto temp=head->next;
+        
         node->next=temp;
         node->prev=head;
         head->next=node;
@@ -30,113 +31,85 @@ struct List{
         size++;
     }
     
-    void deleteNode(Node* delNode){
-        Node* delNodePrev=delNode->prev;
-        Node* delNodeNext=delNode->next;
+    void deleteNode(Node *node){
+        auto behind=node->prev;
+        auto ahead=node->next;
         
-        delNodePrev->next=delNodeNext;
-        delNodeNext->prev=delNodePrev;
+        behind->next=ahead;
+        ahead->prev=behind;
         
         size--;
-    }
+    }   
 };
 class LFUCache {
 private:
-    int limit,minFreq,cacheSize;;
-    map<int,List*>listMap; // it maps frequency to its list
-    map<int,Node*>addressMap; // it maps key to its node-address, that is which node represent the key
+    map<int,Node*>addressMap;
+    map<int,List*>listMap;
+    int minFreq,cacheSize,limit;
 public:
     LFUCache(int capacity) {
         limit=capacity;
         minFreq=cacheSize=0;
     }
     
-    void updateListMap(Node *node){
-        /*remove node's key from address map and 
-        remove node from listMap based on node cnt
-        which is its frequency.If size of list might
-        become zero here after deletion of node
-        (head and tail does not count)*/
+    void updateListMap(Node* node){
         addressMap.erase(node->key);
-        listMap[node->cnt]->deleteNode(node);
+        listMap[node->count]->deleteNode(node);
         
-        /*node's frequency will increase as it has been visited
-        therefore if it was in list of minFreq and that frquency's
-        list had become zero above after deletion of node than 
-        minFreq has to be increase by one which will be same as
-        node->cnt +1 soon*/
-        if(minFreq==node->cnt && listMap[node->cnt]->size==0) minFreq++;
+        if(minFreq==node->count && listMap[node->count]->size==0)
+            minFreq++;
         
-        
-        /*In below steps we take the list which will receive the 
-        node now if the icreased frequency was there in map as key
-        then the new list is assigned that as this new list will add
-        the node and kind of update the already existing list for the 
-        frequecny Otherwise this list will be mapped as fresh new list 
-        to the newly added freqeuncy in map with a node 'node' in it*/
-        auto nextHigherFreqList=new List();
-        if(listMap.find(node->cnt+1)!=listMap.end()){
-            nextHigherFreqList=listMap[node->cnt+1];
+        auto newList=new List();
+        if(listMap.find(node->count+1)!=listMap.end()){
+            newList=listMap[node->count+1];
         }
         
-        node->cnt+=1;
-        nextHigherFreqList->addNodeAtListFront(node);
-        listMap[node->cnt]=nextHigherFreqList;
+        node->count++;
+        newList->addNode(node);
+        listMap[node->count]=newList;
         
         addressMap[node->key]=node;
     }
     
     int get(int key) {
         if(addressMap.find(key)==addressMap.end()) return -1;
-        Node* node = addressMap[key];
-        int val=node->value;
         
-        /*'get' changes the frequency of node
-        with the given key, hence the listMap 
-        and other things needs to be updated*/
+        auto node=addressMap[key];
+        int retVal=node->val;
+        
         updateListMap(node);
-        
-        return val;
+    
+        return retVal;
     }
     
     void put(int key, int value) {
         if(limit==0) return;
-        /*If the given key node is already there
-        then its frequency changes and listMap
-        and other things needs to be updated just
-        like how it happened in 'get' operation*/
+        
         if(addressMap.find(key)!=addressMap.end()){
-            Node* node=addressMap[key];
-            node->value=value;
+            auto node=addressMap[key];
+            node->val=value;
             updateListMap(node);
         }
-        /*if it was not present then we need to add it
-        into listMap by making a new list.Also since its
-        the first time entering the map , its frequency
-        will be one and minfrequnecy to will become 1*/
         else{
-            /*But before anything we need to check if the
-            new node adding will exceed the achelimit, if yes
-            then we need to remove the LR and LF used node
-            from iistMap*/
             if(limit==cacheSize){
                 auto list=listMap[minFreq];
                 addressMap.erase(list->tail->prev->key);
-                listMap[minFreq]->deleteNode(list->tail->prev);
+                list->deleteNode(list->tail->prev);
+                
                 cacheSize--;
             }
             cacheSize++;
             minFreq=1;
-            /*Below procedure is same as the one in put 
-            operation*/
+            
             auto list=new List();
             if(listMap.find(minFreq)!=listMap.end()){
                 list=listMap[minFreq];
             }
             auto node =new Node(key,value);
-            list->addNodeAtListFront(node);
+            list->addNode(node);
             addressMap[key]=node;
             listMap[minFreq]=list;
+            
         }
     }
 };
