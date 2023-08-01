45c6f1d3d9970178ed9a85bc5bd9adc6aa6585e7
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-01 08:55:27 +0530

    Time: 7 ms (99.02%), Space: 19.7 MB (90.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,23 @@
-//Withput using extra space
+//Revising second approach ( O(1) SC)
 class Solution {
 public:
     int trap(vector<int>& nums) {
-        int n=nums.size(),res=0;
-        int l=0,r=n-1 , leftMax=-1,rightMax=-1;
-        for(int i=0;i<n;i++){
+        int l=0,n=nums.size() , r=n-1;
+        int maxLeft=-1,maxRight=-1,res=0;
+        
+        while(l<r){
             if(nums[l]<=nums[r]){
-                if(nums[l]>=leftMax) leftMax=nums[l];
-                else res+=(leftMax-nums[l]);
+                if(nums[l]>=maxLeft) maxLeft=nums[l];
+                else res+=(maxLeft-nums[l]);
                 l++;
             }
             else{
-                if(nums[r]>=rightMax) rightMax=nums[r];
-                else res+=(rightMax-nums[r]);
+                if(nums[r]>=maxRight) maxRight=nums[r];
+                else res+=(maxRight-nums[r]);
                 r--;
             }
         }
+        
         return res;
     }
 };
