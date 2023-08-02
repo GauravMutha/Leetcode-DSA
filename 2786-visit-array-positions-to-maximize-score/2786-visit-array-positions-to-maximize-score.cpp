9b7e55dfcc0ba20bce96bc82055750905b8c3125
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-02 11:30:40 +0530

    Time: 296 ms (61.17%), Space: 135.3 MB (49.46%) - LeetHub

--- 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
+++ 2786-visit-array-positions-to-maximize-score/2786-visit-array-positions-to-maximize-score.cpp
@@ -1,22 +1,27 @@
-//Tabulation
+//Space optimisation
 
 class Solution {
 public:
     long long maxScore(vector<int>& nums, int x) {
         
         int n=nums.size();
-        vector<vector<long long>>dp(n+1,vector<long long>(2,0));
+        vector<long long>dp(2,0);
         
         for(int ind=n-1;ind>=1;ind--){
+            
+            vector<long long>tempDP(2,0);
+            
             for(int parity=0;parity<=1;parity++){
                 
-                long long pick = nums[ind]- (!(parity==(nums[ind]%2)))*x + dp[ind+1][nums[ind]%2];
-                long long notPick = dp[ind+1][parity];
+                long long pick = nums[ind]- (!(parity==(nums[ind]%2)))*x + dp[nums[ind]%2];
+                long long notPick = dp[parity];
 
-                dp[ind][parity]=max(pick,notPick);
+                tempDP[parity]=max(pick,notPick);
             }
+            
+            dp=move(tempDP);
         }
         
-        return (nums[0]+dp[1][nums[0]%2]);
+        return (nums[0]+dp[nums[0]%2]);
     }
 };
