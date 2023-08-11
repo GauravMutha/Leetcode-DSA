6b7c0156ebfe143b26ef3e99ef89270c146fb426
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-11 15:49:42 +0530

    Time: 0 ms (100.00%), Space: 10 MB (14.58%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int helper(int ind,vector<int>& dp,vector<int>& days,vector<int>& costs)
-    {
-        if(ind>=days.size()) return 0;
-        
-        if(dp[ind]!=-1) return dp[ind];
-        
-        //one day pass
-        int cost_one=costs[0]+ helper(ind+1,dp,days,costs);
-        
-        int i;
-        //week day pass
-        for(i=ind;i<days.size() && days[i]<(days[ind]+7);i++);
-        int cost_week=costs[1]+helper(i,dp,days,costs);
-        
-        //month day pass
-        for(i=ind;i<days.size() && days[i]<(days[ind]+30);i++);
-        int cost_month=costs[2]+helper(i,dp,days,costs);
-        
-        return dp[ind]=min({cost_one,cost_week,cost_month});
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(366,-1);
-        return helper(0,dp,days,costs);
+        
+        int lastDay=days.back();
+        set<int>st(days.begin(),days.end());
+        vector<int>dp(lastDay+1,0);
+        
+        for(int i=1;i<=lastDay;i++){
+            if(st.find(i)==st.end()) dp[i]=dp[i-1];
+            else{
+                int cost_one=costs[0]+dp[i-1];
+                int cost_week=costs[1]+dp[max(0,i-7)];
+                int cost_month=costs[2]+dp[max(0,i-30)];
+                
+                dp[i]=min({cost_one,cost_week,cost_month});
+            }
+        }
+        
+        return dp[lastDay];
     }
 };
