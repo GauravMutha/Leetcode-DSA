03eb250e97f0ad603b1d57702b873f1b325e7e83
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-12 19:10:17 +0530

    Time: 265 ms (90.95%), Space: 10.4 MB (8.37%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -19,8 +19,24 @@ public:
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         
-        vector<vector<int>>dp(lastInd+1,vector<int>(lastInd+1,-1));
+        vector<vector<int>>dp(lastInd+2,vector<int>(lastInd+2,0));
         
-        return helper(1,lastInd,nums,dp);
+        for(int i=lastInd;i>=1;i--){
+            for(int j=i;j<=lastInd;j++){
+                
+                int maxCoins=INT_MIN;
+                for(int k=i;k<=j;k++){
+
+                    int val=nums[i-1]*nums[k]*nums[j+1];
+                    int coins=val+dp[i][k-1]+dp[k+1][j];
+
+                    maxCoins=max(maxCoins,coins);
+                }
+
+                dp[i][j]= maxCoins;
+            }
+        }
+        
+        return dp[1][lastInd];
     }
 };
