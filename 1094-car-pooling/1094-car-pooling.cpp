f2eeb3968a4b9b3e3acd22d176076ee630089840
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-12 15:24:03 +0530

    Time: 7 ms (97.82%), Space: 10.1 MB (90.29%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,24 +1,25 @@
-//Prefix Sum
-//O(n) TC
-//3 pass
+typedef pair<int,int>pii; 
 class Solution {
 public:
-    bool carPooling(vector<vector<int>>& vec, int cap) {
-        int in_car=0;
-        auto max_drop = (*std::max_element(begin(vec), end(vec), [](auto& a, auto& b){ return a[2] < b[2]; }))[2];
+    bool carPooling(vector<vector<int>>& trips, int cap) {
         
-        vector<int>stops(max_drop+1,0);
+        sort(begin(trips),end(trips),[](const auto &a,const auto &b){
+            return a[1]<b[1];
+        });
+        priority_queue<pii,vector<pii>,greater<pii>>pq;
         
-        for(int i=0;i<vec.size();i++){
-            stops[vec[i][1]]+=vec[i][0];
-            stops[vec[i][2]]-=vec[i][0];
-        }
+        int curr=trips[0][0];
+        pq.push({trips[0][2],trips[0][0]});
+        if(curr>cap) return false;
         
-        for(int i=0;i<stops.size();i++){
-            in_car+=stops[i];
-            if(in_car>cap) return false;
+        for(int i=1;i<trips.size();i++){
+            while(pq.size() && trips[i][1]>=pq.top().first) 
+                curr-=pq.top().second,pq.pop();
+            
+            curr+=trips[i][0];
+            pq.push({trips[i][2],trips[i][0]});
+            if(curr>cap) return false;
         }
         return true;
-        
     }
 };
