132e5bc559e49574a973ca9c095d42d2548b2e1a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-17 15:37:04 +0530

    Time: 4 ms (83.56%), Space: 17.5 MB (77.36%) - LeetHub

--- 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -1,35 +1,12 @@
-//memoization
-class Solution {
-private:
-    vector<int>pre;    
+//greedy
+class Solution {  
 public:
-    int helper(int i,int j,int m,vector<int>& nums,vector<vector<int>>& dp){
-        
-        if(i==j) return dp[i][j]=1; 
-        if(i>j || (pre[j]-pre[i]+nums[i])<m) return dp[i][j]=0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        for(int k=i;k<j;k++){
-            bool possible=(helper(i,k,m,nums,dp) && helper(k+1,j,m,nums,dp));
-            
-            if(possible) return dp[i][j]=1;
-        }
-        
-        return dp[i][j]=0;
-    }
     bool canSplitArray(vector<int>& nums, int m) {
-        
         int n=nums.size();
         if(n<=2) return true;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        
-        pre.resize(n);
-        pre[0]=nums[0];
-        
         for(int i=1;i<n;i++)
-            pre[i]=nums[i]+pre[i-1];
+            if((nums[i]+nums[i-1])>=m) return true;
         
-        return helper(0,n-1,m,nums,dp);
+        return false;
     }
 };
