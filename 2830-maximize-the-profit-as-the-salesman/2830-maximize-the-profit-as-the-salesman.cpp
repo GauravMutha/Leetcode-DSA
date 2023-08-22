62adda89c294eaeb9eddf7ed22d4115902e28376
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-08-22 09:19:29 +0530

    Time: 474 ms (53.48%), Space: 123.4 MB (45.98%) - LeetHub

---
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
@@ -0,0 +1,34 @@
+//Memoization
+//Implementing Upper bound
+class Solution {
+public:
+    int helper(int ind,vector<vector<int>>& offers,vector<int>& dp){
+        if(ind>=offers.size()) return 0;
+        
+        if(dp[ind]!=-1) return dp[ind];
+        
+        int nextInd=offers.size() , low=ind+1,high=offers.size()-1;
+        
+        
+        while (low <= high) {
+            int mid = (low+high)/2;
+            if (offers[mid][0] > offers[ind][1]) {
+                high = mid - 1;
+                nextInd = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        int notPick=helper(ind+1,offers,dp);
+        int pick=offers[ind][2]+helper(nextInd,offers,dp);
+        
+        return dp[ind]=max(notPick,pick);
+    }
+    int maximizeTheProfit(int n, vector<vector<int>>& offers) {
+        int m=offers.size();
+        sort(offers.begin(),offers.end());
+        vector<int> dp(m,-1);
+        return helper(0,offers,dp);
+    }
+};
