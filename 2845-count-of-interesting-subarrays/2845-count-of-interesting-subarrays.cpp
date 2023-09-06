9ef1dd74fddc005952fb759c70ecb7d2f218cbbd
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-06 22:51:35 +0530

    Time: 411 ms (5.04%), Space: 121.7 MB (37.76%) - LeetHub

--- 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.cpp
@@ -2,16 +2,28 @@ class Solution {
 public:
     long long countInterestingSubarrays(vector<int>& nums, int mod, int k) {
         long long res=0;
-        int prefix=0;
+        
+        vector<int>prefix(nums.size(),0);
+        prefix[0]=((nums[0]%mod)==k) ? 1 : 0;
+        
         unordered_map<int,int>mp;
         mp[0]=1;
         
-        for(auto num : nums){
-            prefix=(prefix+((num%mod==k)?1:0))%mod;
-            res+=mp[(prefix-k+mod)%mod];
-            mp[prefix]++;
+        for(int i=1;i<nums.size();i++){
+            int x=((nums[i]%mod)==k);
+            prefix[i]=(prefix[i-1]+(x ? 1 : 0));
+        
+        }
+        for(int i=0;i<nums.size();i++){
+            
+            cout<<prefix[i]<<endl;
+            
+            int find=prefix[i]%mod-k;
+            if(find<0) find+=mod;
+            res+=mp[find];
+            mp[prefix[i]%mod]++;
         }
         
-        return res;       
+        return res;
     }
 };
