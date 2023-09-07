853c49dcf8f08cbe2933213b9bdcafde8c5902b6
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-07 12:12:54 +0530

    Time: 6 ms (23.93%), Space: 6.1 MB (96.48%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.cpp
@@ -1,24 +1,22 @@
+//DP Memoization
 class Solution {
 public:
-    int minimumOperations(string num) {
+    int dp[101][25];
+    int helper(int ind,int num,string &s){
         
-        bool exist5=false, exist0=false;
-        int sz=num.size();
+        if(ind==s.size()) return (num%25==0)? 0 : 1e9;
+        if(dp[ind][num]!=-1) return dp[ind][num];
         
-        for(int i=sz-1;i>=0;i--){
-            
-            char d=num[i];
-            
-            if(d=='0'){
-                if(exist0) return sz-i-2;
-                exist0=true;
-            }
-            if(d=='5'){
-                if(exist0) return sz-i-2;
-                exist5=true;
-            }
-            if((d=='2' || d=='7') && exist5) return sz-i-2;
-        }
-        return sz-exist0;
+        int dig=s[ind]-'0';
+        
+        int pick=helper(ind+1,(num*10+dig)%25,s);
+        int notPick=1+helper(ind+1,num,s);
+        
+        return dp[ind][num]=min(pick,notPick);
+        
+    }
+    int minimumOperations(string s) {
+        memset(dp,-1,sizeof(dp));
+        return helper(0,0,s);    
     }
 };
