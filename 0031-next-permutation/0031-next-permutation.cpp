5dd1817fd4112bbf77dad954e500604f49f9b8dc
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-08 16:04:23 +0530

    Time: 2 ms (73.96%), Space: 12.1 MB (25.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,12 +1,14 @@
+//Striver 79 sheet
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i=nums.size()-1;
+        
+        int sz=nums.size(), i=sz-1;
         while(i>0 && nums[i]<=nums[i-1]) i--;
         reverse(nums.begin()+i,nums.end());
         if(i==0) return;
         int k=i-1;
-        while(i<nums.size() && nums[i]<=nums[k]) i++;
+        while(i<sz && nums[i]<=nums[k]) i++;
         swap(nums[k],nums[i]);
         return;
     }
