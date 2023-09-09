8dad64a60bee831531b27abe0e44e8b47725e2d4
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-09 11:38:10 +0530

    Time: 5 ms (49.31%), Space: 14.2 MB (8.18%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,42 +1,27 @@
-// striver sheet
-//taking pivot concept
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        if(nums.back()==target || nums[0]==target) return true;
+        int low=0,high=nums.size()-1;
         
-        int sz=nums.size();
-        int l=0,r=sz-1;
-        
-        //trimming search space
-        while(l+1<sz && nums[l]==nums[0]) l++;
-        if(l==(sz-1)) return (nums[0]==target);
-        
-        while(r>=0 && nums[r]==nums[r-1]) r--; 
-        
-        //find pivot
-        int start=l, end=r, pivot=-1;
-        while(l<r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]>nums.back()) l=mid+1;
-            else r=mid;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target) return true;
+            
+            if(nums[mid] == nums[low] && nums[mid]==nums[high] ){
+                low++, high--;
+                continue;
+            }
+            
+            if(nums[mid]<=nums[high]){
+                if(target>nums[mid] && target<=nums[high]) low=mid+1;
+                else high=mid-1;
+            }
+            else{
+                if(target>=nums[low] && target<nums[mid]) high=mid-1;
+                else low=mid+1;
+            }
         }
-        pivot=l;
-        //find the correct half to search into
-        if(pivot==-1) l=start, r=end; //there is no half
-        else {
-            if(target>nums[end]) l=start, r=pivot-1;
-            else l=pivot, r=end;
-        }
-        
-        // normal binary search
-        while ( l <= r ){
-            int mid = l + (r-l)/2;
-            if( nums[mid] > target ) r = mid-1;
-            else if( nums[mid] < target ) l = mid+1;
-            else return true;
-        }
-        
         return false;
     }
 };
