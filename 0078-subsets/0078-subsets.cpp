fb4af2e2514f924e47310c24feefbe735ac6b66e
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-09 11:50:46 +0530

    Time: 4 ms (41.01%), Space: 7.2 MB (54.98%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,22 @@
-//Re//
-//A slightly different implementation of backtracking
 class Solution {
-private:
-    vector<vector<int>>ans;
 public:
-    void helper(int pos,vector<int>& ds,vector<int>& nums){
-        ans.push_back(ds);
-        for(int i=pos;i<nums.size();i++){
-            ds.push_back(nums[i]);
-            helper(i+1,ds,nums);
-            ds.pop_back();
+    vector<vector<int>>res;
+    void helper(int ind, vector<int>& ds, vector<int>& nums){
+        if(ind==nums.size()){
+            res.push_back(ds);
+            return;
         }
+        
+        ds.push_back(nums[ind]);
+        helper(ind+1,ds,nums);
+        
+        ds.pop_back();
+        helper(ind+1,ds,nums);
+        
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int>ds;
         helper(0,ds,nums);
-        
-        return ans;
+        return res;
     }
 };
