79600996672c0756a44575dfdc6f390f348430ca
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-12 12:35:25 +0530

    Time: 3 ms (45.98%), Space: 6.3 MB (28.39%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int open=0,closed=0;
-        for(auto c:s){
+        int open=0,extraClosed=0;
+        for(auto c : s){
             if(c=='(') open++;
-            else if(open>0) open--;
-            else  closed++;
+            else (open)?open--:extraClosed++;
         }
-        return open + closed;
+        return open+extraClosed;
     }
 };
-
-//Optimization -> O(1)
