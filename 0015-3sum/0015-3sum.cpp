156b53f67ca2e3c3012207e3eac9f52b36d75aa0
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-12 16:30:00 +0530

    Time: 487 ms (35.62%), Space: 27.1 MB (45.29%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,25 +1,37 @@
-//O(n^2)
 class Solution {
+private:
+    int n=0;
+    vector<vector<int>>res;
 public:
+    int binarySearch(int low,vector<int>& nums, int target){
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
+    }
+    void twoSum(int start,vector<int>& nums, int target){
+        for(int i=start;i<n-1;i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+            int k=binarySearch(i+1,nums,target-nums[i]);
+            if(k==-1) continue;
+            res.push_back(vector<int>({-target,nums[i],nums[k]}));
+        }
+    }
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>>res;
-        for(int i=0;i<nums.size()-2;i++){
+        n=nums.size();
+        int target=0; //target for the triplets is 0 actually
+        for(int i=0;i<n-2;i++){
             if(nums[i]>0) break;
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int target=-nums[i];
-            int l=i+1,r=nums.size()-1;
-            while(l<r){
-                if((nums[l]+nums[r])>target) r--;
-                else if((nums[l]+nums[r])<target) l++;
-                else{
-                    vector<int>triplets={nums[i],nums[l],nums[r]};
-                    res.push_back(triplets);
-                    while(l<r && nums[l]==triplets[1]) l++;
-                    while(l<r && nums[r]==triplets[2]) r--;
-                }
-            }
+            twoSum(i+1,nums,target-nums[i]);
         }
+        
         return res;
     }
 };
