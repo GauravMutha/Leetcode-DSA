75bf8fa5223df6479331a4500a8d24ea95424a57
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-14 10:43:08 +0530

    Time: 89 ms (42.47%), Space: 44.1 MB (17.47%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        int maxFreq =0 , maxFreqCount=0;
+    int leastInterval(vector<char>& tasks, int idle) {
+        int ans=0;
         vector<int>hash(26,0);
-        for(auto &c : tasks){
-            int freq=++hash[c-'A'];
-            if(freq==maxFreq) maxFreqCount++;
-            else if(freq>maxFreq) maxFreq=freq, maxFreqCount=1;
+        priority_queue<int>maxh;
+        
+        for(auto c : tasks) hash[c-'A']++;
+        for(int i=0;i<26;i++) if(hash[i]) maxh.push(hash[i]);
+        
+        while(maxh.size()){
+            int k=min(idle+1,(int)maxh.size());
+            
+            vector<int>temp;
+            for(int i=0;i<k;i++) 
+                temp.push_back(maxh.top()),maxh.pop();
+            for(int i=0;i<k;i++) if(temp[i]-1) maxh.push(temp[i]-1);
+            
+            int extra=maxh.empty()?0:idle+1-k;
+            ans+=(k+extra);
         }
         
-        int fillerBlocks=maxFreq-1;
-        int emptySlots=fillerBlocks*(n-(maxFreqCount-1));
-        int remainingTasks=tasks.size()-maxFreq*maxFreqCount;
-        int idles=max(0,emptySlots-remainingTasks);
-        
-        return (tasks.size()+idles);
-        
+        return ans;
     }
 };
