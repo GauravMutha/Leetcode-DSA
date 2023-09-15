bd883ffbc01313f886007b1b74a31eb9bbd98dd8
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-15 15:54:21 +0530

    Time: 1541 ms (13.30%), Space: 176.4 MB (20.78%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,30 +1,44 @@
-typedef pair<int,int>pii;
+
 class Solution {
 public:
-    int getDist(int curr,int next,vector<vector<int>>& p){
-        return (abs(p[curr][0]-p[next][0])+abs(p[curr][1]-p[next][1]));
+    int findParent(int node,vector<int>& parent){
+        if(parent[node]==node) return node;
+        int temp=findParent(parent[node],parent);
+        return parent[node]=temp;
+    }
+    bool doUnion(int x,int y,vector<int>& parent, vector<int>& rank){
+        int xParent=findParent(x,parent);
+        int yParent=findParent(y,parent);
+        
+        if(xParent==yParent) return false;
+        
+        if(rank[xParent]>rank[yParent])
+            parent[yParent]=xParent;
+        else if(rank[xParent]<rank[yParent])
+            parent[xParent]=yParent;
+        else parent[xParent]=yParent, rank[yParent]++;
+        
+        return true;
     }
     int minCostConnectPoints(vector<vector<int>>& points) {
+        int n=points.size(), res=0;
+        vector<vector<int>>edges;
+        vector<int>parent(n),rank(n,0);
         
-        int n=points.size(), connected=0, curr=0,res=0;
-        priority_queue<pii,vector<pii>,greater<pii>>pq;
-        vector<bool>visited(n,false);
+        for(int i=0;i<n;i++) parent[i]=i;
         
-        while(++connected<n){
-            
-            visited[curr]=true;
-            
-            for(int next=0;next<n;next++){
-                if(visited[next]==true) continue;
-                int dist=getDist(curr,next,points);
-                pq.push({dist,next});
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int dist=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                edges.push_back(vector<int>({dist,i,j}));
             }
-            
-            while(visited[pq.top().second]) pq.pop();
-            int shortestDist=pq.top().first;
-            res+=shortestDist;
-            curr=pq.top().second;
-            pq.pop();
+        }
+        
+        sort(edges.begin(),edges.end());
+        
+        for(int i=0;i<edges.size();i++){
+            int dist=edges[i][0], u=edges[i][1], v=edges[i][2];
+            if(doUnion(u,v,parent,rank)) res+=dist;
         }
         
         return res;
