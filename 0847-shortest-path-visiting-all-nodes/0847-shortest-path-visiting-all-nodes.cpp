39726f22b6388def367d648a3c193e550e0b372a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-17 15:44:04 +0530

    Time: 106 ms (34.82%), Space: 18.6 MB (24.90%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        int n = graph.size();
+        int allVisitedMask = (1<<n)-1;
+        queue<pair<int,pair<int,int>>>Q;
+        set<pair<int,int>>seen;//<currNode,mask as path>
+        // Initially push all nodes in Queue to start the path with all nodes;
+        for(int i=0;i<n;i++){
+            Q.push({i,{0,1<<i}});
+            seen.insert({i,1<<i});
+        }
+        while(not Q.empty()){
+            auto curr = Q.front();
+            Q.pop();
+            int node = curr.first, dist = curr.second.first, mask = curr.second.second;
+            //If all nodes are visited
+            if(mask==allVisitedMask)return dist;
+            // Go through all neighbours
+            for(auto &nei:graph[node]){
+                int nextMask = mask | (1<<nei);
+                if(not seen.count({nei,nextMask})){
+                    Q.push({nei,{dist+1,nextMask}});
+                    seen.insert({nei,nextMask});
+                }
+            }
+        }
+        return 0;
+    }
+};
