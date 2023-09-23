da91f0f4ca8921a6c5859c630bae8a881267837c
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-23 12:03:22 +0530

    Time: 67 ms (92.81%), Space: 13.6 MB (57.07%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,38 +1,22 @@
-//Tabu
 class Solution {
 public:
-    bool check(string &s1 , string &s2){
-        
-        int i=0,j=0,diff=0,n1=s1.length(),n2=s2.size();
-        
-        if(n2!=(n1+1)) return false;
-        
-        while(i<n1 && j<n2){
-            if(s1[i]==s2[j]) i++,j++;
+    bool check(string &str1 , string &str2){
+        int i=0,j=0,diff=0;
+        while(j<str2.size() && i<str1.size()){
+            if(str1[i]==str2[j]) i++ , j++;
             else{
                 diff++;
                 if(diff>1) return false;
                 j++;
             }
         }
+        
         return true;
     }
-    int helper(int ind, int pre,vector<string>& words,vector<vector<int>>& dp){
-        
-        if(ind>=words.size()) return 0;
-        
-        if(dp[ind][pre+1]!=-1) return dp[ind][pre+1];
-        
-        int pick=0;
-        if(pre==-1 || check(words[pre],words[ind]))
-            pick=1+helper(ind+1,ind,words,dp);
-        int notPick=helper(ind+1,pre,words,dp);
-    
-        return dp[ind][pre+1]=max(pick,notPick);
-    }
     int longestStrChain(vector<string>& words) {
-        int n=words.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int n=words.size(),res=1;
+        
+        vector<int>dp(n,1);
         
         auto cmp=[](const string &a , const string &b){
             return a.length()<b.length();
@@ -40,18 +24,15 @@ public:
         
         sort(words.begin(),words.end(),cmp);
         
-        for(int ind=n-1;ind>=0;ind--){
-            for(int pre=ind-1;pre>=-1;pre--){
-                int pick=0;
-                if(pre==-1 || check(words[pre],words[ind]))
-                    pick=1+dp[ind+1][ind+1];
-                int notPick=dp[ind+1][pre+1];
-    
-                dp[ind][pre+1]=max(pick,notPick);
+        for(int ind=0;ind<n;ind++){
+            for(int pre=0;pre<ind;pre++){
+                if(words[ind].size()!=(words[pre].size()+1)) continue;
+                if(check(words[pre],words[ind]))
+                    dp[ind]=max(1+dp[pre],dp[ind]);
             }
+            res=max(res,dp[ind]);
         }
         
-        return helper(0,-1,words,dp);
-    
+        return res;
     }
 };
