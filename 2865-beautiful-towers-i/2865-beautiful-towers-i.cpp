b9369c64014d975a804f312760b7d7d52da09a99
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-09-29 15:54:58 +0530

    Time: 15 ms (96.50%), Space: 26.9 MB (23.27%) - LeetHub

--- 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     long long maximumSumOfHeights(vector<int>& mh) {
-        
-        long long res=0LL;
         int n=mh.size();
+        long long i=0, res=0;
+        stack<int>s1,s2;
+        vector<long long>pref(n,mh[0]),suff(n,mh[n-1]);
+        vector<int>nsl(n,-1),nsr(n,-1);
+        
+        //next smaller to left & filling prefix
+        s1.push(0);
+        for(i=1;i<n;i++){
+            
+            while(s1.size() && mh[s1.top()]>mh[i]) s1.pop();
+            if(s1.size()) nsl[i]=s1.top();
+            s1.push(i);
+            
+            if(nsl[i]==-1) pref[i]=(i+1)*mh[i];
+            else pref[i]=pref[nsl[i]]+(i-nsl[i])*mh[i];
+        }
+        //next smaller to left & filling prefix
+        s2.push(n-1);
+        for(i=n-2;i>=0;i--){
+            
+            while(s2.size() && mh[s2.top()]>mh[i]) s2.pop();
+            if(s2.size()) nsr[i]=s2.top();
+            s2.push(i);
+            
+            if(nsr[i]==-1) suff[i]=(n-i)*mh[i];
+            else suff[i]=suff[nsr[i]]+(nsr[i]-i)*mh[i];
+        }
         
         for(int i=0;i<n;i++){
-            
-            int lpeak,gpeak; //local peak , global peak
-            lpeak=gpeak=mh[i];
-            long long sum=(long long)mh[i];
-            
-            //left side of peak
-            for(int j=i-1;j>=0;j--){
-                int curr=mh[j];
-                if(curr>lpeak) curr=lpeak;
-                else if(curr<lpeak) lpeak=curr;
-                
-                sum+=(long long)curr;
-            }
-            
-            lpeak=gpeak;
-            //right side of peak
-            for(int j=i+1;j<n;j++){
-                int curr=mh[j];
-                if(curr>lpeak) curr=lpeak;
-                else if(curr<lpeak) lpeak=curr;
-                
-                sum+=(long long)curr;
-            }
-            cout<<sum<<endl;
-            res=max(sum,res);
+            long long sum=(pref[i] + suff[i] - mh[i]);
+            res=max(res,sum);
         }
+        
         return res;
     }
 };
