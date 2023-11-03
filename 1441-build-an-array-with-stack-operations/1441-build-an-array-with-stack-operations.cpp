2c6e3f7259cc20bbf59cfde4473792b621370e9a
GauravMutha <107302738+GauravMutha@users.noreply.github.com>
2023-11-03 12:08:04 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (11.73%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    void addremove(int x, vector<string>& ans){
+        while(x--){
+            ans.push_back("Push");
+            ans.push_back("Pop");
+        }
+    }
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string>ans;
-        for(int i=1,k=0;k<target.size() && i<=n;i++){
+        for(int k=0;k<target.size();k++){
+            if(k==0) addremove(target[k]-0-1,ans);
+            else addremove(target[k]-target[k-1]-1,ans);
             ans.push_back("Push");
-            if(i!=target[k]) ans.push_back("Pop");
-            else k++;
         }
         return ans;
     }
